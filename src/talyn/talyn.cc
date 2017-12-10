545fa0bb48182d9f0537d5f4bbc4ffc1290e7ce6
Niels Martignène <niels.martignene@protonmail.com>
2017-12-10 12:20:42 +0100

    Enable content compression for JSON data in Talyn

--- src/talyn/talyn.cc
+++ src/talyn/talyn.cc
@@ -117,152 +117,165 @@ static bool UpdateStaticResources()
 }
 #endif
 
-// FIXME: Switch to stream / callback-based API
-static bool BuildCatalog(Date date, rapidjson::MemoryBuffer *out_buffer)
+static MHD_Response *BuildJson(CompressionType compression_type,
+                               std::function<bool(rapidjson::PrettyWriter<JsonStreamWriter> &)> func)
 {
+    HeapArray<uint8_t> buffer;
+    {
+        StreamWriter st(&buffer, nullptr, compression_type);
+        JsonStreamWriter json_stream(&st);
+        rapidjson::PrettyWriter<JsonStreamWriter> writer(json_stream);
+
+        if (!func(writer))
+            return nullptr;
+    }
+
+    MHD_Response *response = MHD_create_response_from_buffer((size_t)buffer.len, buffer.ptr,
+                                                             MHD_RESPMEM_MUST_COPY);
+    MHD_add_response_header(response, "Content-Type", "application/json");
+    switch (compression_type) {
+        case CompressionType::None: {} break;
+        case CompressionType::Zlib:
+            { MHD_add_response_header(response, "Content-Encoding", "deflate"); } break;
+        case CompressionType::Gzip:
+            { MHD_add_response_header(response, "Content-Encoding", "gzip"); } break;
+    }
+
+    return response;
+}
+
+static MHD_Response *ProducePriceMap(MHD_Connection *conn, const char *,
+                                     CompressionType compression_type)
+{
+    Date date = {};
+    {
+        const char *date_str = MHD_lookup_connection_value(conn, MHD_GET_ARGUMENT_KIND, "date");
+        date = Date::FromString(date_str);
+        if (!date.value)
+            return nullptr;
+    }
+
     const TableIndex *index = table_set->FindIndex(date);
     if (!index) {
         LogError("No table index available on '%1'", date);
-        return false;
+        return nullptr;
     }
+
     const HashSet<GhmCode, GhmConstraint> *constraints = nullptr;
     if (index_to_constraints.len) {
         constraints = index_to_constraints[index - table_set->indexes.ptr];
     }
 
-    Allocator temp_alloc;
-    rapidjson::PrettyWriter<rapidjson::MemoryBuffer> writer(*out_buffer);
+    MHD_Response *response = BuildJson(compression_type,
+                                       [&](rapidjson::PrettyWriter<JsonStreamWriter> &writer) {
+        char buf[512];
 
-    writer.StartArray();
-    for (const GhmRootInfo &ghm_root_info: index->ghm_roots) {
-        const GhmRootDesc *ghm_root_desc = catalog_set->ghm_roots_map.Find(ghm_root_info.ghm_root);
-
-        writer.StartObject();
-        // TODO: Use buffer-based Fmt API
-        writer.Key("ghm_root"); writer.String(Fmt(&temp_alloc, "%1", ghm_root_info.ghm_root).ptr);
-        if (ghm_root_desc) {
-            writer.Key("ghm_root_desc"); writer.String(ghm_root_desc->ghm_root_desc);
-        }
-        writer.Key("ghs"); writer.StartArray();
-
-        Span<const GhsInfo> compatible_ghs = index->FindCompatibleGhs(ghm_root_info.ghm_root);
-        for (const GhsInfo &ghs_info: compatible_ghs) {
-            const GhmConstraint *constraint = constraints ? constraints->Find(ghs_info.ghm) : nullptr;
-//            if (!constraint)
-//                continue;
-            const GhsPricing *ghs_pricing = pricing_set->FindGhsPricing(ghs_info.ghs[0], date);
-            if (!ghs_pricing)
-                continue;
+        writer.StartArray();
+        for (const GhmRootInfo &ghm_root_info: index->ghm_roots) {
+            const GhmRootDesc *ghm_root_desc = catalog_set->ghm_roots_map.Find(ghm_root_info.ghm_root);
 
             writer.StartObject();
-            writer.Key("ghm"); writer.String(Fmt(&temp_alloc, "%1", ghs_info.ghm).ptr);
-            writer.Key("ghm_mode"); writer.String(&ghs_info.ghm.parts.mode, 1);
-            if (constraint) {
-                writer.Key("duration_mask"); writer.Uint(constraint->duration_mask);
-            } else {
-                writer.Key("duration_mask"); writer.Uint(UINT_MAX);
+            // TODO: Use buffer-based Fmt API
+            writer.Key("ghm_root"); writer.String(Fmt(buf, "%1", ghm_root_info.ghm_root).ptr);
+            if (ghm_root_desc) {
+                writer.Key("ghm_root_desc"); writer.String(ghm_root_desc->ghm_root_desc);
             }
-            if (ghm_root_info.young_severity_limit) {
-                writer.Key("young_age_treshold"); writer.Int(ghm_root_info.young_age_treshold);
-                writer.Key("young_severity_limit"); writer.Int(ghm_root_info.young_severity_limit);
-            }
-            if (ghm_root_info.old_severity_limit) {
-                writer.Key("old_age_treshold"); writer.Int(ghm_root_info.old_age_treshold);
-                writer.Key("old_severity_limit"); writer.Int(ghm_root_info.old_severity_limit);
-            }
-            writer.Key("ghs"); writer.Int(ghs_pricing->ghs.number);
+            writer.Key("ghs"); writer.StartArray();
 
-            writer.Key("conditions"); writer.StartArray();
-            if (ghs_info.bed_authorization) {
-                writer.String(Fmt(&temp_alloc, "Autorisation Lit %1", ghs_info.bed_authorization).ptr);
-            }
-            if (ghs_info.unit_authorization) {
-                writer.String(Fmt(&temp_alloc, "Autorisation Unité %1", ghs_info.unit_authorization).ptr);
-                if (ghs_info.minimal_duration) {
-                    writer.String(Fmt(&temp_alloc, "Durée Unitée Autorisée ≥ %1", ghs_info.minimal_duration).ptr);
+            Span<const GhsInfo> compatible_ghs = index->FindCompatibleGhs(ghm_root_info.ghm_root);
+            for (const GhsInfo &ghs_info: compatible_ghs) {
+                const GhmConstraint *constraint = constraints ? constraints->Find(ghs_info.ghm) : nullptr;
+//              if (!constraint)
+//                  continue;
+                const GhsPricing *ghs_pricing = pricing_set->FindGhsPricing(ghs_info.ghs[0], date);
+                if (!ghs_pricing)
+                    continue;
+
+                writer.StartObject();
+                writer.Key("ghm"); writer.String(Fmt(buf, "%1", ghs_info.ghm).ptr);
+                writer.Key("ghm_mode"); writer.String(&ghs_info.ghm.parts.mode, 1);
+                if (constraint) {
+                    writer.Key("duration_mask"); writer.Uint(constraint->duration_mask);
+                } else {
+                    writer.Key("duration_mask"); writer.Uint(UINT_MAX);
                 }
-            } else if (ghs_info.minimal_duration) {
-                // TODO: Take into account in addition to constraints (when we plug them in)
-                writer.String(Fmt(&temp_alloc, "Durée ≥ %1", ghs_info.minimal_duration).ptr);
-            }
-            if (ghs_info.minimal_age) {
-                writer.String(Fmt(&temp_alloc, "Age ≥ %1", ghs_info.minimal_age).ptr);
-            }
-            if (ghs_info.main_diagnosis_mask.value) {
-                writer.String(Fmt(&temp_alloc, "DP de la liste D$%1.%2",
-                                  ghs_info.main_diagnosis_mask.offset, ghs_info.main_diagnosis_mask.value).ptr);
-            }
-            if (ghs_info.diagnosis_mask.value) {
-                writer.String(Fmt(&temp_alloc, "Diagnostic de la liste D$%1.%2",
-                                  ghs_info.diagnosis_mask.offset, ghs_info.diagnosis_mask.value).ptr);
-            }
-            for (const ListMask &mask: ghs_info.procedure_masks) {
-                writer.String(Fmt(&temp_alloc, "Acte de la liste A$%1.%2",
-                                  mask.offset, mask.value).ptr);
+                if (ghm_root_info.young_severity_limit) {
+                    writer.Key("young_age_treshold"); writer.Int(ghm_root_info.young_age_treshold);
+                    writer.Key("young_severity_limit"); writer.Int(ghm_root_info.young_severity_limit);
+                }
+                if (ghm_root_info.old_severity_limit) {
+                    writer.Key("old_age_treshold"); writer.Int(ghm_root_info.old_age_treshold);
+                    writer.Key("old_severity_limit"); writer.Int(ghm_root_info.old_severity_limit);
+                }
+                writer.Key("ghs"); writer.Int(ghs_pricing->ghs.number);
+
+                writer.Key("conditions"); writer.StartArray();
+                if (ghs_info.bed_authorization) {
+                    writer.String(Fmt(buf, "Autorisation Lit %1", ghs_info.bed_authorization).ptr);
+                }
+                if (ghs_info.unit_authorization) {
+                    writer.String(Fmt(buf, "Autorisation Unité %1", ghs_info.unit_authorization).ptr);
+                    if (ghs_info.minimal_duration) {
+                        writer.String(Fmt(buf, "Durée Unitée Autorisée ≥ %1", ghs_info.minimal_duration).ptr);
+                    }
+                } else if (ghs_info.minimal_duration) {
+                    // TODO: Take into account in addition to constraints (when we plug them in)
+                    writer.String(Fmt(buf, "Durée ≥ %1", ghs_info.minimal_duration).ptr);
+                }
+                if (ghs_info.minimal_age) {
+                    writer.String(Fmt(buf, "Age ≥ %1", ghs_info.minimal_age).ptr);
+                }
+                if (ghs_info.main_diagnosis_mask.value) {
+                    writer.String(Fmt(buf, "DP de la liste D$%1.%2",
+                                      ghs_info.main_diagnosis_mask.offset, ghs_info.main_diagnosis_mask.value).ptr);
+                }
+                if (ghs_info.diagnosis_mask.value) {
+                    writer.String(Fmt(buf, "Diagnostic de la liste D$%1.%2",
+                                      ghs_info.diagnosis_mask.offset, ghs_info.diagnosis_mask.value).ptr);
+                }
+                for (const ListMask &mask: ghs_info.procedure_masks) {
+                    writer.String(Fmt(buf, "Acte de la liste A$%1.%2",
+                                      mask.offset, mask.value).ptr);
+                }
+                writer.EndArray();
+
+                writer.Key("price_cents"); writer.Int(ghs_pricing->sectors[0].price_cents);
+                if (ghs_pricing->sectors[0].exh_treshold) {
+                    writer.Key("exh_treshold"); writer.Int(ghs_pricing->sectors[0].exh_treshold);
+                    writer.Key("exh_cents"); writer.Int(ghs_pricing->sectors[0].exh_cents);
+                }
+                if (ghs_pricing->sectors[0].exb_treshold) {
+                    writer.Key("exb_treshold"); writer.Int(ghs_pricing->sectors[0].exb_treshold);
+                    writer.Key("exb_cents"); writer.Int(ghs_pricing->sectors[0].exb_cents);
+                    if (ghs_pricing->sectors[0].flags & (int)GhsPricing::Flag::ExbOnce) {
+                        writer.Key("exb_once"); writer.Bool(true);
+                    }
+                }
+
+                writer.EndObject();
             }
             writer.EndArray();
-
-            writer.Key("price_cents"); writer.Int(ghs_pricing->sectors[0].price_cents);
-            if (ghs_pricing->sectors[0].exh_treshold) {
-                writer.Key("exh_treshold"); writer.Int(ghs_pricing->sectors[0].exh_treshold);
-                writer.Key("exh_cents"); writer.Int(ghs_pricing->sectors[0].exh_cents);
-            }
-            if (ghs_pricing->sectors[0].exb_treshold) {
-                writer.Key("exb_treshold"); writer.Int(ghs_pricing->sectors[0].exb_treshold);
-                writer.Key("exb_cents"); writer.Int(ghs_pricing->sectors[0].exb_cents);
-                if (ghs_pricing->sectors[0].flags & (int)GhsPricing::Flag::ExbOnce) {
-                    writer.Key("exb_once"); writer.Bool(true);
-                }
-            }
-
             writer.EndObject();
         }
         writer.EndArray();
-        writer.EndObject();
-    }
-    writer.EndArray();
 
-    return true;
+        return true;
+    });
+
+    return response;
 }
 
-// TODO: Deny if URL too long (MHD option?)
-static int HandleHttpConnection(void *, struct MHD_Connection *conn,
-                                const char *url, const char *,
-                                const char *, const char *,
-                                size_t *, void **)
+static MHD_Response *ProduceGhmRoots(MHD_Connection *, const char *,
+                                     CompressionType compression_type)
 {
-    const char *const error_page = "<html><body>Error</body></html>";
+    MHD_Response *response = BuildJson(compression_type,
+                                       [&](rapidjson::PrettyWriter<JsonStreamWriter> &writer) {
+        char buf[32];
 
-    MHD_Response *response = nullptr;
-    unsigned int code = MHD_HTTP_INTERNAL_SERVER_ERROR;
-
-    if (TestStr(url, "/api/price_map.json")) {
-        Date date = {};
-        {
-            const char *date_str = MHD_lookup_connection_value(conn, MHD_GET_ARGUMENT_KIND,
-                                                               "date");
-            date = Date::FromString(date_str);
-        }
-
-        if (date.value) {
-            rapidjson::MemoryBuffer buffer;
-            if (BuildCatalog(date, &buffer)) {
-                response = MHD_create_response_from_buffer(buffer.GetSize(),
-                                                           (void *)buffer.GetBuffer(),
-                                                           MHD_RESPMEM_MUST_COPY);
-                MHD_add_response_header(response, "Content-Type", "application/json");
-                code = MHD_HTTP_OK;
-            }
-        }
-    } else if (TestStr(url, "/api/ghm_roots.json")) {
-        Allocator temp_alloc;
-
-        rapidjson::MemoryBuffer buffer;
-        rapidjson::PrettyWriter<rapidjson::MemoryBuffer> writer(buffer);
         writer.StartArray();
         for (const GhmRootDesc &desc: catalog_set->ghm_roots) {
             writer.StartObject();
-            writer.Key("ghm_root"); writer.String(Fmt(&temp_alloc, "%1", desc.ghm_root).ptr);
+            writer.Key("ghm_root"); writer.String(Fmt(buf, "%1", desc.ghm_root).ptr);
             writer.Key("ghm_root_desc"); writer.String(desc.ghm_root_desc);
             writer.Key("da"); writer.String(desc.da);
             writer.Key("da_desc"); writer.String(desc.da_desc);
@@ -271,54 +284,101 @@ static int HandleHttpConnection(void *, struct MHD_Connection *conn,
             writer.EndObject();
         }
         writer.EndArray();
-        response = MHD_create_response_from_buffer(buffer.GetSize(),
-                                                   (void *)buffer.GetBuffer(),
-                                                   MHD_RESPMEM_MUST_COPY);
-        MHD_add_response_header(response, "Content-Type", "application/json");
-        code = MHD_HTTP_OK;
-    } else if (TestStr(url, "/api/pages.json")) {
-        rapidjson::MemoryBuffer buffer;
-        rapidjson::PrettyWriter<rapidjson::MemoryBuffer> writer(buffer);
-        writer.StartArray();
-        for (Size i = 0; i < ARRAY_SIZE(pages);) {
-            writer.StartObject();
-            writer.Key("category"); writer.String(pages[i].category);
-            writer.Key("pages"); writer.StartArray();
-            Size j = i;
-            for (; j < ARRAY_SIZE(pages) && pages[j].category == pages[i].category; j++) {
-                writer.StartObject();
-                writer.Key("url"); writer.String(pages[j].url + 1);
-                writer.Key("name"); writer.Key(pages[j].name);
-                writer.EndObject();
-            }
-            i = j;
-            writer.EndArray();
-            writer.EndObject();
-        }
-        writer.EndArray();
-        response = MHD_create_response_from_buffer(buffer.GetSize(),
-                                                   (void *)buffer.GetBuffer(),
-                                                   MHD_RESPMEM_MUST_COPY);
-        MHD_add_response_header(response, "Content-Type", "application/json");
-        code = MHD_HTTP_OK;
-    } else {
+
+        return true;
+    });
+
+    return response;
+}
+
+static MHD_Response *ProducePages(MHD_Connection *, const char *,
+                                  CompressionType compression_type)
+{
+    MHD_Response *response = BuildJson(compression_type,
+                                       [&](rapidjson::PrettyWriter<JsonStreamWriter> &writer) {
+       writer.StartArray();
+       for (Size i = 0; i < ARRAY_SIZE(pages);) {
+           writer.StartObject();
+           writer.Key("category"); writer.String(pages[i].category);
+           writer.Key("pages"); writer.StartArray();
+           Size j = i;
+           for (; j < ARRAY_SIZE(pages) && pages[j].category == pages[i].category; j++) {
+               writer.StartObject();
+               writer.Key("url"); writer.String(pages[j].url + 1);
+               writer.Key("name"); writer.Key(pages[j].name);
+               writer.EndObject();
+           }
+           i = j;
+           writer.EndArray();
+           writer.EndObject();
+       }
+       writer.EndArray();
+
+       return true;
+    });
+
+    return response;
+}
+
+static MHD_Response *ProduceStaticResource(MHD_Connection *, const char *url, CompressionType)
+{
+    // TODO: Support compression for static resources
+
 #if !defined(NDEBUG) && defined(_WIN32)
-        UpdateStaticResources();
+    UpdateStaticResources();
 #endif
 
-        Span<const uint8_t> resource_data = routes.FindValue(url, {});
-        if (resource_data.IsValid()) {
-            response = MHD_create_response_from_buffer((size_t)resource_data.len,
-                                                       (void *)resource_data.ptr,
-                                                       MHD_RESPMEM_PERSISTENT);
-            code = MHD_HTTP_OK;
-        } else {
-            code = MHD_HTTP_NOT_FOUND;
+    Span<const uint8_t> resource_data = routes.FindValue(url, {});
+    if (!resource_data.IsValid())
+        return nullptr;
+
+    MHD_Response *response = MHD_create_response_from_buffer((size_t)resource_data.len,
+                                                             (void *)resource_data.ptr,
+                                                             MHD_RESPMEM_PERSISTENT);
+    return response;
+}
+
+// TODO: Deny if URL too long (MHD option?)
+static int HandleHttpConnection(void *, MHD_Connection *conn,
+                                const char *url, const char *,
+                                const char *, const char *,
+                                size_t *, void **)
+{
+    const char *const error_page = "<html><body>Internal Server Error</body></html>";
+
+    CompressionType compression_type = CompressionType::None;
+    {
+        Span<const char> encodings = MHD_lookup_connection_value(conn, MHD_HEADER_KIND, "Accept-Encoding");
+        while (encodings.len) {
+            Span<const char> encoding = TrimStr(SplitStr(encodings, ',', &encodings));
+            if (encoding == "gzip") {
+                compression_type = CompressionType::Gzip;
+                break;
+            } else if (encoding == "deflate") {
+                compression_type = CompressionType::Zlib;
+                break;
+            }
         }
     }
-    if (!response) {
+
+    MHD_Response *response = nullptr;
+    if (TestStr(url, "/api/price_map.json")) {
+        response = ProducePriceMap(conn, url, compression_type);
+    } else if (TestStr(url, "/api/ghm_roots.json")) {
+        response = ProduceGhmRoots(conn, url, compression_type);
+    } else if (TestStr(url, "/api/pages.json")) {
+        response = ProducePages(conn, url, compression_type);
+    } else {
+        response = ProduceStaticResource(conn, url, compression_type);
+    }
+
+    unsigned int code;
+    if (response) {
+        code = MHD_HTTP_OK;
+    } else {
         response = MHD_create_response_from_buffer(strlen(error_page), (void *)error_page,
                                                    MHD_RESPMEM_PERSISTENT);
+        code = MHD_HTTP_INTERNAL_SERVER_ERROR;
     }
     DEFER { MHD_destroy_response(response); };
 
