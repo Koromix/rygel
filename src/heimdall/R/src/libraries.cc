71f30e6816c66f5ff3a840a1c4d4c65103701ac3
Niels Martignène <niels.martignene@protonmail.com>
2017-12-15 15:58:15 +0100

    Add missing LICENSE header in some files

--- src/heimdall/R/src/libraries.cc
+++ src/heimdall/R/src/libraries.cc
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+   License, v. 2.0. If a copy of the MPL was not distributed with this
+   file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 #ifdef _WIN32
     #define WINVER 0x0602
     #define _WIN32_WINNT 0x0602
