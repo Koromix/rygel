c75704ef69675f3d7f4027c1086a3cb3fcf00a04
Niels Martignène <niels.martignene@protonmail.com>
2017-12-20 23:04:05 +0100

    Switch to C++-style comments for LICENSE notice
    Turns out, the RcppExports generator really does not like
    C-style comments. For consistency I changed the notice
    in all source files, not just the Rcpp ones.

--- src/moya/R/R/moyaR.R
+++ src/moya/R/R/moyaR.R
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 summary_columns <- c('ghs_price', 'rea', 'reasi', 'si',
                      'src', 'nn1', 'nn2', 'nn3', 'rep')
 
