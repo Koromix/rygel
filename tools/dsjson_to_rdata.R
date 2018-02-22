f8c28265ae0a0751755365549dd6c71fdb8384d9
Niels Martignène <niels.martignene@protonmail.com>
2018-02-22 08:08:32 +0100

    Add dsjson-to-Rdata script to repository

---
+++ tools/dsjson_to_rdata.R
@@ -0,0 +1,56 @@
+library(jsonlite)
+library(data.table)
+
+stays <- fromJSON("V:/chu/mco/rss_2016.dsjson", flatten = TRUE)
+stays <- data.table(stays)
+
+# save(stays, file = 'V:/test.RData')
+
+stays[, `:=`(
+    test.cluster_len = NULL,
+    test.error = NULL,
+    test.ghm = NULL,
+    test.ghs = NULL,
+    test.nn1 = NULL,
+    test.nn2 = NULL,
+    test.nn3 = NULL,
+    test.rea = NULL,
+    test.reasi = NULL,
+    test.rep = NULL,
+    test.si = NULL,
+    test.src = NULL
+)]
+setnames(stays, c('dp', 'dr'), c('main_diagnosis', 'linked_diagnosis'))
+
+# save(stays, file = 'V:/test2.RData')
+
+diagnoses <- rbindlist(lapply(1:nrow(stays), function(i) {
+    das <- unlist(stays$das[i])
+    if (length(das) > 0) {
+        data.table(
+            id = i,
+            diag = das
+        )
+    } else {
+        NULL
+    }
+}))
+procedures <- rbindlist(lapply(1:nrow(stays), function(i) {
+    procedures <- stays$procedures[i][[1]]
+    if (nrow(procedures) > 0) {
+        procedures$id <- i
+        as.data.table(procedures)
+    } else {
+        NULL
+    }
+}))
+
+# save(stays, file = 'V:/test3.RData')
+
+stays[, `:=`(
+    id = 1:nrow(stays),
+    das = NULL,
+    procedures = NULL
+)]
+
+save(stays, diagnoses, procedures, file = 'V:/test4.RData')
