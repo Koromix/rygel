7cff3c362894bfc74f26d6a9c2e57cc1f98de2a7
Niels Martignène <niels.martignene@protonmail.com>
2018-12-09 18:05:57 +0100

    Add comment about libmicrohttpd thread pool bug on MSVC builds

--- src/thop/server/thop.cc
+++ src/thop/server/thop.cc
@@ -975,7 +975,11 @@ Options:
         mhd_options.Append({MHD_OPTION_NOTIFY_COMPLETED, (intptr_t)ReleaseConnectionData, nullptr});
         if (thop_config.pool_size > 1) {
 #ifdef _MSC_VER
-            // FIXME: Investigate why libmicrohttpd in thread pool mode works badly on MSVC builds
+            // In thread pool mode, each libmicrohttpd thread polls the same listening socket
+            // descriptor, set to non-blocking mode. They all call accept() but only one will
+            // succeed for each client. On MSVC builds, this does not seem to work correctly and
+            // some threads get stuck in accept() while others get WSAEWOULDBLOCK as expected.
+            // For now, work around this with MHD_USE_THREAD_PER_CONNECTION.
             LogError("Cannot use libmicrohttpd thread pool on MSVC builds");
             flags |= MHD_USE_THREAD_PER_CONNECTION;
 #else
