fa174be6ad6b2d5e5deedd6984a5efc4004f1a7f
Niels Martignène <niels.martignene@protonmail.com>
2019-03-05 09:54:02 +0100

    Move most felix code to dedicated files

--- src/felix/felix.cc
+++ src/felix/felix.cc
@@ -2,456 +2,10 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-#ifndef _WIN32
-    #include <unistd.h>
-#endif
-
 #include "../libcc/libcc.hh"
+#include "build.hh"
 #include "config.hh"
 #include "toolchain.hh"
-
-struct TargetData {
-    const char *name;
-    TargetType type;
-
-    Span<const char *const> include_directories;
-    HeapArray<const char *> libraries;
-
-    HeapArray<ObjectInfo> pch_objects;
-    const char *c_pch_filename;
-    const char *cxx_pch_filename;
-
-    HeapArray<ObjectInfo> objects;
-};
-
-struct TargetSet {
-    HeapArray<TargetData> targets;
-    HashMap<const char *, Size> targets_map;
-
-    BlockAllocator str_alloc;
-
-    TargetData *CreateTarget(const Config &config, const char *target_name,
-                             const char *output_directory);
-};
-
-struct BuildCommand {
-    const char *type;
-
-    const char *dest_filename;
-    const char *cmd;
-
-    bool sync_after;
-};
-
-struct BuildSet {
-    HeapArray<BuildCommand> commands;
-    HashSet<const char *> commands_set;
-
-    BlockAllocator str_alloc;
-};
-
-static int64_t GetFileModificationTime(const char *filename)
-{
-    FileInfo file_info;
-    if (!StatFile(filename, false, &file_info))
-        return -1;
-
-    return file_info.modification_time;
-}
-
-static const char *BuildOutputPath(const char *src_filename, const char *output_directory,
-                                   const char *suffix, Allocator *alloc)
-{
-    DebugAssert(!PathIsAbsolute(src_filename));
-
-    HeapArray<char> buf;
-    buf.allocator = alloc;
-
-    Size offset = Fmt(&buf, "%1%/objects%/", output_directory).len;
-    Fmt(&buf, "%1%2", src_filename, suffix);
-
-    // Replace '..' components with '__'
-    {
-        char *ptr = buf.ptr + offset;
-
-        while ((ptr = strstr(ptr, ".."))) {
-            if (IsPathSeparator(ptr[-1]) && (IsPathSeparator(ptr[2]) || !ptr[2])) {
-                ptr[0] = '_';
-                ptr[1] = '_';
-            }
-
-            ptr += 2;
-        }
-    }
-
-    return buf.Leak().ptr;
-}
-
-// TODO: Support Make escaping
-static bool ParseCompilerMakeRule(const char *filename, Allocator *alloc,
-                                  HeapArray<const char *> *out_filenames)
-{
-    HeapArray<char> rule_buf;
-    if (ReadFile(filename, Megabytes(2), &rule_buf) < 0)
-        return false;
-    rule_buf.Append(0);
-
-    // Skip output path
-    Span<const char> rule;
-    {
-        const char *ptr = strstr(rule_buf.ptr, ": ");
-        if (ptr) {
-            rule = Span<const char>(ptr + 2);
-        } else {
-            rule = {};
-        }
-    }
-
-    while (rule.len) {
-        Span<const char> path = TrimStr(SplitStr(rule, ' ', &rule));
-
-        if (path.len && path != "\\") {
-            const char *dep_filename = NormalizePath(path, alloc).ptr;
-            out_filenames->Append(dep_filename);
-        }
-    }
-
-    return true;
-}
-
-static bool IsFileUpToDate(const char *dest_filename, Span<const char *const> src_filenames)
-{
-    int64_t dest_time = GetFileModificationTime(dest_filename);
-
-    for (const char *src_filename: src_filenames) {
-        int64_t src_time = GetFileModificationTime(src_filename);
-        if (src_time < 0 || src_time > dest_time)
-            return false;
-    }
-
-    return true;
-}
-
-static bool EnsureDirectoryExists(const char *filename)
-{
-    Span<const char> directory;
-    SplitStrReverseAny(filename, PATH_SEPARATORS, &directory);
-
-    return MakeDirectoryRec(directory);
-}
-
-static bool CreatePrecompileHeader(const char *pch_filename, const char *dest_filename)
-{
-    if (!EnsureDirectoryExists(dest_filename))
-        return false;
-
-    StreamWriter writer(dest_filename);
-    Print(&writer, "#include \"%1%/%2\"", GetWorkingDirectory(), pch_filename);
-    return writer.Close();
-}
-
-TargetData *TargetSet::CreateTarget(const Config &config, const char *target_name,
-                                    const char *output_directory)
-{
-    BlockAllocator temp_alloc;
-
-    const TargetConfig *target_config = config.targets_map.FindValue(target_name, nullptr);
-    if (!target_config) {
-        LogError("Could not find target '%1'", target_name);
-        return nullptr;
-    }
-
-    // Gather target objects
-    HeapArray<ObjectInfo> objects;
-    {
-        HeapArray<const char *> src_filenames;
-        for (const char *src_directory: target_config->src_directories) {
-            if (!EnumerateDirectoryFiles(src_directory, nullptr, 1024, &temp_alloc, &src_filenames))
-                return nullptr;
-        }
-        src_filenames.Append(target_config->src_filenames);
-
-        for (const char *src_filename: src_filenames) {
-            const char *name = SplitStrReverseAny(src_filename, PATH_SEPARATORS).ptr;
-            bool ignore = std::any_of(target_config->exclusions.begin(), target_config->exclusions.end(),
-                                      [&](const char *excl) { return MatchPathName(name, excl); });
-
-            if (!ignore) {
-                ObjectInfo obj = {};
-
-                Span<const char> extension = GetPathExtension(src_filename);
-                if (extension == ".c") {
-                    obj.src_type = SourceType::C_Source;
-                } else if (extension == ".cc" || extension == ".cpp") {
-                    obj.src_type = SourceType::CXX_Source;
-                } else {
-                    continue;
-                }
-
-                obj.src_filename = DuplicateString(src_filename, &str_alloc).ptr;
-                obj.dest_filename = BuildOutputPath(src_filename, output_directory, ".o", &str_alloc);
-
-                objects.Append(obj);
-            }
-        }
-    }
-
-    // Target libraries
-    HeapArray<const char *> libraries;
-    libraries.Append(target_config->libraries);
-
-    // Resolve imported objects and libraries
-    for (const char *import_name: target_config->imports) {
-        const TargetData *import;
-        {
-            Size import_idx = targets_map.FindValue(import_name, -1);
-            if (import_idx >= 0) {
-                import = &targets[import_idx];
-            } else {
-                import = CreateTarget(config, import_name, output_directory);
-                if (!import)
-                    return nullptr;
-            }
-        }
-
-        objects.Append(import->objects);
-        libraries.Append(import->libraries);
-    }
-
-    // Deduplicate object and library arrays
-    std::sort(objects.begin(), objects.end(), [](const ObjectInfo &obj1, const ObjectInfo &obj2) {
-        return CmpStr(obj1.dest_filename, obj2.dest_filename) < 0;
-    });
-    objects.RemoveFrom(std::unique(objects.begin(), objects.end(),
-                                   [](const ObjectInfo &obj1, const ObjectInfo &obj2) {
-        return TestStr(obj1.dest_filename, obj2.dest_filename);
-    }) - objects.begin());
-    std::sort(libraries.begin(), libraries.end(), [](const char *lib1, const char *lib2) {
-        return CmpStr(lib1, lib2) < 0;
-    });
-    libraries.RemoveFrom(std::unique(libraries.begin(), libraries.end(),
-                                   [](const char *lib1, const char *lib2) {
-        return TestStr(lib1, lib2);
-    }) - libraries.begin());
-
-    // PCH files
-    HeapArray<ObjectInfo> pch_objects;
-    const char *c_pch_filename = nullptr;
-    const char *cxx_pch_filename = nullptr;
-    if (target_config->c_pch_filename) {
-        ObjectInfo obj = {};
-
-        obj.src_type = SourceType::C_Header;
-        obj.src_filename = target_config->c_pch_filename;
-        obj.dest_filename = BuildOutputPath(target_config->c_pch_filename, output_directory,
-                                            ".pch.h", &str_alloc);
-        c_pch_filename = obj.dest_filename;
-
-        pch_objects.Append(obj);
-    }
-    if (target_config->cxx_pch_filename) {
-        ObjectInfo obj = {};
-
-        obj.src_type = SourceType::CXX_Header;
-        obj.src_filename = target_config->cxx_pch_filename;
-        obj.dest_filename = BuildOutputPath(target_config->cxx_pch_filename, output_directory,
-                                            ".pch.h", &str_alloc);
-        cxx_pch_filename = obj.dest_filename;
-
-        pch_objects.Append(obj);
-    }
-
-    // Big type, so create it directly in HeapArray
-    // Introduce out_guard if things can start to fail after here
-    TargetData *target = targets.AppendDefault();
-
-    target->name = target_config->name;
-    target->type = target_config->type;
-    target->include_directories = target_config->include_directories;
-    std::swap(target->libraries, libraries);
-    std::swap(target->pch_objects, pch_objects);
-    target->c_pch_filename = c_pch_filename;
-    target->cxx_pch_filename = cxx_pch_filename;
-    std::swap(target->objects, objects);
-
-    bool appended = targets_map.Append(target_config->name, targets.len - 1).second;
-    DebugAssert(appended);
-
-    return target;
-}
-
-static bool AppendTargetCommands(const TargetData &target, const char *output_directory,
-                                 const Toolchain &toolchain, BuildMode build_mode,
-                                 BuildSet *out_set)
-{
-    const Size start_len = out_set->commands.len;
-    DEFER_N(out_guard) { out_set->commands.RemoveFrom(start_len); };
-
-    BlockAllocator temp_alloc;
-
-    // Reuse for performance
-    HeapArray<const char *> src_filenames;
-
-    // Precompiled headers
-    for (const ObjectInfo &obj: target.pch_objects) {
-        const char *deps_filename = Fmt(&temp_alloc, "%1.d", obj.dest_filename).ptr;
-
-        src_filenames.RemoveFrom(0);
-        src_filenames.Append(obj.src_filename);
-
-        // Parse Make rule dependencies
-        bool build = false;
-        if (TestFile(deps_filename, FileType::File)) {
-            if (!ParseCompilerMakeRule(deps_filename, &temp_alloc, &src_filenames))
-                return false;
-
-            build = !IsFileUpToDate(obj.dest_filename, src_filenames);
-        } else {
-            build = true;
-        }
-        build &= !out_set->commands_set.Find(obj.dest_filename);
-
-        if (build) {
-            BuildCommand cmd = {};
-
-            cmd.type = "Precompile";
-            cmd.dest_filename = DuplicateString(obj.dest_filename, &out_set->str_alloc).ptr;
-            if (!CreatePrecompileHeader(obj.src_filename, obj.dest_filename))
-                return false;
-            cmd.cmd = toolchain.BuildObjectCommand(obj.dest_filename, obj.src_type, build_mode, nullptr,
-                                                   target.include_directories, nullptr,
-                                                   deps_filename, &out_set->str_alloc);
-
-            out_set->commands.Append(cmd);
-        }
-    }
-    if (out_set->commands.len > start_len) {
-        out_set->commands[out_set->commands.len - 1].sync_after = true;
-    }
-
-    // Object commands
-    bool relink = false;
-    for (const ObjectInfo &obj: target.objects) {
-        const char *deps_filename = Fmt(&temp_alloc, "%1.d", obj.dest_filename).ptr;
-
-        src_filenames.RemoveFrom(0);
-        src_filenames.Append(obj.src_filename);
-
-        // Parse Make rule dependencies
-        bool build = false;
-        if (TestFile(deps_filename, FileType::File)) {
-            if (!ParseCompilerMakeRule(deps_filename, &temp_alloc, &src_filenames))
-                return false;
-
-            build = !IsFileUpToDate(obj.dest_filename, src_filenames);
-        } else {
-            build = true;
-        }
-        build &= !out_set->commands_set.Find(obj.dest_filename);
-        relink |= build;
-
-        if (build) {
-            BuildCommand cmd = {};
-
-            const char *pch_filename = nullptr;
-            switch (obj.src_type) {
-                case SourceType::C_Source: { pch_filename = target.c_pch_filename; } break;
-                case SourceType::CXX_Source: { pch_filename = target.cxx_pch_filename; } break;
-
-                case SourceType::C_Header:
-                case SourceType::CXX_Header: { DebugAssert(false); } break;
-            }
-
-            cmd.type = "Build";
-            cmd.dest_filename = DuplicateString(obj.dest_filename, &out_set->str_alloc).ptr;
-            if (!EnsureDirectoryExists(obj.dest_filename))
-                return false;
-            cmd.cmd = toolchain.BuildObjectCommand(obj.src_filename, obj.src_type, build_mode,
-                                                   pch_filename, target.include_directories,
-                                                   obj.dest_filename, deps_filename, &out_set->str_alloc);
-
-            out_set->commands.Append(cmd);
-        }
-    }
-    if (out_set->commands.len > start_len) {
-        out_set->commands[out_set->commands.len - 1].sync_after = true;
-    }
-
-    // Link commands
-    if (target.type == TargetType::Executable) {
-#ifdef _WIN32
-        const char *link_filename = Fmt(&out_set->str_alloc, "%1%/%2.exe",
-                                        output_directory, target.name).ptr;
-#else
-        const char *link_filename = Fmt(&out_link_set->str_alloc, "%1%/%2",
-                                        output_directory, target_config.name).ptr;
-#endif
-
-        relink &= !out_set->commands_set.Find(link_filename);
-
-        if (relink || !TestFile(link_filename, FileType::File)) {
-            BuildCommand cmd = {};
-
-            cmd.type = "Link";
-            cmd.dest_filename = link_filename;
-            cmd.cmd = toolchain.BuildLinkCommand(target.objects, target.libraries, link_filename,
-                                                &out_set->str_alloc);
-
-            out_set->commands.Append(cmd);
-        }
-    }
-    if (out_set->commands.len > start_len) {
-        out_set->commands[out_set->commands.len - 1].sync_after = true;
-    }
-
-    // Do this at the end because it's much harder to roll back changes in out_guard
-    for (Size i = start_len; i < out_set->commands.len; i++) {
-        out_set->commands_set.Append(out_set->commands[i].dest_filename);
-    }
-
-    out_guard.disable();
-    return true;
-}
-
-static bool RunBuildCommands(Span<const BuildCommand> commands, bool verbose)
-{
-    Async async;
-
-    static std::atomic_int progress_counter;
-    progress_counter = 0;
-
-    for (const BuildCommand &cmd: commands) {
-        async.AddTask([&, cmd]() {
-            int progress = 100 * (progress_counter += 1) / commands.len;
-
-            if (verbose) {
-                LogInfo("[%1%%] %2", FmtArg(progress).Pad(-3), cmd.cmd);
-            } else {
-                const char *name = SplitStrReverseAny(cmd.dest_filename, PATH_SEPARATORS).ptr;
-                LogInfo("[%1%%] %2 %3", FmtArg(progress).Pad(-3), cmd.type, name);
-            }
-
-            // Run command
-            errno = 0;
-            if (system(cmd.cmd) || errno) {
-                LogError("Command '%1' failed", cmd.cmd);
-#ifdef _WIN32
-                _unlink(cmd.dest_filename);
-#else
-                unlink(cmd.dest_filename);
-#endif
-                return false;
-            }
-
-            return true;
-        });
-
-        if (cmd.sync_after && !async.Sync())
-            return false;
-    }
-
-    return async.Sync();
-}
 
 int main(int argc, char **argv)
 {
@@ -561,6 +115,7 @@ Available build modes:)");
         }
     }
 
+    // Disable PCH?
 #ifdef _WIN32
     if (!disable_pch && toolchain == &GnuToolchain) {
         bool has_pch = std::any_of(config.targets.begin(), config.targets.end(),
@@ -601,32 +156,50 @@ Available build modes:)");
         }
     }
 
-    // Gather targets
+    // Gather target information
     TargetSet target_set;
     {
-        HashSet<const char *> handled_set;
+        TargetSetBuilder target_set_builder(output_directory);
+        target_set_builder.resolve_import = [&](const char *import_name) {
+            const TargetConfig *import_config = config.targets_map.FindValue(import_name, nullptr);
+            return import_config;
+        };
 
+        HashSet<const char *> handled_set;
         for (const char *target_name: target_names) {
             if (!handled_set.Append(target_name).second) {
                 LogError("Target '%1' is specified multiple times", target_name);
                 return 1;
             }
 
-            if (!target_set.targets_map.Find(target_name) &&
-                    !target_set.CreateTarget(config, target_name, output_directory))
+            const TargetConfig *target_config = config.targets_map.FindValue(target_name, nullptr);
+            if (!target_config) {
+                LogError("Target '%1' does not exist", target_name);
+                return 1;
+            }
+
+            if (!target_set_builder.CreateTarget(*target_config))
                 return 1;
         }
+
+        target_set_builder.Finish(&target_set);
     }
 
     // Create build commands
-    BuildSet command_set;
-    for (const TargetData &target: target_set.targets) {
-        if (!AppendTargetCommands(target, output_directory, *toolchain, build_mode, &command_set))
-            return 1;
+    BuildSet build_set;
+    {
+        BuildSetBuilder build_set_builder(toolchain, build_mode, output_directory);
+
+        for (const TargetData &target: target_set.targets) {
+            if (!build_set_builder.AppendTargetCommands(target))
+                return 1;
+        }
+
+        build_set_builder.Finish(&build_set);
     }
 
     // Run build
-    if (!RunBuildCommands(command_set.commands, verbose))
+    if (!RunBuildCommands(build_set.commands, verbose))
         return 1;
 
     LogInfo("Done!");
---
+++ src/felix/target.hh
@@ -0,0 +1,72 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#pragma once
+
+#include "../libcc/libcc.hh"
+#include "toolchain.hh"
+
+enum class TargetType {
+    Executable,
+    Library
+};
+
+struct TargetConfig {
+    const char *name;
+    TargetType type;
+
+    HeapArray<const char *> src_directories;
+    HeapArray<const char *> src_filenames;
+    HeapArray<const char *> exclusions;
+
+    const char *c_pch_filename;
+    const char *cxx_pch_filename;
+
+    HeapArray<const char *> imports;
+    HeapArray<const char *> include_directories;
+    HeapArray<const char *> libraries;
+
+    HASH_TABLE_HANDLER(TargetConfig, name);
+};
+
+struct TargetData {
+    const char *name;
+    TargetType type;
+
+    Span<const char *const> include_directories;
+    HeapArray<const char *> libraries;
+
+    HeapArray<ObjectInfo> pch_objects;
+    const char *c_pch_filename;
+    const char *cxx_pch_filename;
+
+    HeapArray<ObjectInfo> objects;
+
+    HASH_TABLE_HANDLER(TargetData, name);
+};
+
+struct TargetSet {
+    HeapArray<TargetData> targets;
+    HashTable<const char *, const TargetData *> targets_map;
+
+    BlockAllocator str_alloc;
+};
+
+class TargetSetBuilder {
+    const char *output_directory;
+
+    TargetSet set;
+
+    HashMap<const char *, Size> targets_map;
+
+public:
+    std::function<const TargetConfig *(const char *)> resolve_import;
+
+    TargetSetBuilder(const char *output_directory)
+        : output_directory(output_directory) {}
+
+    const TargetData *CreateTarget(const TargetConfig &target_config);
+
+    void Finish(TargetSet *out_set);
+};
