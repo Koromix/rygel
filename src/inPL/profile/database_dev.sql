a2448eec36eff1db0385b28dabae3a705cac58a1
Niels Martignène <niels.martignene@protonmail.com>
2019-04-13 15:48:14 +0200

    Define basic SQL schema for inPL schedule

--- src/inPL/profile/database_dev.sql
+++ src/inPL/profile/database_dev.sql
@@ -2,12 +2,45 @@
    License, v. 2.0. If a copy of the MPL was not distributed with this
    file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-CREATE TABLE gp_values (
+-------------------- Structure --------------------
+
+CREATE TABLE sc_activities (
     id INTEGER PRIMARY KEY,
-    table_name TEXT NOT NULL,
-    entity_id INTEGER NOT NULL,
-    key TEXT NOT NULL,
-    value BLOB,
-    creation_date INTEGER NOT NULL,
-    change_date INTEGER NOT NULL
+    name TEXT UNIQUE NOT NULL,
+    slots_per_doctor INTEGER NOT NULL
 );
+
+CREATE TABLE sc_slots (
+    id INTEGER PRIMARY KEY,
+    activity_id INTEGER NOT NULL,
+    date TEXT NOT NULL,
+    time INTEGER NOT NULL,
+    overbook INTEGER NOT NULL,
+
+    FOREIGN KEY (activity_id) REFERENCES sc_activities(id)
+);
+
+CREATE TABLE sc_resources (
+    slot_id INTEGER NOT NULL,
+    type TEXT CHECK(type IN ('doctor')),
+    name TEXT NOT NULL,
+
+    FOREIGN KEY (slot_id) REFERENCES sc_slots(id)
+);
+
+-------------------- Test data --------------------
+
+INSERT INTO sc_activities VALUES (1, 'pl', 1);
+INSERT INTO sc_slots VALUES (1, 1, '2019-04-01', 730, 0);
+INSERT INTO sc_slots VALUES (2, 1, '2019-04-01', 1130, 3);
+INSERT INTO sc_slots VALUES (3, 1, '2019-04-02', 730, 0);
+INSERT INTO sc_slots VALUES (4, 1, '2019-04-02', 1130, 1);
+INSERT INTO sc_slots VALUES (5, 1, '2019-04-03', 730, 0);
+INSERT INTO sc_slots VALUES (6, 1, '2019-04-03', 1130, 1);
+INSERT INTO sc_resources VALUES (1, 'doctor', 'X');
+INSERT INTO sc_resources VALUES (1, 'doctor', 'Y');
+INSERT INTO sc_resources VALUES (2, 'doctor', 'X');
+INSERT INTO sc_resources VALUES (3, 'doctor', 'X');
+INSERT INTO sc_resources VALUES (4, 'doctor', 'X');
+INSERT INTO sc_resources VALUES (5, 'doctor', 'X');
+INSERT INTO sc_resources VALUES (6, 'doctor', 'X');
