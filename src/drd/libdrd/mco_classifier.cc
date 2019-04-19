fd8ab4d2ff323f54ec38532726c941027dd75f46
Niels Martignène <niels.martignene@protonmail.com>
2019-04-19 14:34:16 +0200

    Implement non-blocking MCO error 192

--- src/drd/libdrd/mco_classifier.cc
+++ src/drd/libdrd/mco_classifier.cc
@@ -422,7 +422,9 @@ static bool AppendValidProcedures(mco_PreparedSet *out_prepared_set, unsigned in
                 if (RG_UNLIKELY(!(flags & (int)mco_ClassifyFlag::IgnoreProcedureExtension) &&
                                 stay.exit.date >= Date(2016, 3, 1) &&
                                 !(proc_info->extensions & (1u << proc.extension)))) {
-                    if (stay.exit.date >= Date(2017, 3, 1)) {
+                    if (stay.exit.date >= Date(2019, 3, 1) && !proc.extension) {
+                        SetError(out_errors, 192, 0);
+                    } else if (stay.exit.date >= Date(2017, 3, 1)) {
                         valid &= SetError(out_errors, 186);
                     } else {
                         SetError(out_errors, 186, 0);
