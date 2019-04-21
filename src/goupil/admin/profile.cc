100dc5615491cbbed92869c6a1cdb1afeb211f26
Niels Martignène <niels.martignene@protonmail.com>
2019-04-21 18:35:28 +0200

    Add --dev option to `goupil_admin create_profile`

--- src/goupil/admin/profile.cc
+++ src/goupil/admin/profile.cc
@@ -19,23 +19,48 @@ int RunCreateProfile(Span<const char *> arguments)
 {
     BlockAllocator temp_alloc;
 
+    // Options
+    bool dev = false;
+    const char *profile_directory = nullptr;
+
+    static const auto PrintUsage = [](FILE *fp) {
+        PrintLn(fp, R"(Usage: goupil_admin create_profile [options] profile_directory
+
+Options:
+       --dev                     Create developer-oriented profile)");
+    };
+
     static const char *const DefaultConfig =
 R"([Resources]
-DatabaseFile = database.db
+DatabaseFile = %1
 
 # [HTTP]
 # IPStack = Dual
 # Port = 8888
 # Threads = 4
-# BaseUrl = /
-)";
+# BaseUrl = /)";
 
-    // Options
-    const char *profile_directory = nullptr;
+    static const char *const DevData =
+R"(BEGIN TRANSACTION;
 
-    static const auto PrintUsage = [](FILE *fp) {
-        PrintLn(fp, R"(Usage: goupil_admin create_profile [options] profile_directory)");
-    };
+INSERT INTO sc_resources VALUES ('pl', '2019-04-01', 730, 1, 1);
+INSERT INTO sc_resources VALUES ('pl', '2019-04-01', 1130, 2, 0);
+INSERT INTO sc_resources VALUES ('pl', '2019-04-02', 730, 1, 1);
+INSERT INTO sc_resources VALUES ('pl', '2019-04-02', 1130, 2, 0);
+INSERT INTO sc_resources VALUES ('pl', '2019-04-03', 730, 1, 1);
+INSERT INTO sc_resources VALUES ('pl', '2019-04-03', 1130, 2, 0);
+INSERT INTO sc_resources VALUES ('pl', '2019-04-04', 730, 1, 1);
+INSERT INTO sc_resources VALUES ('pl', '2019-04-04', 1130, 2, 0);
+INSERT INTO sc_resources VALUES ('pl', '2019-04-05', 730, 1, 1);
+INSERT INTO sc_resources VALUES ('pl', '2019-04-05', 1130, 2, 0);
+
+INSERT INTO sc_meetings VALUES ('pl', '2019-04-01', 730, 'Peter PARKER');
+INSERT INTO sc_meetings VALUES ('pl', '2019-04-01', 730, 'Mary JANE');
+INSERT INTO sc_meetings VALUES ('pl', '2019-04-01', 730, 'Gwen STACY');
+INSERT INTO sc_meetings VALUES ('pl', '2019-04-02', 730, 'Clark KENT');
+INSERT INTO sc_meetings VALUES ('pl', '2019-04-02', 1130, 'Lex LUTHOR');
+
+END TRANSACTION;)";
 
     // Parse arguments
     {
@@ -45,7 +70,10 @@ DatabaseFile = database.db
             if (opt.Test("--help")) {
                 PrintUsage(stdout);
                 return 0;
+            } else if (opt.Test("--dev")) {
+                dev = true;
             } else {
+                LogError("Cannot handle option '%1'", opt.current_option);
                 return 1;
             }
         }
@@ -77,8 +105,19 @@ DatabaseFile = database.db
     };
 
     // Create database
-    {
-        const char *filename = Fmt(&temp_alloc, "%1%/database.db", profile_directory).ptr;
+    const char *database_name;
+    if (dev) {
+        database_name = "database_dev.sql";
+
+        const char *filename = Fmt(&temp_alloc, "%1%/%2", profile_directory, database_name).ptr;
+        files.Append(filename);
+
+        if (!WriteFile(DevData, filename))
+            return 1;
+    } else {
+        database_name = "database.db";
+
+        const char *filename = Fmt(&temp_alloc, "%1%/%2", profile_directory, database_name).ptr;
         files.Append(filename);
 
         SQLiteDatabase database;
@@ -93,7 +132,10 @@ DatabaseFile = database.db
         const char *filename = Fmt(&temp_alloc, "%1%/goupil.ini", profile_directory).ptr;
         files.Append(filename);
 
-        WriteFile(DefaultConfig, filename);
+        StreamWriter st(filename);
+        PrintLn(&st, DefaultConfig, database_name);
+        if (!st.Close())
+            return 1;
     }
 
     out_guard.Disable();
