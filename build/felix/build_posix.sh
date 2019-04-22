96dfa16bb77bc9e3207d36482e0fad46d38a61de
Niels Martignène <niels.martignene@protonmail.com>
2019-04-22 09:57:29 +0200

    Fix missing -pthread in felix bootstrap build script

--- build/felix/build_posix.sh
+++ build/felix/build_posix.sh
@@ -7,13 +7,13 @@ BIN=../../felix
 
 if command -v g++ >/dev/null 2>&1; then
     echo "Building felix with GCC"
-    g++ -std=gnu++17 -O2 -DNDEBUG $SRC -o $BIN
+    g++ -std=gnu++17 -O2 -pthread -DNDEBUG $SRC -o $BIN
     exit
 fi
 
 if command -v clang++ >/dev/null 2>&1; then
     echo "Building felix with Clang"
-    clang++ -std=gnu++17 -O2 -DNDEBUG $SRC -o $BIN
+    clang++ -std=gnu++17 -O2 -pthread -DNDEBUG $SRC -o $BIN
     exit
 fi
 
