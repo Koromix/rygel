95998b3d7f4dcaeedaed3aa3587f5b52116a5788
Niels Martignène <niels.martignene@protonmail.com>
2019-04-22 21:42:06 +0200

    Fetch schedule data from goupil server

---
+++ src/goupil/server/schedule.hh
@@ -0,0 +1,15 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#pragma once
+
+#include "../../libcc/libcc.hh"
+#include "../../wrappers/http.hh"
+
+namespace RG {
+
+int ProduceScheduleResources(const http_Request &request, http_Response *out_response);
+int ProduceScheduleMeetings(const http_Request &request, http_Response *out_response);
+
+}
