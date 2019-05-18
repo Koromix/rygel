e045f225cef894c9dc851ab8256ce1ca6a5542c4
Niels Martignène <niels.martignene@protonmail.com>
2019-05-18 08:43:42 +0200

    Use simple tutorial as goupil first page

--- src/goupil/client/autoform.js
+++ src/goupil/client/autoform.js
@@ -488,9 +488,67 @@ let autoform = (function() {
 
     function loadDefaultPages() {
         pages = new Map([
-            ['aq', {
-                key: 'aq',
-                title: 'Auto-questionnaire',
+            ['tuto', {
+                key: 'tuto',
+                title: 'Tutoriel',
+                script: `form.output(html\`
+    <p>Une <b>fonction</b> est composée d'un <i>nom</i> et de plusieurs <i>paramètres</i> et permet de proposer un outil de saisie (champ texte, menu déroulant ...).
+    <p>Exemple : la fonction form.text("num_patient", "Numéro de patient") propose un champ de saisie texte intitulé <i>Numéro de patient</i> et le stocke dans la variable <i>num_patient</i>.
+    <p>Vous pouvez copier les fonctions présentées dans la section <b>Exemples</b> dans <b>Nouvelle section</b> pour créer votre propre formulaire.
+\`);
+
+form.section("Nouvelle section", () => {
+    // Copier coller les fonctions ci-dessous
+    // Modifier les noms de variables. Ex : "nom" -> "prenom"
+
+    // Remplacer ce commentaire par une première fonction
+
+    // Remplacer ce commentaire par une deuxième fonction
+});
+
+form.section("Exemples", () => {
+    // Variable texte
+    form.text("nom", "Quel est votre nom ?");
+
+    // Variable numérique
+    form.number("age", "Quel est votre âge ?", {min: 0, max: 120});
+
+    // Variable de choix (boutons)
+    form.choice("sexe", "Quel est votre sexe ?", [["M", "Homme"], ["F", "Femme"]]);
+
+    // Variable à choix unique (menu déroulant)
+    form.dropdown("csp", "Quelle est votre CSP ?", [
+        [1, "Agriculteur exploitant"],
+        [2, "Artisan, commerçant ou chef d'entreprise"],
+        [3, "Cadre ou profession intellectuelle supérieure"],
+        [4, "Profession intermédiaire"],
+        [5, "Employé"],
+        [6, "Ouvrier"],
+        [7, "Retraité"],
+        [8, "Autre ou sans activité professionnelle"]
+    ]);
+
+    // Variable à choix unique (bouton radio)
+    form.radio("lieu_vie", "Quel est votre lieu de vie ?", [
+        ["maison", "Maison"],
+        ["appartement", "Appartement"]
+    ]);
+
+    // Variable cases à cocher multiples
+    form.multi("sommeil", "Présentez-vous un trouble du sommeil ?", [
+        [1, "Troubles d’endormissement"],
+        [2, "Troubles de maintien du sommeil"],
+        [3, "Réveil précoce"],
+        [4, "Sommeil non récupérateur"],
+        [null, "Aucune de ces réponses"]
+    ]);
+});
+`
+            }],
+
+            ['complicated', {
+                key: 'complicated',
+                title: 'Formulaire compliqué',
                 script: `form.text("name", "Quel est votre nom ?");
 form.number("age", "Quel est votre âge ?", {min: 0, max: 120,
                                             suffix: value => util.pluralFR(value, "an", "ans")});
@@ -546,19 +604,19 @@ form.section("Autres", () => {
 });
 
 form.output(html\`On peut aussi mettre du <b>HTML directement</b> si on veut...
-                 <button class="af_button" @click=\${e => go("aide")}>Afficher l'aide</button>\`);
+                 <button class="af_button" @click=\${e => go("complicated_help")}>Afficher l'aide</button>\`);
 form.output("Ou bien encore mettre du <b>texte brut</b>.");
 `
             }],
 
-            ['aide', {
-                key: 'aide',
-                title: 'Aide',
+            ['complicated_help', {
+                key: 'complicated_help',
+                title: 'Formulaire compliqué (aide)',
                 script: `form.output("Loreum ipsum");
 
 form.buttons([
     ["Donner l'alerte", () => alert("Alerte générale !!")],
-    ["Revenir à l'auto-questionnaire", () => go("aq")]
+    ["Revenir à l'auto-questionnaire", () => go("complicated")]
 ]);
 `
             }]
