e7b9189fb9acc300f28ed2c695317eda4f262004
Niels Martignène <niels.martignene@protonmail.com>
2019-05-30 23:27:38 +0200

    Add default page setting to goupil autoform

--- src/goupil/client/goupil.js
+++ src/goupil/client/goupil.js
@@ -30,6 +30,7 @@ let goupil = (function() {
 
         let db_name = `goupil_${settings.project_key}`;
         self.database = data.open(db_name, db => {
+            db.createObjectStore('settings');
             db.createObjectStore('pages', {keyPath: 'key'});
         });
     }
