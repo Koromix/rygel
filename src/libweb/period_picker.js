1a769a4b077afc4974fa75907bd335a88bc2effa
Niels Martignène <niels.martignene@protonmail.com>
2019-06-08 23:43:11 +0200

    Migrate PeriodPicker widget to lit-html

--- src/libweb/period_picker.js
+++ src/libweb/period_picker.js
@@ -2,14 +2,18 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-function PeriodPicker(widget, min_date, max_date, start_date, end_date)
+function PeriodPicker()
 {
+    let self = this;
+
     this.changeHandler = null;
 
-    let self = this;
-    let main = null;
-    let handles = null;
-    let bar = null;
+    let limit_dates = [new Date(1900, 1, 1), new Date(2100, 1, 1)];
+    let current_dates = limit_dates.map(date => new Date(date));
+
+    let main_el;
+    let handle_els;
+    let bar_el;
 
     let grab_capture = false;
     let grab_offset;
@@ -18,51 +22,50 @@ function PeriodPicker(widget, min_date, max_date, start_date, end_date)
     let grab_start_day;
     let grab_end_day;
 
-    let parser = dom.h('input', {type: 'date'});
+    // Used by dateToStr() and strToDate()
+    let date_el = document.createElement('input');
+    date_el.setAttribute('type', 'date');
+
+    this.getRootElement = function() { return root_el; };
+
+    this.setLimitDates = function(dates) { limit_dates = dates.map(strToDate); };
+    this.getLimitDates = function() { return limit_dates.map(dateToStr); };
+    this.setDates = function(dates) { current_dates = dates.map((str, idx) => str ? strToDate(str) : limit_dates[idx]); };
+    this.getDates = function() { return current_dates.map(dateToStr); };
+
+    function dateToStr(date)
+    {
+        date_el.valueAsDate = date;
+        return date_el.value;
+    }
+
     function strToDate(str)
     {
+        date_el.value = str;
+
+        let date = date_el.valueAsDate;
         // Put a few hours into the day to avoid DST problems
-        parser.value = str;
-        let date = parser.valueAsDate;
         date.setHours(2);
 
         return date;
     }
 
-    function syncHandle(handle)
+    function syncHandle(handle, date)
     {
-        let new_pos = 100 * (handle.lastChild.valueAsDate - min_date) / (max_date - min_date);
+        let input = handle.querySelector('input');
+
+        let new_pos = 100 * (date - limit_dates[0]) / (limit_dates[1] - limit_dates[0]);
         handle.style.left = '' + new_pos + '%';
+        input.valueAsDate = date;
     }
 
-    function syncBar()
+    function syncView()
     {
-        bar.style.left = handles[0].style.left;
-        bar.style.width = 'calc(' + handles[1].style.left + ' - ' + handles[0].style.left + ')';
-    }
+        syncHandle(handle_els[0], current_dates[0]);
+        syncHandle(handle_els[1], current_dates[1]);
 
-    function clampHandleDate(handle, date)
-    {
-        let min = min_date;
-        let max = max_date;
-        if (handles[0].lastChild.valueAsDate && handle !== handles[0]) {
-            min = handles[0].lastChild.valueAsDate;
-            min.setDate(min.getDate() + 1);
-            min.setHours(2);
-        }
-        if (handles[1].lastChild.valueAsDate && handle !== handles[1]) {
-            max = handles[1].lastChild.valueAsDate;
-            max.setDate(min.getDate() - 1);
-            max.setHours(2);
-        }
-
-        if (date < min) {
-            return min;
-        } else if (date > max) {
-            return max;
-        } else {
-            return date;
-        }
+        bar_el.style.left = handle_els[0].style.left;
+        bar_el.style.width = 'calc(' + handle_els[1].style.left + ' - ' + handle_els[0].style.left + ')';
     }
 
     function positionToDate(pos)
@@ -70,18 +73,18 @@ function PeriodPicker(widget, min_date, max_date, start_date, end_date)
         let date_delta;
         {
             let min_pos = 0;
-            let max_pos = main.offsetWidth;
+            let max_pos = main_el.offsetWidth;
             if (pos < min_pos) {
                 pos = min_pos;
             } else if (pos > max_pos) {
                 pos = max_pos;
             }
 
-            date_delta = ((max_date - min_date) * ((pos - min_pos) / (max_pos - min_pos)));
+            date_delta = ((limit_dates[1] - limit_dates[0]) * ((pos - min_pos) / (max_pos - min_pos)));
         }
 
         // Is there any part of the standard JS library that isn't complete crap?
-        let date = new Date(min_date);
+        let date = new Date(limit_dates[0]);
         date.setTime(date.getTime() + date_delta + 2 * 86400000);
         date.setDate(1);
         date.setHours(2);
@@ -89,114 +92,115 @@ function PeriodPicker(widget, min_date, max_date, start_date, end_date)
         return date;
     }
 
+    function clampStartDate(date)
+    {
+        let max = new Date(current_dates[1]);
+        max.setDate(max.getDate() - 1);
+        max.setHours(2);
+
+        if (date <= limit_dates[0]) {
+            return limit_dates[0];
+        } else if (date >= max) {
+            return max;
+        } else {
+            return date;
+        }
+    }
+
+    function clampEndDate(date)
+    {
+        let min = new Date(current_dates[0]);
+        min.setDate(min.getDate() + 1);
+        min.setHours(2);
+
+        if (date >= limit_dates[1]) {
+            return limit_dates[1];
+        } else if (date <= min) {
+            return min;
+        } else {
+            return date;
+        }
+    }
+
     function handleHandleDown(e)
     {
-        if (Element.prototype.setPointerCapture) {
-            this.setPointerCapture(e.pointerId);
-        } else {
-            let self = this;
-            function forwardMove(e)
-            {
-                return handleHandleMove.call(self, e);
-            }
-
-            document.body.addEventListener('mousemove', forwardMove);
-            document.body.addEventListener('mouseup', function() {
-                document.body.removeEventListener('mousemove', forwardMove);
-                handlePointerUp.call(self, e);
-            }, {once: true});
-        }
+        e.target.setPointerCapture(e.pointerId);
         grab_capture = true;
     }
 
     function handleHandleMove(e)
     {
         if (grab_capture) {
-            let handle = this.parentNode;
-            let input = handle.lastChild;
+            let handle = e.target.parentNode;
+            let date = positionToDate(e.clientX - main_el.offsetLeft);
 
-            let new_date = positionToDate(e.clientX - main.offsetLeft);
-            new_date = clampHandleDate(handle, new_date);
-            input.valueAsDate = new_date;
+            if (handle === handle_els[0]) {
+                current_dates[0] = clampStartDate(date);
+            } else {
+                current_dates[1] = clampEndDate(date);
+            }
 
-            syncHandle(handle);
-            syncBar();
+            syncView();
         }
     }
 
     function handleDateChange(e)
     {
-        let handle = this.parentNode;
+        let handle = e.target.parentNode;
+        let input = handle.querySelector('input');
 
-        let call_change_handler = false;
-        if (this.valueAsDate) {
-            this.valueAsDate = clampHandleDate(handle, this.valueAsDate);
+        if (input.valueAsDate) {
+            if (handle === handle_els[0]) {
+                current_dates[0] = clampStartDate(input.valueAsDate);
+            } else {
+                current_dates[1] = clampEndDate(input.valueAsDate);
+            }
         } else {
-            this.valueAsDate = (handle === handles[0]) ? min_date : max_date;
-            call_change_handler = true;
+            if (handle === handle_els[0]) {
+                current_dates[0] = limit_dates[0];
+            } else {
+                current_dates[1] = limit_dates[1];
+            }
+
+            if (self.changeHandler)
+                setTimeout(() => self.changeHandler.call(self, e), 0);
         }
 
-        syncHandle(this.parentNode);
-        syncBar();
-
-        if (call_change_handler && self.changeHandler)
-            setTimeout(function() { self.changeHandler.call(widget); }, 0);
+        syncView();
     }
 
     function handleDateFocusOut(e)
     {
         if (self.changeHandler)
-            setTimeout(function() { self.changeHandler.call(widget); }, 0);
+            setTimeout(() => self.changeHandler.call(self, e), 0);
     }
 
     function handleBarDown(e)
     {
-        if (Element.prototype.setPointerCapture) {
-            this.setPointerCapture(e.pointerId);
-        } else {
-            let self = this;
-            function forwardMove(e)
-            {
-                return handleBarMove.call(self, e);
-            }
-
-            document.body.addEventListener('mousemove', forwardMove);
-            document.body.addEventListener('mouseup', function() {
-                document.body.removeEventListener('mousemove', forwardMove);
-                handlePointerUp.call(self, e);
-            }, {once: true});
-        }
+        e.target.setPointerCapture(e.pointerId);
         grab_capture = true;
 
-        let start_date = handles[0].lastChild.valueAsDate;
-        let end_date = handles[1].lastChild.valueAsDate;
-
         grab_offset = e.offsetX;
-        grab_diff_year = end_date.getFullYear() - start_date.getFullYear();
-        grab_diff_month = end_date.getMonth() - start_date.getMonth();
-        grab_start_day = start_date.getDate();
-        grab_end_day = end_date.getDate();
+        grab_diff_year = current_dates[1].getFullYear() - current_dates[0].getFullYear();
+        grab_diff_month = current_dates[1].getMonth() - current_dates[0].getMonth();
+        grab_start_day = current_dates[0].getDate();
+        grab_end_day = current_dates[1].getDate();
     }
 
     function handleBarMove(e)
     {
         if (grab_capture) {
-            let new_start_date = positionToDate(e.clientX - main.offsetLeft - grab_offset);
-            let new_end_date = new Date(new_start_date.getFullYear() + grab_diff_year,
-                                        new_start_date.getMonth() + grab_diff_month,
+            current_dates[0] = positionToDate(e.clientX - main_el.offsetLeft - grab_offset);
+            current_dates[1] = new Date(current_dates[0].getFullYear() + grab_diff_year,
+                                        current_dates[0].getMonth() + grab_diff_month,
                                         grab_end_day, 2);
-            while (new_end_date > max_date) {
-                new_start_date.setMonth(new_start_date.getMonth() - 1);
-                new_end_date.setMonth(new_end_date.getMonth() - 1);
+            while (current_dates[1] > limit_dates[1]) {
+                current_dates[0].setMonth(current_dates[0].getMonth() - 1);
+                current_dates[1].setMonth(current_dates[1].getMonth() - 1);
             }
-            new_start_date.setDate(grab_start_day);
+            current_dates[0].setDate(grab_start_day);
 
-            handles[0].lastChild.valueAsDate = new_start_date;
-            handles[1].lastChild.valueAsDate = new_end_date;
-
-            syncHandle(handles[0]);
-            syncHandle(handles[1]);
-            syncBar();
+            syncView();
         }
     }
 
@@ -205,65 +209,43 @@ function PeriodPicker(widget, min_date, max_date, start_date, end_date)
         grab_capture = false;
 
         if (self.changeHandler)
-            setTimeout(function() { self.changeHandler.call(widget); }, 0);
+            setTimeout(() => self.changeHandler.call(self, e), 0);
     }
 
-    // TODO: Only mess with the (visible?) DOM when 'rendering'
-    this.render = function() {
-        handles[0].lastChild.valueAsDate = clampHandleDate(handles[0], start_date);
-        handles[1].lastChild.valueAsDate = clampHandleDate(handles[1], end_date);
-
-        syncHandle(handles[0]);
-        syncHandle(handles[1]);
-        syncBar();
-    }
-
-    this.getValues = function() { return [handles[0].lastChild.value, handles[1].lastChild.value]; }
-    this.getWidget = function() { return widget; }
-
-    function mouseEvents(down, move)
+    function makeMouseElement(cls, down, move)
     {
-        if (Element.prototype.setPointerCapture) {
-            return {
-                pointerdown: down,
-                pointermove: move,
-                pointerup: handlePointerUp
-            };
-        } else {
-            return {
-                mousedown: down
-            };
-        }
+        return html`<div class=${cls} @pointerdown=${down} @pointermove=${move}
+                         @pointerup=${handlePointerUp}></div>`;
     }
 
-    widget.addClass('ppik');
-    widget.replaceContent(
-        // This dummy button catches click events that happen when a label encloses the widget
-        dom.h('button', {style: 'display: none;', click: function(e) { e.preventDefault(); }}),
+    this.render = function(new_root_el) {
+        root_el = new_root_el;
 
-        dom.h('div', {class: 'ppik_main'},
-            dom.h('div', {class: 'ppik_line'}),
-            dom.h('div', Object.assign({class: 'ppik_bar'}, mouseEvents(handleBarDown, handleBarMove))),
-            dom.h('div', {class: 'ppik_handle'},
-                dom.h('div', mouseEvents(handleHandleDown, handleHandleMove)),
-                dom.h('input', {type: 'date', style: 'bottom: 4px;',
-                                change: handleDateChange, focusout: handleDateFocusOut})
-            ),
-            dom.h('div', {class: 'ppik_handle'},
-                dom.h('div', mouseEvents(handleHandleDown, handleHandleMove)),
-                dom.h('input', {type: 'date', style: 'top: 18px;',
-                                change: handleDateChange, focusout: handleDateFocusOut})
-            )
-        )
-    );
-    main = widget.query('.ppik_main');
-    handles = widget.queryAll('.ppik_handle');
-    bar = widget.query('.ppik_bar');
+        // The dummy button catches click events that happen when a label encloses the widget
+        render(html`
+            <div class="ppik">
+                <button style="display: none;" @click=${e => e.preventDefault()}></button>
 
-    min_date = min_date ? strToDate(min_date) : new Date(1900, 1, 1);
-    max_date = max_date ? strToDate(max_date) : new Date(2100, 1, 1);
-    start_date = start_date ? strToDate(start_date) : min_date;
-    end_date = end_date ? strToDate(end_date) : max_date;
+                <div class="ppik_main">
+                    <div class="ppik_line"></div>
+                    ${makeMouseElement('ppik_bar', handleBarDown, handleBarMove)}
+                    <div class="ppik_handle">
+                        ${makeMouseElement('', handleHandleDown, handleHandleMove)}
+                        <input type="date" style="bottom: 4px;" @change=${handleDateChange}
+                               @focusout=${handleDateFocusOut}/>
+                    </div>
+                    <div class="ppik_handle">
+                        ${makeMouseElement('', handleHandleDown, handleHandleMove)}
+                        <input type="date" style="top: 18px;" @change=${handleDateChange}
+                               @focusout=${handleDateFocusOut}/>
+                    </div>
+                </div>
+            </div>
+        `, root_el);
+        main_el = root_el.query('.ppik_main');
+        handle_els = root_el.queryAll('.ppik_handle');
+        bar_el = root_el.query('.ppik_bar');
 
-    widget.object = this;
+        syncView();
+    };
 }
