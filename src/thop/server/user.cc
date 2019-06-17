abfc3e4697661fe458f9df31b47128835149fd17
Niels Martignène <niels.martignene@protonmail.com>
2019-06-17 15:54:42 +0200

    Add constant safety delay after failed THOP logins

--- src/thop/server/user.cc
+++ src/thop/server/user.cc
@@ -396,10 +396,17 @@ int HandleConnect(const http_Request &request, const User *, http_Response *out_
 
     // Find and validate user
     const User *user = thop_user_set.FindUser(username);
-    if (!user || !user->password_hash ||
-            crypto_pwhash_str_verify(user->password_hash, password, strlen(password)) != 0) {
-        LogError("Incorrect username or password");
-        return http_ProduceErrorPage(403, out_response);
+    {
+        int64_t now = GetMonotonicTime();
+
+        if (!user || !user->password_hash ||
+                crypto_pwhash_str_verify(user->password_hash, password, strlen(password)) != 0) {
+            int64_t safety_delay = std::max(2000 - GetMonotonicTime() + now, (int64_t)0);
+            WaitForDelay(safety_delay);
+
+            LogError("Incorrect username or password");
+            return http_ProduceErrorPage(403, out_response);
+        }
     }
 
     // Create session key
