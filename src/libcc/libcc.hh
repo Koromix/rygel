46c2486d7e9d7b49058cfd4a732a192ff3dd97f9
Niels Martignène <niels.martignene@protonmail.com>
2019-06-19 20:37:26 +0200

    Support writing to stdin in ExecuteCommandLine()

--- src/libcc/libcc.hh
+++ src/libcc/libcc.hh
@@ -3205,7 +3205,7 @@ enum class OpenFileMode {
 
 FILE *OpenFile(const char *path, OpenFileMode mode);
 
-bool ExecuteCommandLine(const char *cmd_line,
+bool ExecuteCommandLine(const char *cmd_line, Span<const char> in_buf,
                         std::function<void(Span<char> buf)> out_func, int *out_code);
 bool ExecuteCommandLine(const char *cmd_line, Size max_len,
                         HeapArray<char> *out_buf, int *out_code);
