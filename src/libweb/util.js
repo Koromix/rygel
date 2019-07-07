e8f79bdaaee0f5614ed38395af53653b6d2e9e10
Niels Martignène <niels.martignene@protonmail.com>
2019-07-07 10:50:54 +0200

    Drop unused getRandomX() methods from libweb util

--- src/libweb/util.js
+++ src/libweb/util.js
@@ -279,18 +279,6 @@ let util = (function() {
         return JSON.stringify(value);
     };
 
-    // Minimum is inclusive, maximum is exclusive
-    this.getRandomFloat = function(min, max) {
-        return Math.random() * (max - min) + min;
-    };
-
-    // Minimum is inclusive, maximum is exclusive
-    this.getRandomInt = function(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min)) + min;
-    };
-
     function encode30bits(value, buf, offset, len) {
         let characters = '0123456789ABCDEFGHJKMNPQRSTVWXYZ';
 
