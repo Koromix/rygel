4eb33b3d88ee4551ae1fd55222c013416f4df6f1
Niels Martignène <niels.martignene@protonmail.com>
2019-07-12 22:26:28 +0200

    Avoid comments and semi-columns in example scripts

--- src/goupil/client/autoform_default.js
+++ src/goupil/client/autoform_default.js
@@ -13,34 +13,27 @@ let autoform_default = (function() {
             title: 'Tutoriel',
             script: `// Retirer le commentaire de la ligne suivante pour afficher les
 // champs (texte, numérique, etc.) à droite du libellé.
-// form.pushOptions({large: true});
+// form.pushOptions({large: true})
 
 form.output(html\`
     <p>Une <b>fonction</b> est composée d'un <i>nom</i> et de plusieurs <i>paramètres</i> et permet de proposer un outil de saisie (champ texte, menu déroulant ...).
     <p>Exemple : la fonction form.text("num_patient", "Numéro de patient") propose un champ de saisie texte intitulé <i>Numéro de patient</i> et le stocke dans la variable <i>num_patient</i>.
     <p>Vous pouvez copier les fonctions présentées dans la section <b>Exemples</b> dans <b>Nouvelle section</b> pour créer votre propre formulaire.
-\`);
+\`)
 
 form.section("Nouvelle section", () => {
-    // Copier coller les fonctions ci-dessous
-    // Modifier les noms de variables. Ex : "nom" -> "prenom"
+    // Copier coller les fonctions dans les lignes vides ci-dessous
 
-    // Remplacer ce commentaire par une première fonction
 
-    // Remplacer ce commentaire par une deuxième fonction
-});
+})
 
 form.section("Exemples", () => {
-    // Variable texte
-    form.text("nom", "Quel est votre nom ?", {mandatory: true});
+    form.text("nom", "Quel est votre nom ?", {mandatory: true})
 
-    // Variable numérique
-    form.number("age", "Quel est votre âge ?", {min: 0, max: 120});
+    form.number("age", "Quel est votre âge ?", {min: 0, max: 120})
 
-    // Variable de choix (boutons)
-    form.choice("sexe", "Quel est votre sexe ?", [["M", "Homme"], ["F", "Femme"]]);
+    form.choice("sexe", "Quel est votre sexe ?", [["M", "Homme"], ["F", "Femme"]])
 
-    // Variable à choix unique (menu déroulant)
     form.dropdown("csp", "Quelle est votre CSP ?", [
         [1, "Agriculteur exploitant"],
         [2, "Artisan, commerçant ou chef d'entreprise"],
@@ -50,39 +43,37 @@ form.section("Exemples", () => {
         [6, "Ouvrier"],
         [7, "Retraité"],
         [8, "Autre ou sans activité professionnelle"]
-    ]);
+    ])
 
-    // Variable à choix unique (bouton radio)
     form.radio("lieu_vie", "Quel est votre lieu de vie ?", [
         ["maison", "Maison"],
         ["appartement", "Appartement"]
-    ]);
+    ])
 
-    // Variable cases à cocher multiples
     form.multi("sommeil", "Présentez-vous un trouble du sommeil ?", [
         [1, "Troubles d’endormissement"],
         [2, "Troubles de maintien du sommeil"],
         [3, "Réveil précoce"],
         [4, "Sommeil non récupérateur"],
         [null, "Aucune de ces réponses"]
-    ]);
-});
+    ])
+})
 
-form.errorList();
-form.buttons('save');
+form.errorList()
+form.buttons('save')
 `
         },
 
         {
             key: 'complicated',
             title: 'Formulaire compliqué',
-            script: `form.pushOptions({large: true});
+            script: `form.pushOptions({large: true})
 
-form.text("name", "Quel est votre nom ?", {mandatory: true});
+form.text("name", "Quel est votre nom ?", {mandatory: true})
 form.number("age", "Quel est votre âge ?", {min: 0, max: 120,
-                                            suffix: value => value > 1 ? "ans" : "an"});
+                                            suffix: value => value > 1 ? "ans" : "an"})
 
-let sexe = form.choice("sexe", "Quel est votre sexe ?", [["M", "Homme"], ["F", "Femme"]]);
+let sexe = form.choice("sexe", "Quel est votre sexe ?", [["M", "Homme"], ["F", "Femme"]])
 
 form.dropdown("csp", "Quelle est votre CSP ?", [
     [1, "Agriculteur exploitant"],
@@ -93,12 +84,12 @@ form.dropdown("csp", "Quelle est votre CSP ?", [
     [6, "Ouvrier"],
     [7, "Retraité"],
     [8, "Autre ou sans activité professionnelle"]
-]);
+])
 
 form.radio("lieu_vie", "Quel est votre lieu de vie ?", [
     ["maison", "Maison"],
     ["appartement", "Appartement"]
-]);
+])
 
 form.multi("sommeil", "Présentez-vous un trouble du sommeil ?", [
     [1, "Troubles d’endormissement"],
@@ -106,50 +97,50 @@ form.multi("sommeil", "Présentez-vous un trouble du sommeil ?", [
     [3, "Réveil précoce"],
     [4, "Sommeil non récupérateur"],
     [null, "Aucune de ces réponses"]
-]);
+])
 
 if (sexe.value == "F") {
     form.binary("enceinte", "Êtes-vous enceinte ?");
 }
 
 form.section("Alcool", () => {
-    let alcool = form.binary("alcool", "Consommez-vous de l'alcool ?");
+    let alcool = form.binary("alcool", "Consommez-vous de l'alcool ?")
 
     if (alcool.value && form.value("enceinte")) {
         alcool.error("Pensez au bébé...");
         alcool.error("On peut mettre plusieurs erreurs");
-        form.error("alcool", "Et de plein de manières différentes !");
+        form.error("alcool", "Et de plein de manières différentes !")
     }
 
     if (alcool.value) {
-        form.number("alcool_qt", "Combien de verres par semaine ?", {min: 1, max: 30});
+        form.number("alcool_qt", "Combien de verres par semaine ?", {min: 1, max: 30})
     }
-});
+})
 
 form.section("Autres", () => {
-    form.number("enfants", "Combien avez-vous d'enfants ?", {min: 0, max: 30});
+    form.number("enfants", "Combien avez-vous d'enfants ?", {min: 0, max: 30})
     form.binary("frites", "Aimez-vous les frites ?",
-                {help: "Si si, c'est important, je vous le jure !"});
+                {help: "Si si, c'est important, je vous le jure !"})
 });
 
 form.output(html\`On peut aussi mettre du <b>HTML directement</b> si on veut...
-                 <button class="af_button" @click=\${e => go("complicated_help")}>Afficher l'aide</button>\`);
-form.output("Ou bien encore mettre du <b>texte brut</b>.");
+                 <button class="af_button" @click=\${e => go("complicated_help")}>Afficher l'aide</button>\`)
+form.output("Ou bien encore mettre du <b>texte brut</b>.")
 
-form.errorList();
-form.buttons('save');
+form.errorList()
+form.buttons('save')
 `
         },
 
         {
             key: 'complicated_help',
             title: 'Formulaire compliqué (aide)',
-            script: `form.output("Loreum ipsum");
+            script: `form.output("Loreum ipsum")
 
 form.buttons([
     ["Donner l'alerte", () => alert("Alerte générale !!")],
     ["Revenir à l'auto-questionnaire", () => go("complicated")]
-]);
+])
 `
         }
     ];
