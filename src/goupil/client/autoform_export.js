85c46ad1617e1146ac03a9ff69aa8a561a1c2300
Niels Martignène <niels.martignene@protonmail.com>
2019-07-15 13:37:48 +0200

    Fix ordering of intra-set columns in goupil exports

--- src/goupil/client/autoform_export.js
+++ src/goupil/client/autoform_export.js
@@ -3,54 +3,85 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 let autoform_export = (function() {
+    function reverseLastColumns(columns, start_idx) {
+        for (let i = 0; i < (columns.length - start_idx) / 2; i++) {
+            let tmp = columns[start_idx + i];
+            columns[start_idx + i] = columns[columns.length - i - 1];
+            columns[columns.length - i - 1] = tmp;
+        }
+    }
+
     function orderColumns(variables) {
+        variables = variables.slice();
+        variables.sort((variable1, variable2) => util.compareValues(variable1.key, variable2.key));
+
         let first_set = new Set;
-        let before_map = {};
+        let sets_map = {};
+        let variables_map = {};
         for (let variable of variables) {
             if (variable.before == null) {
-                first_set.add(variable);
+                first_set.add(variable.key);
             } else {
-                let set_ptr = before_map[variable.before];
+                let set_ptr = sets_map[variable.before];
                 if (!set_ptr) {
                     set_ptr = new Set;
-                    before_map[variable.before] = set_ptr;
+                    sets_map[variable.before] = set_ptr;
                 }
 
-                set_ptr.add(variable);
+                set_ptr.add(variable.key);
             }
+
+            variables_map[variable.key] = variable;
         }
 
         let columns = [];
         {
-            let set_candidates = [first_set];
-            let set_idx = 0;
+            let next_sets = [first_set];
+            let next_set_idx = 0;
 
-            while (set_idx < set_candidates.length) {
-                let set_ptr = set_candidates[set_idx++];
+            while (next_set_idx < next_sets.length) {
+                let set_ptr = next_sets[next_set_idx++];
+                let set_start_idx = columns.length;
 
                 while (set_ptr.size) {
-                    for (let variable of set_ptr) {
-                        if (!set_ptr.has(variable.after)) {
-                            let next_set = before_map[variable.key];
-                            if (next_set) {
-                                set_candidates.push(next_set);
-                                delete before_map[variable.key];
-                            }
+                    let frag_start_idx = columns.length;
 
-                            columns.push(variable.key);
-                            set_ptr.delete(variable);
+                    for (let key of set_ptr) {
+                        let variable = variables_map[key];
+
+                        if (!set_ptr.has(variable.after))
+                            columns.push(key);
+                    }
+
+                    reverseLastColumns(columns, frag_start_idx);
+
+                    // Avoid infinite loop that may happen in rare cases
+                    if (columns.length === frag_start_idx) {
+                        let use_key = str_ptr.values().next().value;
+                        columns.push(use_key);
+                    }
+
+                    for (let i = frag_start_idx; i < columns.length; i++) {
+                        let key = columns[i];
+
+                        let next_set = sets_map[key];
+                        if (next_set) {
+                            next_sets.push(next_set);
+                            delete sets_map[key];
                         }
+
+                        delete variables_map[key];
+                        set_ptr.delete(key);
                     }
                 }
+
+                reverseLastColumns(columns, set_start_idx);
             }
         }
 
         // Remaining variables (probably from old forms)
-        for (let key in before_map) {
-            let variables = before_map[key];
-            for (let variable of variables)
-                columns.push(variable.key);
-        }
+        for (let key in variables_map)
+            columns.push(key);
 
         return columns;
     }
