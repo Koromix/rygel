cf7102bf7a61fe113204c0b901038b2725498e46
Niels Martignène <niels.martignene@protonmail.com>
2019-08-21 23:54:11 +0200

    Support heimdallR compilation on Linux

--- src/libgui/libgui.cc
+++ src/libgui/libgui.cc
@@ -2,5 +2,6 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+#include "window_glfw.cc"
 #include "window_win32.cc"
 #include "imgui.cc"
