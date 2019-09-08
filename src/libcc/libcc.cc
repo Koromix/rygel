Koromix/rygel

commit ce6bdfb01726c1b7620c375909ca8a3e76bcfae3

Fix incorrect link between Async and AsyncPool

src/libcc/libcc.cc
@@ -2434,59 +2434,49 @@ public:
 
 // thread_local breaks down on MinGW when destructors are involved, work
 // around this with heap allocation.
-static RG_THREAD_LOCAL Async *g_async = nullptr;
 static RG_THREAD_LOCAL AsyncPool *g_async_pool = nullptr;
 static RG_THREAD_LOCAL int g_async_worker_idx;
 static RG_THREAD_LOCAL bool g_task_running = false;
 
 Async::Async(int workers)
 {
-    RG_ASSERT(!g_async);
-
-    prev_pool = g_async_pool;
-    prev_worker_idx = g_async_worker_idx;
-
     if (workers >= 0) {
         if (workers > RG_ASYNC_MAX_WORKERS) {
             LogError("Async cannot use more than %1 workers", RG_ASYNC_MAX_WORKERS);
             workers = RG_ASYNC_MAX_WORKERS;
         }
 
-        g_async_pool = new AsyncPool(workers, false);
-        g_async_worker_idx = 0;
-    } else if (!g_async_pool) {
-        workers = std::min(GetCoreCount() - 1, RG_ASYNC_MAX_WORKERS);
+        pool = new AsyncPool(workers, false);
+    } else {
+        if (!g_async_pool) {
+            workers = std::min(GetCoreCount() - 1, RG_ASYNC_MAX_WORKERS);
 
-        // NOTE: We're leaking one AsyncPool each time a non-worker thread uses Async() for
-        // the first time. That's only one leak in most cases, when the main thread is the
-        // only non-worker thread using Async, but still. Something to keep in mind.
-        g_async_pool = new AsyncPool(workers, true);
-        g_async_worker_idx = 0;
+            // NOTE: We're leaking one AsyncPool each time a non-worker thread uses Async()
+            // for the first time. That's only one leak in most cases, when the main thread
+            // is the only non-worker thread using Async, but still. Something to keep in mind.
+            g_async_pool = new AsyncPool(workers, true);
+        }
+
+        pool = g_async_pool;
     }
 
-    g_async = this;
-    g_async_pool->RegisterAsync();
+    pool->RegisterAsync();
 }
 
 Async::~Async()
 {
     RG_ASSERT(!remaining_tasks);
-
-    g_async_pool->UnregisterAsync();
-    g_async = nullptr;
-
-    g_async_pool = (AsyncPool *)prev_pool;
-    g_async_worker_idx = prev_worker_idx;
+    pool->UnregisterAsync();
 }
 
 void Async::Run(const std::function<bool()> &func)
 {
-    g_async_pool->AddTask(this, func);
+    pool->AddTask(this, func);
 }
 
 bool Async::Sync()
 {
-    g_async_pool->SyncOn(this);
+    pool->SyncOn(this);
     return success;
 }
 
@@ -2529,9 +2519,7 @@ void AsyncPool::UnregisterAsync()
 
 void AsyncPool::AddTask(Async *async, const std::function<bool()> &func)
 {
-    // If we are the main thread, and haven't reached Sync() yet, try to assign
-    // task to a "random" queue first.
-    if (!g_task_running && workers_state.len > 1) {
+    if (g_async_pool != async->pool) {
         for (;;) {
             TaskQueue *queue = &queues[next_queue_idx];
 
@@ -2588,6 +2576,15 @@ void AsyncPool::RunWorker(int worker_idx)
 
 void AsyncPool::SyncOn(Async *async)
 {
+    RG_DEFER_C(pool = g_async_pool,
+               worker_idx = g_async_worker_idx) {
+        g_async_pool = pool;
+        g_async_worker_idx = worker_idx;
+    };
+
+    g_async_pool = this;
+    g_async_worker_idx = 0;
+
     while (async->remaining_tasks) {
         RunTasks(g_async_worker_idx);
 

src/libcc/libcc.hh
@@ -59,7 +59,7 @@ namespace RG {
 
 #define RG_LINE_READER_STEP_SIZE 65536
 
-#define RG_ASYNC_MAX_WORKERS 256
+#define RG_ASYNC_MAX_WORKERS 255
 #define RG_ASYNC_MAX_IDLE_TIME 10000
 
 // ------------------------------------------------------------------------
@@ -3355,12 +3355,13 @@ static const char *const IPStackNames[] = {
 // Tasks
 // ------------------------------------------------------------------------
 
+class AsyncPool;
+
 class Async {
     std::atomic_int success {1};
     std::atomic_int remaining_tasks {0};
 
-    void *prev_pool;
-    int prev_worker_idx;
+    AsyncPool *pool;
 
 public:
     Async(int workers = -1);
@@ -3374,7 +3375,7 @@ public:
 
     static bool IsTaskRunning();
 
-    friend class AsyncPool;
+    friend AsyncPool;
 };
 
 // ------------------------------------------------------------------------

commit e0e5b532cc98f14449c496725684d93af7bb97cc

Fix Async performance regression introduced in ce6bdfb

src/libcc/libcc.cc
@@ -2465,6 +2465,7 @@ public:
 
 // thread_local breaks down on MinGW when destructors are involved, work
 // around this with heap allocation.
+static RG_THREAD_LOCAL AsyncPool *async_default_pool = nullptr;
 static RG_THREAD_LOCAL AsyncPool *async_running_pool = nullptr;
 static RG_THREAD_LOCAL int async_running_worker_idx;
 static RG_THREAD_LOCAL bool async_running_task = false;
@@ -2478,17 +2479,19 @@ Async::Async(int workers)
         }
 
         pool = new AsyncPool(workers, false);
+    } else if (async_running_pool) {
+        pool = async_running_pool;
     } else {
-        if (!async_running_pool) {
+        if (!async_default_pool) {
             // NOTE: We're leaking one AsyncPool each time a non-worker thread uses Async()
             // for the first time. That's only one leak in most cases, when the main thread
             // is the only non-worker thread using Async, but still. Something to keep in mind.
 
             workers = std::min(GetCoreCount() - 1, RG_ASYNC_MAX_WORKERS);
-            async_running_pool = new AsyncPool(workers, true);
+            async_default_pool = new AsyncPool(workers, true);
         }
 
-        pool = async_running_pool;
+        pool = async_default_pool;
     }
 
     pool->RegisterAsync();