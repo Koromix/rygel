145ac124f3999c18fa95c76c28153f17d192e736
Niels Martignène <niels.martignene@protonmail.com>
2019-09-26 22:21:28 +0200

    Manage scroll state in THOP router

--- src/thop/client/thop.js
+++ src/thop/client/thop.js
@@ -9,6 +9,8 @@ let thop = (function() {
     let self = this;
 
     let route_mod;
+    let route_url = '';
+    let scroll_cache = new LruMap(32);
 
     document.addEventListener('readystatechange', e => {
         if (document.readyState === 'complete')
@@ -20,13 +22,17 @@ let thop = (function() {
         initNavigation();
         await initSettings();
 
+        // We deal with this
+        if (window.history.scrollRestoration)
+            window.history.scrollRestoration = 'manual';
+
         // Initialize module routes
         mco_info.route = mco_info.parseURL('');
         mco_casemix.route = mco_casemix.parseURL('');
         user.route = user.parseURL('');
 
         // Initialize some UI elements
-        updateMenu('');
+        updateMenu();
         updateSession();
 
         self.go(window.location.href, {}, false);
@@ -77,8 +83,9 @@ let thop = (function() {
 
                 default: {
                     // Cannot make canonical URL (because it's invalid), but it's better than nothing
-                    updateHistory(mod, push_history);
-                    updateMenu(mod);
+                    route_url = mod;
+                    updateHistory(push_history);
+                    updateMenu();
 
                     log.error('Aucun module disponible pour cette adresse');
                     return;
@@ -92,9 +99,20 @@ let thop = (function() {
             util.assignDeep(route_mod.route, args);
         }
 
+        // Memorize current scroll state
+        if (window.history.scrollRestoration || push_history) {
+            let target = [window.pageXOffset, window.pageYOffset];
+            if (target[0] || target[1]) {
+                scroll_cache.set(route_url, target);
+            } else {
+                scroll_cache.delete(route_url);
+            }
+        }
+
         // Update URL quickly, even though we'll do it again after module run because some
         // parts may depend on fetched resources.
-        updateHistory(route_mod.makeURL(), push_history);
+        route_url = route_mod.makeURL();
+        updateHistory(push_history);
 
         let view_el = document.querySelector('#th_view');
 
@@ -112,9 +130,15 @@ let thop = (function() {
         updateSession();
 
         // Update address bar and sidebar (basically)
-        let url = route_mod.makeURL();
-        updateHistory(url, false);
-        updateMenu(url);
+        route_url = route_mod.makeURL();
+        updateHistory(false);
+        updateMenu();
+
+        // Set appropriate scroll state
+        if (window.history.scrollRestoration || push_history) {
+            let target = scroll_cache.get(route_url) || [0, 0];
+            window.scrollTo(target[0], target[1]);
+        }
     };
 
     function updateSession() {
@@ -131,15 +155,15 @@ let thop = (function() {
         }
     }
 
-    function updateHistory(current_url, push_history) {
+    function updateHistory(push_history) {
         if (push_history) {
-            window.history.pushState(null, null, current_url);
+            window.history.pushState(null, null, route_url);
         } else {
-            window.history.replaceState(null, null, current_url);
+            window.history.replaceState(null, null, route_url);
         }
     }
 
-    function updateMenu(current_url) {
+    function updateMenu() {
         let links = [
             {category: 'Informations MCO', title: 'Tarifs GHS', func: () => mco_info.makeURL({mode: 'ghs'})},
             {category: 'Informations MCO', title: 'Arbre de groupage', func: () => mco_info.makeURL({mode: 'tree'})}
@@ -148,7 +172,7 @@ let thop = (function() {
         let prev_category = null;
         render(links.map(link => {
             let url = link.func();
-            let active = current_url.startsWith(url);
+            let active = route_url.startsWith(url);
 
             if (link.category === prev_category) {
                 return html`<a class=${active ? 'active' : ''} href=${url}>${link.title}</a>`;
