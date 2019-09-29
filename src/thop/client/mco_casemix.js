733cc0a4a6482c9aea663c716295b7a68c8f1a46
Niels Martignène <niels.martignene@protonmail.com>
2019-09-29 14:05:14 +0200

    Implement most mco_casemix options

--- src/thop/client/mco_casemix.js
+++ src/thop/client/mco_casemix.js
@@ -12,11 +12,30 @@ let mco_casemix = (function() {
         if (!settings.permissions.mco_casemix)
             throw new Error('Vous n\'avez pas accès à cette page');
 
+        // Fetch resources
+        let [ghm_roots, structures] = await Promise.all([
+            concepts.load('mco').then(mco => mco.ghm_roots),
+            fetch(`${env.base_url}api/structures.json`).then(response => response.json())
+        ]);
+
+        // Check options
+        if (!route.structure) {
+            route.structure = structures[0].name;
+        } else if (!structures.find(structure => structure.name === route.structure)) {
+            log.error(`La structure '${route.structure}' n'existe pas`);
+            route.structure = structures[0].name;
+        }
+        if (!route.main.units)
+            route.main.units = new Set(structures[0].entities.map(ent => ent.unit));
+        if (!route.main.ghm_roots)
+            route.main.ghm_roots = new Set(ghm_roots.map(ghm_root => ghm_root.code));
+
+        // Run appropriate mode handler
         switch (route.mode) {
-            case 'ghm': {} break;
-            case 'units': {} break;
+            case 'ghm': { await runSummary('ghm_root'); } break;
+            case 'units': { await runSummary('unit'); } break;
             case 'valorisation': { await runValorisation(); } break;
-            case 'rss': {} break;
+            case 'rss': { await runRSS(); } break;
 
             default: {
                 throw new Error(`Mode inconnu '${route.mode}'`);
@@ -39,8 +58,11 @@ let mco_casemix = (function() {
         args.main = {
             ...parseDateRange(path[1], settings.mco.casemix.max_date),
             algorithm: params.algorithm || settings.mco.casemix.default_algorithm,
+            units: params.units != null ? decodeUnits(params.units) : null,
+            ghm_roots: params.ghm_roots != null ? decodeGhmRoots(params.ghm_roots) : null
         };
         args.coeff = !!parseInt(params.coeff, 10) || false;
+        args.structure = params.structure || null;
 
         // Mode-specific part
         switch (args.mode) {
@@ -83,6 +105,62 @@ let mco_casemix = (function() {
         return default_range;
     }
 
+    function decodeUnits(str) {
+        let units = new Set;
+
+        try {
+            let encoded = atob(str);
+
+            let unit = 0;
+            for (let i = 0; i < encoded.length; i) {
+                let x = encoded.charCodeAt(i++);
+
+                if (x & 0x40) {
+                    x &= ~0x40;
+                    unit += (x & ~0x40);
+                } else {
+                    let y = encoded.charCodeAt(i++);
+                    let z = encoded.charCodeAt(i++);
+                    unit = (x << 12) + (y << 6) + z;
+                }
+
+                units.add(unit);
+            }
+        } catch (err) {
+            log.error('La chaîne d\'unités n\'est pas valide');
+        }
+
+        return units;
+    }
+
+    function decodeGhmRoots(str) {
+        let ghm_roots = new Set;
+
+        try {
+            let encoded = atob(str);
+
+            let x;
+            let y;
+            for (let i = 0; i < encoded.length;) {
+                let z = encoded.charCodeAt(i++);
+                if (z & 0x40) {
+                    z &= ~0x40;
+                } else {
+                    x = z || 90;
+                    y = encoded[i++];
+                    z = encoded.charCodeAt(i++);
+                }
+
+                let ghm_root = ('' + x).padStart(2, '0') + y + ('' + z).padStart(2, '0');
+                ghm_roots.add(ghm_root);
+            }
+        } catch (err) {
+            log.error('La chaîne de racines de GHM n\'est pas valide');
+        }
+
+        return ghm_roots;
+    }
+
     this.makeURL = function(args = {}) {
         args = util.assignDeep({}, route, args);
 
@@ -94,8 +172,11 @@ let mco_casemix = (function() {
         if (args.main.start_date && args.main.end_date)
             path.push(`${args.main.start_date}..${args.main.end_date}`);
 
-        params.algorithm = args.main.algorithm;
         params.coeff = 0 + args.coeff;
+        params.structure = args.structure;
+        params.algorithm = args.main.algorithm;
+        params.ghm_roots = args.main.ghm_roots ? encodeGhmRoots(args.main.ghm_roots) : null;
+        params.units = args.main.units ? encodeUnits(args.main.units) : null;
 
         // Mode-specific part
         switch (args.mode) {
@@ -108,30 +189,117 @@ let mco_casemix = (function() {
         return util.pasteURL(`${env.base_url}${path.join('/')}`, params);
     };
 
+    function encodeUnits(units) {
+        units = Array.from(units);
+
+        let encoded = '';
+        for (let i = 0; i < units.length; i++) {
+            let delta = units[i] - (i ? units[i - 1] : Infinity);
+            if (delta > 0 && delta < 64) {
+                let delta = units[i] - units[i - 1];
+                encoded += String.fromCharCode(delta | 0x40);
+            } else {
+                encoded += String.fromCharCode((units[i] >> 12) & 0x3F) +
+                           String.fromCharCode((units[i] >> 6) & 0x3F) +
+                           String.fromCharCode(units[i] & 0x3F);
+            }
+        }
+
+        return btoa(encoded);
+    }
+
+    function encodeGhmRoots(ghm_roots) {
+        ghm_roots = Array.from(ghm_roots);
+
+        let encoded = '';
+
+        for (let i = 0; i < ghm_roots.length; i++) {
+            if (i && ghm_roots[i].substr(0, 3) == ghm_roots[i - 1].substr(0, 3)) {
+                let z = parseInt(ghm_roots[i].substr(3), 10);
+                encoded += String.fromCharCode((z & 0x3F) | 0x40);
+            } else {
+                let x = parseInt(ghm_roots[i].substr(0, 2), 10);
+                if (x === 90)
+                    x = 0;
+                let y = ghm_roots[i][2];
+                let z = parseInt(ghm_roots[i].substr(3), 10);
+                encoded += String.fromCharCode(x & 0x3F) + y +
+                                 String.fromCharCode(z & 0x3F);
+            }
+        }
+
+        return btoa(encoded);
+    }
+
+    // ------------------------------------------------------------------------
+    // Summaries
+    // ------------------------------------------------------------------------
+
+    async function runSummary(by) {
+        let [ghm_roots, structures] = await Promise.all([
+            concepts.load('mco').then(mco => mco.ghm_roots),
+            fetch(`${env.base_url}api/structures.json`).then(response => response.json())
+        ]);
+
+        // Options
+        render(html`
+            ${renderPeriodPicker(settings.mco.casemix.min_date, settings.mco.casemix.max_date,
+                                 route.main.start_date, route.main.end_date)}
+            ${renderAlgorithmSelector(settings.mco.casemix.algorithms, route.main.algorithm)}
+            ${renderCoefficientCheckbox(route.coeff)}
+            ${renderUnitTree(structures, route.structure, route.main.units)}
+            ${renderGhmRootTree(ghm_roots, route.main.ghm_roots)}
+        `, document.querySelector('#th_options'));
+
+        // View
+        render(html``, document.querySelector('#th_view'));
+    }
+
     // ------------------------------------------------------------------------
     // Valorisation
     // ------------------------------------------------------------------------
 
     async function runValorisation() {
-        let ghm_roots = await concepts.load('mco').then(mco => mco.ghm_roots);
+        let [ghm_roots, structures] = await Promise.all([
+            concepts.load('mco').then(mco => mco.ghm_roots),
+            fetch(`${env.base_url}api/structures.json`).then(response => response.json())
+        ]);
 
         if (!route.valorisation.ghm_root)
             route.valorisation.ghm_root = ghm_roots[0].code;
 
         // Options
         render(html`
-            ${renderPeriodPicker(route.main.start_date, route.main.end_date)}
-            <label>Algorithme <select @change=${e => thop.go(self, {main: {algorithm: e.target.value}})}>
-                ${settings.mco.casemix.algorithms.map(algo => {
-                    let selected = (route.main.algorithm === algo);
-                    let label = `Algorithme ${algo}${algo === settings.mco.casemix.default_algorithm ? '*' : ''}`;
-
-                    return html`<option value=${algo} .selected=${selected}>${label}</option>`;
-                })}
-            </select></label>
-            <label>Coefficient <input type="checkbox" .checked=${route.coeff}
-                                      @change=${e => thop.go(self, {coeff: e.target.checked})}/></label>
+            ${renderPeriodPicker(settings.mco.casemix.min_date, settings.mco.casemix.max_date,
+                                 route.main.start_date, route.main.end_date)}
+            ${renderAlgorithmSelector(settings.mco.casemix.algorithms, route.main.algorithm)}
+            ${renderCoefficientCheckbox(route.coeff)}
+            ${renderUnitTree(structures, route.structure, route.main.units)}
             ${renderGhmRootSelector(ghm_roots, route.valorisation.ghm_root)}
+        `, document.querySelector('#th_options'));
+
+        // View
+        render(html``, document.querySelector('#th_view'));
+    }
+
+    // ------------------------------------------------------------------------
+    // RSS
+    // ------------------------------------------------------------------------
+
+    async function runRSS() {
+        let [ghm_roots, structures] = await Promise.all([
+            concepts.load('mco').then(mco => mco.ghm_roots),
+            fetch(`${env.base_url}api/structures.json`).then(response => response.json())
+        ]);
+
+        // Options
+        render(html`
+            ${renderPeriodPicker(settings.mco.casemix.min_date, settings.mco.casemix.max_date,
+                                 route.main.start_date, route.main.end_date)}
+            ${renderAlgorithmSelector(settings.mco.casemix.algorithms, route.main.algorithm)}
+            ${renderCoefficientCheckbox(route.coeff)}
+            ${renderUnitTree(structures, route.structure, route.main.units)}
+            ${renderGhmRootTree(ghm_roots, route.main.ghm_roots)}
         `, document.querySelector('#th_options'));
 
         // View
@@ -142,16 +310,67 @@ let mco_casemix = (function() {
     // Options
     // ------------------------------------------------------------------------
 
-    function renderPeriodPicker(start_date, end_date) {
+    function renderPeriodPicker(min_date, max_date, start_date, end_date) {
         let ppik = new PeriodPicker;
 
         ppik.changeHandler = (start_date, end_date) =>
             thop.go(self, {main: {start_date: start_date, end_date: end_date}});
 
-        ppik.setRange(settings.mco.casemix.min_date, settings.mco.casemix.max_date);
+        ppik.setRange(min_date, max_date);
         ppik.setDates(start_date, end_date);
 
         return ppik.render();
+    }
+
+    function renderAlgorithmSelector(algorithms, current_algorithm) {
+        return html`
+            <label>Algorithme <select @change=${e => thop.go(self, {main: {algorithm: e.target.value}})}>
+                ${settings.mco.casemix.algorithms.map(algo => {
+                    let selected = (route.main.algorithm === algo);
+                    let label = `Algorithme ${algo}`;
+
+                    return html`<option value=${algo} .selected=${selected}>${label}</option>`;
+                })}
+            </select></label>
+        `;
+    }
+
+    function renderCoefficientCheckbox(current_coeff) {
+        return html`<label>Coefficient <input type="checkbox" .checked=${current_coeff}
+                                              @change=${e => thop.go(self, {coeff: e.target.checked})}/></label>`;
+    }
+
+    function renderUnitTree(structures, current_structure, current_units) {
+        let tsel = new TreeSelector;
+
+        tsel.changeHandler = (values, tab) =>
+            thop.go(self, {main: {units: values}, structure: tab});
+
+        tsel.setPrefix('Unités : ');
+        for (let structure of structures) {
+            tsel.addTab(structure.name);
+            for (ent of structure.entities)
+                tsel.addOption(ent.path, ent.unit, {selected: current_units.has(ent.unit)});
+        }
+        tsel.setCurrentTab(current_structure);
+
+        return tsel.render();
+    }
+
+    function renderGhmRootTree(ghm_roots, current_ghm_roots) {
+        let tsel = new TreeSelector;
+
+        tsel.changeHandler = (values, tab) =>
+            thop.go(self, {main: {ghm_roots: values}});
+
+        tsel.setPrefix('Racines de GHM : ');
+        for (let ghm_root of ghm_roots) {
+            let title = `${ghm_root.code} - ${ghm_root.desc}`;
+            tsel.addOption(title, ghm_root.code, {selected: current_ghm_roots.has(ghm_root.code)});
+        }
+        // tsel.setCurrentTab(current_structure);
+
+        return tsel.render();
     }
 
     function renderGhmRootSelector(ghm_roots, current_ghm_root) {
