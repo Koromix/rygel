d855c0156b5d2cc1220bc8328fa8dc74e578d756
Niels Martignène <niels.martignene@protonmail.com>
2019-10-01 17:40:44 +0200

    Don't intercept anchor clicks after e.preventDefault()

--- src/goupil/client/goupil.js
+++ src/goupil/client/goupil.js
@@ -83,7 +83,7 @@ let goupil = (function() {
         });
 
         document.body.addEventListener('click', e => {
-            if (e.target && e.target.tagName == 'A' &&
+            if (e.target && e.target.tagName == 'A' && !e.defaultPrevented &&
                     !e.ctrlKey && !e.target.getAttribute('download')) {
                 let href = e.target.getAttribute('href');
                 if (href && !href.match(/^(?:[a-z]+:)?\/\//) && href[0] != '#') {
