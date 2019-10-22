aa8c20c0f995c64249748fb689d3440f0249ea3f
Niels Martignène <niels.martignene@protonmail.com>
2019-10-22 02:31:45 +0200

    Expose goupil forms and schedules in Application object

--- src/goupil/client/form_data.js
+++ src/goupil/client/form_data.js
@@ -2,9 +2,10 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-function Application() {
-    let self = this;
+function FormData(values = {}) {
+    this.values = values;
 
-    this.forms = [];
-    this.schedules = [];
+    this.missing_errors = new Set;
+    this.sections_state = {};
+    this.file_lists = new Map;
 }
