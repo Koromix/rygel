0898e7a795805c2016713fe6bde2e87943e9d1d7
Niels Martignène <niels.martignene@protonmail.com>
2019-10-27 14:54:16 +0100

    Add changed property to variable interface object

--- src/goupil/client/form_executor.js
+++ src/goupil/client/form_executor.js
@@ -3,15 +3,18 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 function FormExecutor(values = {}) {
+    let self = this;
+
     // Storage for user scripts
     this.memory = {};
 
     // Page state
     this.missing_errors = new Set;
+    this.changed_variables = new Set;
     this.sections_state = {};
     this.file_lists = new Map;
 
-    // Key and value handling
+    // Key handling
     this.decodeKey = function(key) {
         if (!key)
             throw new Error('Empty keys are not allowed');
@@ -25,7 +28,14 @@ function FormExecutor(values = {}) {
 
         return key;
     };
-    this.setValue = function(key, value) { values[key] = value; };
+
+    // Value handling
+    this.setValue = function(key, value) {
+        values[key] = value;
+
+        self.missing_errors.delete(key.toString());
+        self.changed_variables.add(key.toString());
+    };
     this.getValue = function(key, default_value) {
         if (values.hasOwnProperty(key)) {
             return values[key];
