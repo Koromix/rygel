ca52f99df61a025e79d6ce5c68513d4b1d85cd43
Niels Martignène <niels.martignene@protonmail.com>
2019-11-24 22:08:39 +0100

    Move server SSE code to dedicated source files

---
+++ src/goupile/server/events.cc
@@ -0,0 +1,140 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include "../../libcc/libcc.hh"
+#include "config.hh"
+#include "events.hh"
+#include "goupile.hh"
+
+namespace RG {
+
+struct PushContext {
+    PushContext *prev;
+    PushContext *next;
+
+    MHD_Connection *conn;
+    bool suspended = false;
+    std::atomic_uint events = 0;
+};
+
+static bool run = true;
+
+static std::mutex mutex;
+static PushContext root = {&root, &root};
+
+static ssize_t SendClientEvents(void *cls, uint64_t, char *buf, size_t max)
+{
+    PushContext *ctx = (PushContext *)cls;
+
+    if (run) {
+        if (ctx->events) {
+            int ctz = CountTrailingZeros(ctx->events);
+            ctx->events &= ~(1u << ctz);
+
+            // FIXME: This may result in truncation when max is very low
+            return Fmt(MakeSpan(buf, max), "event: %1\ndata: {}\n\n", EventTypeNames[ctz]).len;
+        } else {
+            if (!ctx->suspended) {
+                MHD_suspend_connection(ctx->conn);
+                ctx->suspended = true;
+            }
+
+            // libmicrohttpd crashes (assert) if you return 0
+            buf[0] = '\n';
+            return 1;
+        }
+    } else {
+        return MHD_CONTENT_READER_END_OF_STREAM;
+    }
+}
+
+static void UnregisterEventConnection(PushContext *ctx)
+{
+    std::lock_guard lock(mutex);
+
+    ctx->prev->next = ctx->next;
+    ctx->next->prev = ctx->prev;
+}
+
+void CloseAllEventConnections()
+{
+    std::unique_lock<std::mutex> lock(mutex);
+
+    run = false;
+
+    // Wake up all SSE connections
+    PushContext *ctx = root.next;
+    while (ctx != &root) {
+        if (ctx->suspended) {
+            MHD_resume_connection(ctx->conn);
+            ctx->suspended = false;
+        }
+
+        ctx = ctx->next;
+    }
+
+    // Wait until all SSE connections are over
+    while (root.prev != root.next) {
+        lock.unlock();
+        WaitForDelay(20);
+        lock.lock();
+    }
+}
+
+void HandleEvents(const http_RequestInfo &request, http_IO *io)
+{
+    PushContext *ctx = (PushContext *)Allocator::Allocate(&io->allocator, RG_SIZE(*ctx));
+    new (ctx) PushContext();
+    ctx->conn = request.conn;
+
+    // Issuing keepalive is better for Firefox. For a start, the open event gets triggered.
+    ctx->events = 1u << (int)EventType::KeepAlive;
+
+    // Register SSE connection
+    {
+        std::lock_guard lock(mutex);
+
+        if (!run) {
+            LogError("Server is shutting down");
+            return;
+        }
+
+        root.prev->next = ctx;
+        ctx->next = &root;
+        ctx->prev = root.prev;
+        root.prev = ctx;
+    }
+
+    MHD_Response *response =
+        MHD_create_response_from_callback(MHD_SIZE_UNKNOWN, 1024, SendClientEvents, ctx,
+                                          [](void *cls) { UnregisterEventConnection((PushContext *)cls); });
+
+    io->AttachResponse(200, response);
+    io->AddHeader("Content-Type", "text/event-stream");
+    io->AddHeader("Cache-Control", "no-cache");
+    io->AddHeader("Connection", "keep-alive");
+}
+
+static void PushEvents(unsigned int events)
+{
+    std::lock_guard<std::mutex> lock(mutex);
+
+    PushContext *ctx = root.next;
+    while (ctx != &root) {
+        ctx->events |= events;
+        if (ctx->suspended) {
+            MHD_resume_connection(ctx->conn);
+            ctx->suspended = false;
+        }
+
+        ctx = ctx->next;
+    }
+}
+
+void PushEvent(EventType type)
+{
+    PushEvents(1u << (int)type);
+}
+
+}
--- src/goupile/server/events.hh
+++ src/goupile/server/events.hh
@@ -5,12 +5,22 @@
 #pragma once
 
 #include "../../libcc/libcc.hh"
-#include "config.hh"
-#include "data.hh"
 
 namespace RG {
 
-extern Config goupile_config;
-extern SQLiteDatabase goupile_db;
+enum class EventType {
+    Schedule,
+    KeepAlive
+};
+static const char *const EventTypeNames[] = {
+    "schedule",
+    "keepalive"
+};
+
+void CloseAllEventConnections();
+
+void HandleEvents(const http_RequestInfo &request, http_IO *io);
+
+void PushEvent(EventType type);
 
 }
