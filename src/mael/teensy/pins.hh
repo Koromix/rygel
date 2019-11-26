92011acef0cf1f5f953cf897c35cf5c0fd162870
Niels Martignène <niels.martignene@protonmail.com>
2019-11-26 22:58:28 +0100

    Move pin definitions to dedicated header file

---
+++ src/mael/teensy/pins.hh
@@ -0,0 +1,27 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#pragma once
+
+#include "util.hh"
+
+// Encoder pins
+#define PIN_ENC0_INT 12
+#define PIN_ENC0_DIR 16
+#define PIN_ENC1_INT 13
+#define PIN_ENC1_DIR 17
+#define PIN_ENC2_INT 14
+#define PIN_ENC2_DIR 18
+#define PIN_ENC3_INT 15
+#define PIN_ENC3_DIR 19
+
+// DC driver pins
+#define PIN_DC0_DIR 22
+#define PIN_DC0_PWM 26
+#define PIN_DC1_DIR 23
+#define PIN_DC1_PWM 27
+#define PIN_DC2_DIR 24
+#define PIN_DC2_PWM 28
+#define PIN_DC3_DIR 25
+#define PIN_DC3_PWM 29
