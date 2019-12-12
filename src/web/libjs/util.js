fea25279fad4705553bf55420da60dd9c7839aa3
Niels Martignène <niels.martignene@protonmail.com>
2019-12-13 00:52:45 +0100

    Add size formatting utility functions to libjs

--- src/web/libjs/util.js
+++ src/web/libjs/util.js
@@ -330,6 +330,30 @@ let util = new function() {
             }
         });
     };
+
+    this.formatDiskSize = function(size) {
+        if (size >= 1000 * 1000 * 1000) {
+            return `${(size / 1000 / 1000 / 1000).toFixed(1)} GB`;
+        } else if (size >= 1000 * 1000) {
+            return `${(size / 1000 / 1000).toFixed(1)} MB`;
+        } else if (size >= 1000) {
+            return `${(size / 1000).toFixed(1)} kB`;
+        } else {
+            return `${size} B`;
+        }
+    };
+
+    this.formatDiskMemorySize = function(size) {
+        if (size >= 1024 * 1024 * 1024) {
+            return `${(size / 1024 / 1024 / 1024).toFixed(1)} GB`;
+        } else if (size >= 1024 * 1024) {
+            return `${(size / 1024 / 1024).toFixed(1)} MB`;
+        } else if (size >= 1024) {
+            return `${(size / 1024).toFixed(1)} kB`;
+        } else {
+            return `${size} B`;
+        }
+    };
 };
 
 // ------------------------------------------------------------------------
