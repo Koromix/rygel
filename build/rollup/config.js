8fbc771321fc885bc19747883a77a0ce18df8126
Niels Martignène <niels.martignene@protonmail.com>
2019-12-31 19:31:15 +0100

    Configure rollup/babel for JS transpilation (Chrome 42, IE11)

---
+++ build/rollup/config.js
@@ -0,0 +1,48 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import babel from 'rollup-plugin-babel';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+// Tried @rollup/plugin-virtual, it did weird crap
+import hypothetical from 'rollup-plugin-hypothetical';
+
+let code;
+try {
+    const fs = require('fs');
+    code = fs.readFileSync(0, 'UTF-8');
+} catch (err) {
+    // Nothing on stdin (probably)
+    code = '';
+}
+
+module.exports = {
+    input: '__stdin__.js',
+    plugins: [
+        babel({
+            babelrc: false,
+            exclude: 'node_modules/**',
+            presets: [
+                ['@babel/env', {
+                    targets: {
+                        ie: '11',
+                        chrome: '42'
+                    },
+                    useBuiltIns: 'usage',
+                    corejs: 3,
+                    modules: false
+                }]
+            ]
+        }),
+        resolve(),
+        commonjs(),
+        hypothetical({
+            files: {
+                '__stdin__.js': code
+            },
+            allowFallthrough: true,
+            leaveIdsAlone: true
+        })
+    ]
+};
