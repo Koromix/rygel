fd37aa80844889998cfacd5502d3944b9e435cff
Niels Martignène <niels.martignene@protonmail.com>
2020-04-21 21:41:47 +0200

    Better separate context and message in blik errors

--- src/blik/util.hh
+++ src/blik/util.hh
@@ -29,18 +29,15 @@ void ReportError(Span<const char> code, const char *filename, int line, Size off
     int column = (int)(offset - (extract.ptr - code.ptr)) + 1;
 
     if (LogUsesTerminalOutput()) {
-        Print(stderr, "\x1B[38;5;202m%1(%2:%3):\x1B[0m \x1B[31m", filename, line, column);
+        Print(stderr, "\x1B[91m%1(%2:%3):\x1B[0m \x1B[1m", filename, line, column);
         PrintLn(stderr, fmt, args...);
-        Print(stderr, "\x1B[0m");
-
-        PrintLn(stderr, "    %1", extract);
-        PrintLn(stderr, "   %1\x1B[38;5;202m^^^\x1B[0m", FmtArg(' ').Repeat(column));
+        PrintLn(stderr, "%1 |\x1B[0m  %2", FmtArg(line).Pad(-7), extract);
+        PrintLn(stderr, "        | %1\x1B[95m^^^\x1B[0m", FmtArg(' ').Repeat(column));
     } else {
         Print(stderr, "%1(%2:%3): ", filename, line, column);
         PrintLn(stderr, fmt, args...);
-
-        PrintLn(stderr, "    %1", extract);
-        PrintLn(stderr, "   %1^^^", FmtArg(' ').Repeat(column));
+        PrintLn(stderr, "%1 |  %2", FmtArg(line).Pad(-7), extract);
+        PrintLn(stderr, "        | %1^^^", FmtArg(' ').Repeat(column));
     }
 }
 
