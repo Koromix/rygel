b6921ac6e76e78b49f8a5fe33c3e9ee58c6c978c
Niels Martignène <niels.martignene@protonmail.com>
2020-04-22 23:20:22 +0200

    Adjust reported column for single-character tokens

--- src/blik/util.hh
+++ src/blik/util.hh
@@ -43,26 +43,27 @@ template <typename... Args>
 void ReportError(Span<const char> code, const char *filename, int line, Size offset,
                  const char *fmt, Args... args)
 {
-    // We point the user to error location with '^^^', if the token is a single
-    // character (e.g. operator) we want the second caret to be centered on it.
-    // There is a small trap: we can't do that if the character before is a tabulation,
-    // see below for tab handling.
-    offset -= (offset > 0 && code[offset - 1] == ' ' &&
-               offset + 1 < code.len && IsAsciiWhite(code[offset + 1]));
-
-    // Extract code line
-    int column = 1;
+    // Find entire code line and compute column from offset
+    int column = 0;
     Span<const char> extract = MakeSpan(code.ptr + offset, 0);
     while (extract.ptr > code.ptr && extract.ptr[-1] != '\n') {
         extract.ptr--;
         extract.len++;
 
-        // Ignore UTF-8 trailing bytes
+        // Ignore UTF-8 trailing bytes to count code points. Not perfect (we want
+        // to count graphemes), but close enough for now.
         column += ((extract.ptr[0] & 0xC0) != 0x80);
     }
     while (extract.end() < code.end() && !strchr("\r\n", extract.ptr[extract.len])) {
         extract.len++;
     }
+
+    // We point the user to error location with '^^^', but if the token is a single
+    // character (e.g. operator) we want the second caret to be centered on it.
+    // There is a small trap: we can't do that if the character before is a tabulation,
+    // see below for tab handling.
+    bool center = (offset > 0 && code[offset - 1] == ' ' &&
+                   offset + 1 < code.len && IsAsciiWhite(code[offset + 1]));
 
     // Because we accept tabulation users, including the crazy ones who may put tabulations
     // after other characters, we can't just repeat ' ' (column - 1) times to align the
@@ -71,23 +72,23 @@ void ReportError(Span<const char> code, const char *filename, int line, Size off
     char align[1024];
     int align_more;
     {
-        int align_len = std::min((int)RG_SIZE(align) - 1, column - 1);
+        int align_len = std::min((int)RG_SIZE(align) - 1, column - center);
         for (Size i = 0; i < align_len; i++) {
             align[i] = (extract[i] == '\t') ? '\t' : ' ';
         }
         align[align_len] = 0;
 
-        // Tabulations and very long lines... we'll try out best, but you really need to stop...
-        align_more = column - align_len - 1;
+        // Tabulations and very long lines... if you can read this comment: just stop.
+        align_more = column - center - align_len;
     }
 
     if (EnableAnsiOutput()) {
-        Print(stderr, "\x1B[91m%1(%2:%3):\x1B[0m \x1B[1m", filename, line, column);
+        Print(stderr, "\x1B[91m%1(%2:%3):\x1B[0m \x1B[1m", filename, line, column + 1);
         PrintLn(stderr, fmt, args...);
         PrintLn(stderr, "%1 |\x1B[0m  %2", FmtArg(line).Pad(-7), extract);
         PrintLn(stderr, "        |  %1%2\x1B[95m^^^\x1B[0m", align, FmtArg(' ').Repeat(align_more));
     } else {
-        Print(stderr, "%1(%2:%3): ", filename, line, column);
+        Print(stderr, "%1(%2:%3): ", filename, line, column + 1);
         PrintLn(stderr, fmt, args...);
         PrintLn(stderr, "%1 |  %2", FmtArg(line).Pad(-7), extract);
         PrintLn(stderr, "        |  %1%2^^^", align, FmtArg(' ').Repeat(align_more));
