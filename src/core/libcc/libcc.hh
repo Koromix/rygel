f32eaa0f53fffef65615b2f7b09b68d89605efe3
Niels Martignène <niels.martignene@protonmail.com>
2020-04-28 18:09:43 +0200

    Drop unused Win32-specific function GetApplicationExecutable83()

--- src/core/libcc/libcc.hh
+++ src/core/libcc/libcc.hh
@@ -3497,9 +3497,6 @@ const char *GetWorkingDirectory();
 
 const char *GetApplicationExecutable(); // Can be NULL
 const char *GetApplicationDirectory(); // Can be NULL
-#ifdef _WIN32
-const char *GetApplicationExecutable83(); // Can be NULL
-#endif
 
 bool MakeDirectory(const char *directory, bool error_if_exists = true);
 bool MakeDirectoryRec(Span<const char> directory);
