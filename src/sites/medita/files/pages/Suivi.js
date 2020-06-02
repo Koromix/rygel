844a09a28df1f6a910f251cfa8ef4eae55f6f0e2
Niels Martignène <niels.martignene@protonmail.com>
2020-06-02 04:07:03 +0200

    Show Medita score information in tables below graph

--- src/sites/medita/files/pages/Suivi.js
+++ src/sites/medita/files/pages/Suivi.js
@@ -1,98 +1,3 @@
-function addChart(id, echelles) {
-    let colors = ['#236b20', '#24579d', 'rgb(187, 35, 35)', 'black'];
-    let canvas = scratch.canvas;
-
-    if (!canvas) {
-        canvas = document.createElement('canvas');
-        scratch.canvas = canvas;
-    }
-
-    let p = Promise.all([
-        window.Chart || util.loadScript(`${env.base_url}files/resources/chart.bundle.min.js`),
-        ...echelles.map(echelle => virt_data.loadAll(echelle.form))
-    ]);
-
-    p.then(tables => {
-        // Remove parasite script thingy
-        tables.shift();
-
-        let chart = scratch.chart;
-
-        if (!chart) {
-            chart = new Chart(canvas.getContext('2d'), {
-                type: 'line',
-                data: {
-                    labels: [],
-                    datasets: []
-                },
-                options: {
-                    legend: {
-                        position: 'bottom',
-                        align: 'start'
-                    },
-                    scales: {
-                        xAxes: [{
-                            type: 'time',
-
-                            time: {
-                                unit: 'day',
-                                unitStepSize: 1,
-                                displayFormats: {
-                                   day: 'DD/MM HH:mm'
-                                }
-                            },
-
-                            ticks: {
-                                source: 'data',
-                                minRotation: 90,
-                                maxRotation: 90
-                            }
-                        }],
-                        yAxes: [{type: 'linear'}]
-                    }
-                }
-            });
-            scratch.chart = chart;
-        }
-        chart.data.datasets.length = 0;
-
-        for (let i = 0; i < tables.length; i++) {
-            let records = tables[i].filter(record => record.values.id === id);
-            let echelle = echelles[i];
-            let keys = echelle.keys;
-
-            if (records.length) {
-                for (let j = 0; j < keys.length; j++) {
-                    let dataset = {
-                        label: keys[j] === 'score' ? echelle.name : `${echelle.name} [${keys[j]}]`,
-                        borderColor: colors[j % colors.length],
-                        fill: false,
-                        pointBorderColor: [],
-                        pointBackgroundColor:[],
-                        data: []
-                    };
-                    chart.data.datasets.push(dataset);
-
-                    for (let record of records) {
-                        dataset.pointBorderColor.push(colors[j % colors.length]);
-                        dataset.pointBackgroundColor.push(colors[j % colors.length]);
-
-                        dataset.data.push({
-                            x: new Date(util.decodeULIDTime(record.id)),
-                            y: record.values[keys[j]]
-                        });
-                    }
-                }
-            }
-        }
-
-        canvas.style.display = chart.data.datasets.length ? 'block' : 'none';
-        chart.update({duration: 0});
-    });
-
-    page.output(canvas);
-}
-
 data.makeFormHeader("Suivi d'échelles", page)
 page.pushOptions({compact: true})
 
@@ -117,11 +22,162 @@ let id = form.find("id");
     page.output(tsel.render());
 }
 
+// Graphique et tableaux
 if (id.value) {
     let forms = new Set(scratch.echelles || data.echelles.map(echelle => echelle.form));
     let echelles = data.echelles.filter(echelle => forms.delete(echelle.form));
 
-    addChart(id.value, echelles);
+    let p = Promise.all([
+        window.Chart || util.loadScript(`${env.base_url}files/resources/chart.bundle.min.js`),
+        ...echelles.map(echelle => virt_data.loadAll(echelle.form))
+    ]);
+
+    scratch.canvas = scratch.canvas || document.createElement('canvas');
+    scratch.div = scratch.div || document.createElement('div');
+    page.output(scratch.canvas);
+    page.output(scratch.div);
+
+    p.then(tables => {
+        // Remove parasite script thingy
+        tables.shift();
+        tables = tables.map(table => table.filter(record => record.values.id === id.value));
+
+        updateChart(echelles, tables);
+        updateTables(echelles, tables);
+    });
+}
+
+function updateChart(echelles, tables) {
+    let canvas = scratch.canvas;
+    let chart = scratch.chart;
+
+    let colors = ['#236b20', '#24579d', 'rgb(187, 35, 35)', 'black'];
+
+    if (!chart) {
+        chart = new Chart(canvas.getContext('2d'), {
+            type: 'line',
+            data: {
+                labels: [],
+                datasets: []
+            },
+            options: {
+                legend: {
+                    position: 'bottom',
+                    align: 'start'
+                },
+                scales: {
+                    xAxes: [{
+                        type: 'time',
+
+                        time: {
+                            unit: 'day',
+                            unitStepSize: 1,
+                            displayFormats: {
+                               day: 'DD/MM HH:mm'
+                            }
+                        },
+
+                        ticks: {
+                            source: 'data',
+                            minRotation: 90,
+                            maxRotation: 90
+                        }
+                    }],
+                    yAxes: [{type: 'linear'}]
+                },
+                onClick: function(x, e) { console.log(x, e) }
+            }
+        });
+        scratch.chart = chart;
+    }
+    chart.data.datasets.length = 0;
+
+    for (let i = 0; i < echelles.length; i++) {
+        let echelle = echelles[i];
+        let keys = echelle.keys;
+        let records = tables[i];
+
+        if (records.length) {
+            for (let j = 0; j < keys.length; j++) {
+                let dataset = {
+                    label: keys[j] === 'score' ? echelle.name : `${echelle.name} [${keys[j]}]`,
+                    borderColor: colors[j % colors.length],
+                    fill: false,
+                    pointBorderColor: [],
+                    pointBackgroundColor:[],
+                    data: []
+                };
+                chart.data.datasets.push(dataset);
+
+                for (let record of records) {
+                    dataset.pointBorderColor.push(colors[j % colors.length]);
+                    dataset.pointBackgroundColor.push(colors[j % colors.length]);
+
+                    dataset.data.push({
+                        x: new Date(util.decodeULIDTime(record.id)),
+                        y: record.values[keys[j]]
+                    });
+                }
+            }
+        }
+    }
+
+    canvas.style.display = chart.data.datasets.length ? 'block' : 'none';
+    chart.update({duration: 0});
+}
+
+function updateTables(echelles, tables) {
+    let div = scratch.div;
+
+    render(echelles.map((echelle, idx) => {
+        let records = tables[idx];
+        let keys = echelle.keys;
+
+        if (records.length) {
+            return html`
+                <fieldset class="af_container af_section">
+                    <legend>${echelle.name}</legend>
+                    <table style="width: 100%;">
+                        <thead>
+                            <tr>
+                                <th>Date</th>
+                                ${keys.map(key => html`<th>${key}</th>`)}
+                                <th colspan="2"></th>
+                            </tr>
+                        </thead>
+                        <tbody>${records.map(record => {
+                            let date = new Date(util.decodeULIDTime(record.id));
+                            let values = keys.map(key => record.values[key]);
+
+                            return html`<tr>
+                                <td>${date.toLocaleString()}</td>
+                                ${values.map(value => html`<td>${value}</td>`)}
+                                <td><a href=${nav.link(echelle.form, null, record)}>Modifier</a></td>
+                                <td><a href="#" @click=${e => {showDeleteDialog(e, echelle.form, record.id); e.preventDefault();}}>Supprimer</a></td>
+                            </tr>`;
+                        })}</tbody>
+                    </table>
+                </fieldset>
+            `;
+        } else {
+            return '';
+        }
+    }), div);
+}
+
+function showDeleteDialog(e, form, id) {
+    ui.popup(e, popup => {
+        popup.output('Voulez-vous vraiment supprimer cet enregistrement ?');
+        
+        popup.submitHandler = async () => {
+            popup.close();
+
+            await virt_data.delete(form, id);
+            page.restart();
+        };
+        popup.buttons(popup.buttons.std.ok_cancel('Supprimer'));
+
+    });
 }
 
 form.output(html`
