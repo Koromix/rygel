1d2e282249ac8944d5aeca75d3ce6d47e370b678
Niels Martignène <niels.martignene@protonmail.com>
2020-07-21 22:23:51 +0200

    Fix broken goupile file widget

--- vendor/lit-html/lit-html.js
+++ vendor/lit-html/lit-html.js
@@ -1,6 +1,6 @@
-import { html, svg, render } from 'lit-html/lit-html';
+import { html, svg, render, directive } from 'lit-html/lit-html';
 import { guard } from 'lit-html/directives/guard';
 import { live } from 'lit-html/directives/live';
 import { until } from 'lit-html/directives/until';
 
-export { html, svg, render, guard, live, until };
+export { html, svg, render, directive, guard, live, until };
