894811a5a7a81004f33d6acabcf3b9353a429f6a
Niels Martignène <niels.martignene@protonmail.com>
2020-10-01 07:30:33 +0200

    Split main Configuration menu in two

--- src/goupile/client/goupile.js
+++ src/goupile/client/goupile.js
@@ -449,16 +449,12 @@ let goupile = new function() {
 
         return html`
             ${show_develop ? html`
-                <div class="gp_dropdown">
-                    <button class=${left_panel === 'editor' || left_panel === 'files' ? 'icon active' : 'icon'}
-                            style="background-position-y: calc(-228px + 1.2em)">Configuration</button>
-                    <div>
-                        <button class=${left_panel === 'editor' ? 'active' : ''}
-                                @click=${e => toggleLeftPanel('editor')}>Code</button>
-                        <button class=${left_panel === 'files' ? 'active' : ''}
-                                @click=${e => toggleLeftPanel('files')}>Fichiers</button>
-                    </div>
-                </div>
+                <button class=${left_panel === 'files' ? 'icon active' : 'icon'}
+                        style="font-weight: bold; background-position-y: calc(-538px + 1.2em)"
+                        @click=${e => toggleLeftPanel('files')}>${env.app_name}</button>
+                <button class=${left_panel === 'editor' ? 'icon active' : 'icon'}
+                        style="background-position-y: calc(-228px + 1.2em)"
+                        @click=${e => toggleLeftPanel('editor')}>Code</button>
             ` : ''}
             ${show_data ? html`
                 <div class="gp_dropdown">
