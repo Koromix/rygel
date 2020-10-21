b1c9075627f952ae17adb3dfa87adeb155a5e005
Niels Martignène <niels.martignene@protonmail.com>
2020-10-21 20:48:45 +0200

    Add goupile.fr instance sync tool to repository

---
+++ src/goupile/admin/sync_server.py
@@ -0,0 +1,93 @@
+#!/bin/env python3
+
+import configparser
+import io
+import os
+import re
+import sys
+import subprocess
+
+PROFILES_DIRECTORY = '/srv/www/goupile/profiles'
+NGINX_FILE = '/srv/www/goupile/nginx_goupile.conf'
+
+def list_fs_instances():
+    instances = {}
+
+    for directory in os.listdir(PROFILES_DIRECTORY):
+        filename = os.path.join(PROFILES_DIRECTORY, directory, 'goupile.ini')
+
+        if os.path.isfile(filename):
+            config = configparser.ConfigParser()
+            config.optionxform = str
+            config.read(filename)
+
+            info = {
+                'base_url': config.get('HTTP', 'BaseUrl'),
+                'port': config.getint('HTTP', 'Port')
+            }
+            instances[directory] = info
+
+    return instances
+
+def list_systemd_instances():
+    instances = {}
+
+    output = subprocess.check_output(['systemctl', 'list-units', '--type=service', '--all'])
+    output = output.decode()
+
+    for line in output.splitlines():
+        parts = re.split(' +', line)
+
+        if len(parts) >= 4:
+            match = re.search('^goupile@([0-9A-Za-z_\\-]+)\\.service$', parts[1])
+
+            if match is not None:
+                name = match.group(1)
+                instances[name] = (parts[3] == 'active')
+
+    return instances
+
+def run_systemd_instance(instance, cmd):
+    service = f'goupile@{instance}.service'
+    print(f'{cmd.capitalize()} {service}', file = sys.stderr)
+    subprocess.run(['systemctl', cmd, '--quiet', service])
+
+def write_nginx_entry(f, info):
+    print(f'location {info["base_url"]} {{', file = f)
+    print(f'    proxy_pass http://127.0.0.1:{info["port"]};', file = f)
+    print(f'}}', file = f)
+
+if __name__ == '__main__':
+    fs_instances = list_fs_instances()
+    systemd_instances = list_systemd_instances()
+
+    # Sanity checks
+    ports = {}
+    for instance, info in fs_instances.items():
+        prev_instance = ports.get(info['port'])
+        if prev_instance is not None:
+            raise ValueError(f'Port {info["port"]} is used by {prev_instance} and {instance}')
+        ports[info['port']] = instance
+
+    # Update NGINX locations
+    print('Update NGINX configuration', file = sys.stderr)
+    with open(NGINX_FILE, 'w') as f:
+        for instance, info in fs_instances.items():
+            write_nginx_entry(f, info)
+
+    # Sync systemd services
+    for instance in systemd_instances:
+        info = fs_instances.get(instance)
+        if info is None:
+            run_systemd_instance(instance, 'stop')
+            run_systemd_instance(instance, 'disable')
+    for instance in fs_instances:
+        status = systemd_instances.get(instance)
+        if status is None:
+            run_systemd_instance(instance, 'enable')
+        if not status:
+            run_systemd_instance(instance, 'start')
+
+    # Reload NGINX configuration
+    print('Reload NGINX server', file = sys.stderr)
+    subprocess.run(['systemctl', 'reload', 'nginx.service'])
