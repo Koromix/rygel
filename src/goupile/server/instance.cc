67dadc7b975a018991e0f6c28ee5fa0c524d00c7
Niels Martignène <niels.martignene@protonmail.com>
2020-10-29 01:09:55 +0100

    Merge config and instance source files in goupile server code

--- src/goupile/server/instance.cc
+++ src/goupile/server/instance.cc
@@ -31,6 +31,120 @@ DatabaseFile = database.db
 // If you change SchemaVersion, don't forget to update the migration switch!
 const int SchemaVersion = 14;
 
+static bool LoadConfig(StreamReader *st, InstanceConfig *out_config)
+{
+    InstanceConfig config;
+
+    Span<const char> root_directory;
+    SplitStrReverseAny(st->GetFileName(), RG_PATH_SEPARATORS, &root_directory);
+
+    IniParser ini(st);
+    ini.PushLogFilter();
+    RG_DEFER { PopLogFilter(); };
+
+    bool valid = true;
+    {
+        IniProperty prop;
+        while (ini.Next(&prop)) {
+            if (prop.section == "Application") {
+                do {
+                    if (prop.key == "Key") {
+                        config.app_key = DuplicateString(prop.value, &config.str_alloc).ptr;
+                    } else if (prop.key == "Name") {
+                        config.app_name = DuplicateString(prop.value, &config.str_alloc).ptr;
+                    } else {
+                        LogError("Unknown attribute '%1'", prop.key);
+                        valid = false;
+                    }
+                } while (ini.NextInSection(&prop));
+            } else if (prop.section == "Data") {
+                do {
+                    if (prop.key == "LiveDirectory" || prop.key == "FilesDirectory") {
+                        config.live_directory = NormalizePath(prop.value, root_directory,
+                                                              &config.str_alloc).ptr;
+                    } else if (prop.key == "TempDirectory") {
+                        config.temp_directory = NormalizePath(prop.value, root_directory,
+                                                              &config.str_alloc).ptr;
+                    } else if (prop.key == "DatabaseFile") {
+                        config.database_filename = NormalizePath(prop.value, root_directory,
+                                                                 &config.str_alloc).ptr;
+                    } else {
+                        LogError("Unknown attribute '%1'", prop.key);
+                        valid = false;
+                    }
+                } while (ini.NextInSection(&prop));
+            } else if (prop.section == "Sync") {
+                do {
+                    if (prop.key == "UseOffline") {
+                        valid &= IniParser::ParseBoolValue(prop.value, &config.use_offline);
+                    } else if (prop.key == "MaxFileSize") {
+                        valid &= ParseDec(prop.value, &config.max_file_size);
+                    } else if (prop.key == "SyncMode") {
+                        valid &= OptionToEnum(SyncModeNames, prop.value, &config.sync_mode);
+                    } else if (prop.key == "DemoUser") {
+                        config.demo_user = DuplicateString(prop.value, &config.str_alloc).ptr;
+                    } else {
+                        LogError("Unknown attribute '%1'", prop.key);
+                        valid = false;
+                    }
+                } while (ini.NextInSection(&prop));
+            } else if (prop.section == "HTTP") {
+                do {
+                    if (prop.key == "IPStack") {
+                        if (prop.value == "Dual") {
+                            config.http.ip_stack = IPStack::Dual;
+                        } else if (prop.value == "IPv4") {
+                            config.http.ip_stack = IPStack::IPv4;
+                        } else if (prop.value == "IPv6") {
+                            config.http.ip_stack = IPStack::IPv6;
+                        } else {
+                            LogError("Unknown IP version '%1'", prop.value);
+                        }
+                    } else if (prop.key == "Port") {
+                        valid &= ParseDec(prop.value, &config.http.port);
+                    } else if (prop.key == "MaxConnections") {
+                        valid &= ParseDec(prop.value, &config.http.max_connections);
+                    } else if (prop.key == "IdleTimeout") {
+                        valid &= ParseDec(prop.value, &config.http.idle_timeout);
+                    } else if (prop.key == "Threads") {
+                        valid &= ParseDec(prop.value, &config.http.threads);
+                    } else if (prop.key == "AsyncThreads") {
+                        valid &= ParseDec(prop.value, &config.http.async_threads);
+                    } else if (prop.key == "BaseUrl") {
+                        config.http.base_url = DuplicateString(prop.value, &config.str_alloc).ptr;
+                    } else if (prop.key == "MaxAge") {
+                        valid &= ParseDec(prop.value, &config.max_age);
+                    } else {
+                        LogError("Unknown attribute '%1'", prop.key);
+                        valid = false;
+                    }
+                } while (ini.NextInSection(&prop));
+            } else {
+                LogError("Unknown section '%1'", prop.section);
+                while (ini.NextInSection(&prop));
+                valid = false;
+            }
+        }
+    }
+    if (!ini.IsValid() || !valid)
+        return false;
+
+    // Default values
+    config.app_name = config.app_name ? config.app_name : config.app_key;
+    if (config.database_filename && !config.temp_directory) {
+        config.temp_directory = Fmt(&config.str_alloc, "%1%/tmp", root_directory).ptr;
+    }
+
+    std::swap(*out_config, config);
+    return true;
+}
+
+static bool LoadConfig(const char *filename, InstanceConfig *out_config)
+{
+    StreamReader st(filename);
+    return LoadConfig(&st, out_config);
+}
+
 bool InstanceData::Open(const char *filename)
 {
     Close();
--- src/goupile/server/instance.hh
+++ src/goupile/server/instance.hh
@@ -5,17 +5,46 @@
 #pragma once
 
 #include "../../core/libcc/libcc.hh"
-#include "config.hh"
 #include "../../core/libwrap/sqlite.hh"
+#include "../../web/libhttp/libhttp.hh"
 
 namespace RG {
 
 extern const char *const DefaultConfig;
 extern const int SchemaVersion;
 
+enum SyncMode {
+    Offline,
+    Mirror
+};
+static const char *const SyncModeNames[] = {
+    "Offline",
+    "Mirror"
+};
+
+struct InstanceConfig {
+    const char *app_key = nullptr;
+    const char *app_name = nullptr;
+
+    const char *live_directory = nullptr;
+    const char *temp_directory = nullptr;
+    const char *database_filename = nullptr;
+
+    bool use_offline = false;
+    int max_file_size = (int)Megabytes(8);
+    SyncMode sync_mode = SyncMode::Offline;
+    const char *demo_user = nullptr;
+
+    http_Config http {.port = 8889};
+    int max_age = 900;
+
+    BlockAllocator str_alloc;
+};
+
 class InstanceData {
 public:
-    Config config;
+    InstanceConfig config;
+
     sq_Database db;
     int version = -1;
 
