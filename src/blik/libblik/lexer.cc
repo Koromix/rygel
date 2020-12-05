ae90fd3752bb07c422fab7f3f133374c6f55cdec
Niels Martignène <niels.martignene@protonmail.com>
2020-12-05 23:10:20 +0100

    Detect all func keywords in lexer

--- src/blik/libblik/lexer.cc
+++ src/blik/libblik/lexer.cc
@@ -480,8 +480,7 @@ bool bk_Lexer::Tokenize(Span<const char> code, const char *filename)
                 if (keyword) {
                     // In order to have order-independent top-level functions, we need to parse
                     // their declarations first! Tell the parser where to look to help it.
-                    if (keyword->kind == bk_TokenKind::Func &&
-                            (!tokens.len || tokens[tokens.len - 1].kind == bk_TokenKind::EndOfLine)) {
+                    if (keyword->kind == bk_TokenKind::Func) {
                         file->funcs.Append(tokens.len);
                     }
 
