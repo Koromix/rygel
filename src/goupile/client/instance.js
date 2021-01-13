20900807ff267f3e87fddfe2a02612dc4ba661f0
Niels Martignène <niels.martignene@protonmail.com>
2021-01-13 06:16:00 +0100

    Hide 'Numéro' header text in data panel table

--- src/goupile/client/instance.js
+++ src/goupile/client/instance.js
@@ -211,7 +211,7 @@ function InstanceController() {
                     <thead>
                         <tr>
                             <th></th>
-                            <th>Numéro</th>
+                            <th></th>
                             ${util.map(data_form.pages.values(), page => html`<th>${page.title}</th>`)}
                             ${util.map(data_form.children.values(), child_form => html`<th>${child_form.title}</th>`)}
                         </tr>
