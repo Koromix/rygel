e1b5fddce9642d8018ac177c879953b757dc3b62
Niels Martignène <niels.martignene@protonmail.com>
2021-05-14 16:54:40 +0200

    Add script to extract subrepo ansible-hds

--- deploy/subrepo/ansible-hds/extract.py
+++ deploy/subrepo/ansible-hds/extract.py
@@ -45,7 +45,7 @@ if __name__ == "__main__":
 
     # Prepare clone directory
     if args.output_dir is None:
-        clone_directory = os.path.join(root_directory, 'bin/subrepo/goupile')
+        clone_directory = os.path.join(root_directory, 'bin/subrepo/ansible-hds')
     elif not os.path.isabs(args.output_dir):
         clone_directory = os.path.join(start_directory, args.output_directory)
     else:
@@ -70,82 +70,9 @@ if __name__ == "__main__":
     subprocess.run(['git', 'clone', root_directory, clone_directory, '--no-local'], check = True)
     os.chdir(clone_directory)
 
-    # FelixBuild.ini blobs
-    ids = subprocess.check_output(['git', 'rev-list', '--objects', '--all', '--no-object-names', '--', 'FelixBuild.ini'])
-    ids = ids.decode('utf-8').splitlines()
-
-    NEWLINE = '\\n'
-    REWRITE_FELIXBUILD = f'''
-      from collections import OrderedDict
-      import configparser
-      import io
-
-      class MultiOrderedDict(OrderedDict):
-        def __setitem__(self, key, value):
-            if isinstance(value, list) and key in self:
-                self[key].extend(value)
-            else:
-                super().__setitem__(key, value)
-
-      ids = ['{"','".join(ids)}']
-
-      id = blob.original_id.decode('utf-8')
-      if not id in ids:
-        return
-
-      ini = blob.data.decode('utf-8')
-
-      config = configparser.RawConfigParser(dict_type = MultiOrderedDict, strict = False)
-      config.optionxform = str
-      config.read_string(ini)
-
-      imports = []
-      if config.has_section('goupile'):
-        imports.append('goupile')
-      if config.has_section('goupile_admin'):
-        imports.append('goupile_admin')
-      if config.has_section('goupil'):
-        imports.append('goupil')
-      if config.has_section('goupil_admin'):
-        imports.append('goupil_admin')
-      if config.has_section('felix'):
-        imports.append('felix')
-      if len(imports) == 0:
-        return
-
-      next_imports = imports.copy()
-      while len(next_imports) > 0:
-        new_imports = []
-        for imp in next_imports:
-          import_str = config.get(imp, 'ImportFrom', fallback = '').strip()
-          if import_str:
-            new_imports.extend(import_str.split(' '))
-        imports.extend(new_imports)
-        next_imports = new_imports
-
-      for section in config.sections():
-        if not section in imports:
-          config.remove_section(section)
-
-      with io.StringIO() as f:
-        for section in config.sections():
-          if not section in imports:
-            continue
-          print('[' + section + ']' , file = f)
-          for k, vv in config.items(section):
-            vv = vv.split('{NEWLINE}')
-            for v in vv:
-              print(k + ' = ' + v.strip(), file = f)
-          print('', file = f)
-        blob.data = f.getvalue().encode('utf-8')
-    '''
-
     # Filter it out and rewrite FelixBuild.ini
     subprocess.run([sys.executable, script_directory + '/git-filter-repo.py',
-                    '--blob-callback', REWRITE_FELIXBUILD,
                     '--paths-from-file', script_directory + '/keep.txt'])
-    subprocess.run([sys.executable, script_directory + '/git-filter-repo.py',
-                    '--invert-paths', '--paths-from-file', script_directory + '/remove.txt'])
 
     # Push to repository
     if args.push_url is not None:
