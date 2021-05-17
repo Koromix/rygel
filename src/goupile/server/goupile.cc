cfa181ebb4f9cb1923d5129b5a6f197198ceba88
Niels Martignène <niels.martignene@protonmail.com>
2021-05-17 09:02:35 +0200

    Add sched_yield to allowed Goupile syscalls

--- src/goupile/server/goupile.cc
+++ src/goupile/server/goupile.cc
@@ -138,6 +138,7 @@ static bool ApplySandbox(Span<const char *const> paths)
         {"clock_nanosleep", sb_FilterAction::Allow},
         {"clock_nanosleep_time64", sb_FilterAction::Allow},
         {"nanosleep", sb_FilterAction::Allow},
+        {"sched_yield", sb_FilterAction::Allow},
         {"recv", sb_FilterAction::Allow},
         {"recvfrom", sb_FilterAction::Allow},
         {"recvmmsg", sb_FilterAction::Allow},
