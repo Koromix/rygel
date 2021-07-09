0abbbf0aecce86aa1288705029978308cfcf5ff6
Niels Martignène <niels.martignene@protonmail.com>
2021-07-09 14:08:12 +0200

    Switch to forms for demo project

--- src/goupile/demo/main.js
+++ src/goupile/demo/main.js
@@ -1,5 +1,5 @@
 app.form("suivi", "Suivi", () => {
-    app.page("introduction", "Introduction")
-    app.page("avance", "Avancé")
-    app.page("mise_en_page", "Mise en page")
+    app.form("introduction", "Introduction")
+    app.form("avance", "Avancé")
+    app.form("mise_en_page", "Mise en page")
 })
