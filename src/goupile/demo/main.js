c601396218cee19494b52d1576783d48e2756b48
Niels Martignène <niels.martignene@protonmail.com>
2021-07-11 17:06:48 +0200

    Revert "Switch to forms for demo project"
    Not a good idea yet until HID system is improved

--- src/goupile/demo/main.js
+++ src/goupile/demo/main.js
@@ -1,5 +1,5 @@
 app.form("suivi", "Suivi", () => {
-    app.form("introduction", "Introduction")
-    app.form("avance", "Avancé")
-    app.form("mise_en_page", "Mise en page")
+    app.page("introduction", "Introduction")
+    app.page("avance", "Avancé")
+    app.page("mise_en_page", "Mise en page")
 })
