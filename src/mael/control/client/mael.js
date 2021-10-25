b0804b86e5391dfadebda34387991992d412dcda
Niels Martignène <niels.martignene@protonmail.com>
2021-10-25 13:13:51 +0200

    Paint 2022 background on Mael control page

--- src/mael/control/client/mael.js
+++ src/mael/control/client/mael.js
@@ -11,8 +11,22 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-function init() {
-    // Nothing to do yet
+let assets = {};
+
+async function init() {
+    let filenames = {
+        table: 'table.png'
+    };
+
+    let images = await Promise.all(Object.keys(filenames).map(key => {
+        let url = 'static/' + filenames[key];
+        return loadTexture(url);
+    }));
+
+    for (let i = 0; i < Object.keys(filenames).length; i++) {
+        let key = Object.keys(filenames)[i];
+        assets[key] = images[i];
+    }
 }
 
 function update() {
@@ -25,13 +39,25 @@ function draw() {
     ctx.font = '18px Open Sans';
     ctx.setTransform(1, 0, 0, 1, 0, 0);
 
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    // ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Paint stable background
+    {
+        let img = assets.table;
+
+        let cx = canvas.width / 2;
+        let cy = canvas.height / 2;
+        let factor = Math.min(canvas.width / img.width, canvas.height / img.height) * 0.9;
+
+        ctx.drawImage(img, cx - img.width * factor / 2, cy - img.height * factor / 2,
+                           img.width * factor, img.height * factor);
+    }
 
     // Debug / FPS
     {
         let text = `FPS : ${(1000 / frame_time).toFixed(0)} (${frame_time.toFixed(1)} ms)` +
                    ` | Update : ${update_time.toFixed(1)} ms | Draw : ${draw_time.toFixed(1)} ms`;
         ctx.textAlign = 'right';
-        ctx.fillText(text, canvas.width - 8, 24);
+        ctx.fillText(text, canvas.width - 8, canvas.height - 8);
     }
 }
