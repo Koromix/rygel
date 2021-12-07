ba5d52229192193cff110a6a7c0bc339d4fb1e1d
Niels Martignène <niels.martignene@protonmail.com>
2021-12-07 16:12:30 +0100

    Fix dummy version number in manifest file

--- src/felix/build.cc
+++ src/felix/build.cc
@@ -156,7 +156,7 @@ static bool UpdateResourceFile(const char *target_name, bool fake, const char *d
     static const char *const manifest = R"(
 <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <assembly manifestVersion="1.0" xmlns="urn:schemas-microsoft-com:asm.v1">
-    <assemblyIdentity type="win32" name="" version="6.0.0.0"/>
+    <assemblyIdentity type="win32" name="" version="1.0.0.0"/>
     <application>
         <windowsSettings>
             <activeCodePage xmlns="http://schemas.microsoft.com/SMI/2019/WindowsSettings">UTF-8</activeCodePage>
