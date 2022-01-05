aa7cfc069c3e83b7c14e86fb9d62ae76b79174e8
Niels Martignène <niels.martignene@protonmail.com>
2022-01-05 10:30:24 +0100

    Avoid unsupported "-z separate-code" link flag on FreeBSD

--- src/felix/compiler.cc
+++ src/felix/compiler.cc
@@ -504,11 +504,14 @@ public:
                 Fmt(&buf, " -ldl -pthread -framework CoreFoundation -framework SystemConfiguration");
             } break;
 
-            case HostPlatform::Linux: {
-                Fmt(&buf, " -ldl -lrt");
-            } [[fallthrough]];
             default: {
-                Fmt(&buf, " -pthread -Wl,-z,relro,-z,now,-z,noexecstack,-z,separate-code,-z,stack-size=1048576");
+                Fmt(&buf, " -pthread -Wl,-z,relro,-z,now,-z,noexecstack,-z,stack-size=1048576");
+                if (host == HostPlatform::Linux) {
+                    Fmt(&buf, " -ldl -lrt");
+                }
+                if (host != HostPlatform::FreeBSD) {
+                    Fmt(&buf, " -Wl,-z,separate-code");
+                }
                 if (link_type == LinkType::Executable) {
                     Fmt(&buf, " -pie");
                 }
@@ -878,11 +881,14 @@ public:
                 Fmt(&buf, " -ldl -pthread -framework CoreFoundation -framework SystemConfiguration");
             } break;
 
-            case HostPlatform::Linux: {
-                Fmt(&buf, " -ldl -lrt");
-            } [[fallthrough]];
             default: {
-                Fmt(&buf, " -pthread -Wl,-z,relro,-z,now,-z,noexecstack,-z,separate-code,-z,stack-size=1048576");
+                Fmt(&buf, " -pthread -Wl,-z,relro,-z,now,-z,noexecstack,-z,stack-size=1048576");
+                if (host == HostPlatform::Linux) {
+                    Fmt(&buf, " -ldl -lrt");
+                }
+                if (host != HostPlatform::FreeBSD) {
+                    Fmt(&buf, " -Wl,-z,separate-code");
+                }
                 if (link_type == LinkType::Executable) {
                     Fmt(&buf, " -pie");
                 }
