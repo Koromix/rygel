79cbc03a7e4f80ad4beced1cced94307229e699a
Niels Martignène <niels.martignene@protonmail.com>
2022-01-06 14:41:19 +0100

    Add FreeBSD support to static libmicrohttpd config file

--- vendor/libmicrohttpd/MHD_config.h
+++ vendor/libmicrohttpd/MHD_config.h
@@ -171,7 +171,6 @@
     #define HAVE_GMTIME_R 1
     #define HAVE_INET6 1
     #define HAVE_INTTYPES_H 1
-    #define HAVE_LIBCURL 1
     #define HAVE_LIMITS_H 1
     #define HAVE_MACHINE_ENDIAN_H 1
     #define HAVE_MACHINE_PARAM_H 1
@@ -386,7 +385,6 @@
     #define HAVE_GMTIME_R 1
     #define HAVE_INET6 1
     #define HAVE_INTTYPES_H 1
-    #define HAVE_LIBCURL 1
     #define HAVE_LIMITS_H 1
     #define HAVE_LISTEN_SHUTDOWN 1
     #define HAVE_MACHINE_ENDIAN_H 1
@@ -465,6 +463,120 @@
     #define STDC_HEADERS 1
     #define UPGRADE_SUPPORT 1
     #define _BSD_SOURCE 1
+    #define _GNU_SOURCE 1
+    #define _MHD_EXTERN __attribute__((visibility("default"))) extern
+    #define _MHD_ITC_PIPE 1
+    #define _MHD_NORETURN _Noreturn
+    #define _MHD_static_inline static inline __attribute__((always_inline))
+    #define _XOPEN_SOURCE 700
+    #if defined(__x86_64__) || defined(_M_X64) || defined(__aarch64__)
+        #define SIZEOF_SIZE_T 8
+    #else
+        #define SIZEOF_SIZE_T 4
+    #endif
+    #define SIZEOF_STRUCT_TIMEVAL_TV_SEC 4
+    #define SIZEOF_UINT64_T 8
+    #define SIZEOF_UNSIGNED_INT 4
+    #define SIZEOF_UNSIGNED_LONG_LONG 8
+#elif defined(__FreeBSD__)
+    #define BAUTH_SUPPORT 1
+    #define DAUTH_SUPPORT 1
+    #define HAVE_ACCEPT4 1
+    #define HAVE_ARPA_INET_H 1
+    #define HAVE_CALLOC 1
+    #define HAVE_CLOCK_GETTIME 1
+    #define HAVE_C_ALIGNOF 1
+    #define HAVE_C_VARARRAYS 1
+    #define HAVE_DLFCN_H 1
+    #define HAVE_ERRNO_H 1
+    #define HAVE_FCNTL_H 1
+    #define HAVE_FORK 1
+    #define HAVE_FREEBSD_SENDFILE 1
+    #define HAVE_FSEEKO 1
+    #define HAVE_GETSOCKNAME 1
+    #define HAVE_GETTIMEOFDAY 1
+    #define HAVE_GMTIME_R 1
+    #define HAVE_INET6 1
+    #define HAVE_INTTYPES_H 1
+    #define HAVE_LIMITS_H 1
+    #define HAVE_LISTEN_SHUTDOWN 1
+    #define HAVE_MACHINE_ENDIAN_H 1
+    #define HAVE_MACHINE_PARAM_H 1
+    #define HAVE_MEMMEM 1
+    #define HAVE_MESSAGES 1
+    #define HAVE_NANOSLEEP 1
+    #define HAVE_NETDB_H 1
+    #define HAVE_NETINET_ICMP_VAR_H 1
+    #define HAVE_NETINET_IN_H 1
+    #define HAVE_NETINET_IP_H 1
+    #define HAVE_NETINET_IP_ICMP_H 1
+    #define HAVE_NETINET_TCP_H 1
+    #define HAVE_NET_IF_H 1
+    #define HAVE_PIPE2_FUNC 1
+    #define HAVE_POLL 1
+    #define HAVE_POLL_H 1
+    #define HAVE_POSTPROCESSOR 1
+    #define HAVE_PREAD 1
+    #define HAVE_PTHREAD_H 1
+    #define HAVE_PTHREAD_NP_H 1
+    #define HAVE_PTHREAD_PRIO_INHERIT 1
+    #define HAVE_PTHREAD_SET_NAME_NP_FREEBSD 1
+    #define HAVE_PTHREAD_SIGMASK 1
+    #define HAVE_RANDOM 1
+    #define HAVE_REAL_BOOL 1
+    #define HAVE_SEARCH_H 1
+    #define HAVE_SENDMSG 1
+    #define HAVE_SIGNAL_H 1
+    #define HAVE_SNPRINTF 1
+    #define HAVE_SOCK_NONBLOCK 1
+    #define HAVE_STDALIGN_H 1
+    #define HAVE_STDBOOL_H 1
+    #define HAVE_STDDEF_H 1
+    #define HAVE_STDINT_H 1
+    #define HAVE_STDIO_H 1
+    #define HAVE_STDLIB_H 1
+    #define HAVE_STRINGS_H 1
+    #define HAVE_STRING_H 1
+    #define HAVE_STRUCT_SOCKADDR_IN6_SIN6_LEN 1
+    #define HAVE_STRUCT_SOCKADDR_IN_SIN_LEN 1
+    #define HAVE_STRUCT_SOCKADDR_STORAGE_SS_LEN 1
+    #define HAVE_SYSCONF 1
+    #define HAVE_SYSCTL 1
+    #define HAVE_SYSCTLBYNAME 1
+    #define HAVE_SYS_ENDIAN_H 1
+    #define HAVE_SYS_IOCTL_H 1
+    #define HAVE_SYS_MMAN_H 1
+    #define HAVE_SYS_MSG_H 1
+    #define HAVE_SYS_PARAM_H 1
+    #define HAVE_SYS_SELECT_H 1
+    #define HAVE_SYS_SOCKET_H 1
+    #define HAVE_SYS_STAT_H 1
+    #define HAVE_SYS_SYSCTL_H 1
+    #define HAVE_SYS_TIME_H 1
+    #define HAVE_SYS_TYPES_H 1
+    #define HAVE_TIMESPEC_GET 1
+    #define HAVE_TIME_H 1
+    #define HAVE_TSEARCH 1
+    #define HAVE_TWALK 1
+    #define HAVE_UNISTD_H 1
+    #define HAVE_USLEEP 1
+    #define HAVE_WAITPID 1
+    #define HAVE_WRITEV 1
+    #define HAVE_ZLIB_H 1
+    #define HAVE___FUNC__ 1
+    #define INLINE_FUNC 1
+    #define FREEBSD 1
+    #define SOMEBSD 1
+    #define __BSD_VISIBLE 1
+    #define LT_OBJDIR ".libs/"
+    #define MHD_HAVE___BUILTIN_BSWAP32 1
+    #define MHD_HAVE___BUILTIN_BSWAP64 1
+    #define MHD_USE_GETSOCKNAME 1
+    #define MHD_USE_PAGE_SIZE_MACRO 1
+    #define MHD_USE_PAGE_SIZE_MACRO_STATIC 1
+    #define MHD_USE_POSIX_THREADS 1
+    #define STDC_HEADERS 1
+    #define UPGRADE_SUPPORT 1
     #define _GNU_SOURCE 1
     #define _MHD_EXTERN __attribute__((visibility("default"))) extern
     #define _MHD_ITC_PIPE 1
