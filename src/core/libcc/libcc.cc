384df624a903fc186b2f9018847264de28872707
Niels Martignène <niels.martignene@protonmail.com>
2022-02-03 08:55:42 +0100

    Fix CFI crash related to recent POSIX StatFile change

--- src/core/libcc/libcc.cc
+++ src/core/libcc/libcc.cc
@@ -2038,10 +2038,9 @@ EnumStatus EnumerateDirectory(const char *dirname, const char *filter, Size max_
 
 bool StatFile(const char *filename, unsigned int flags, FileInfo *out_info)
 {
-    auto func = (flags & (int)StatFlag::FollowSymlink) ? stat : lstat;
-
     struct stat sb;
-    if (func(filename, &sb) < 0) {
+    int ret = (flags & (int)StatFlag::FollowSymlink) ? stat(filename, &sb) : lstat(filename, &sb);
+    if (ret < 0) {
         if (!(flags & (int)StatFlag::IgnoreMissing) || errno != ENOENT) {
             LogError("Cannot stat '%1': %2", filename, strerror(errno));
         }
