2f6dc645d842dfc6e5e885facb1c04bd7b739251
Niels Martignène <niels.martignene@protonmail.com>
2022-02-12 11:02:59 +0100

    Update vendorized esbuild binaries

--- vendor/esbuild/update.bat
+++ vendor/esbuild/update.bat
@@ -7,22 +7,22 @@ set VERSION=0.14.10
 
 curl -O https://registry.npmjs.org/esbuild-windows-64/-/esbuild-windows-64-%VERSION%.tgz
 tar zx --strip-components=1 -f esbuild-windows-64-%VERSION%.tgz package/esbuild.exe
-move /Y esbuild.exe esbuild_windows_amd64.exe
+move /Y esbuild.exe esbuild_win_x64.exe
 
 curl -O https://registry.npmjs.org/esbuild-windows-32/-/esbuild-windows-32-%VERSION%.tgz
 tar zx --strip-components=1 -f esbuild-windows-32-%VERSION%.tgz package/esbuild.exe
-move /Y esbuild.exe esbuild_windows_x86.exe
+move /Y esbuild.exe esbuild_win_ia32.exe
 
 curl -O https://registry.npmjs.org/esbuild-darwin-64/-/esbuild-darwin-64-%VERSION%.tgz
 tar zx --strip-components=2 -f esbuild-darwin-64-%VERSION%.tgz package/bin/esbuild
-move /Y esbuild esbuild_darwin_amd64
+move /Y esbuild esbuild_mac_x64
 
 curl -O https://registry.npmjs.org/esbuild-linux-64/-/esbuild-linux-64-%VERSION%.tgz
 tar zx --strip-components=2 -f esbuild-linux-64-%VERSION%.tgz package/bin/esbuild
-move /Y esbuild esbuild_linux_amd64
+move /Y esbuild esbuild_linux_x64
 
 curl -O https://registry.npmjs.org/esbuild-linux-arm64/-/esbuild-linux-arm64-%VERSION%.tgz
 tar zx --strip-components=2 -f esbuild-linux-arm64-%VERSION%.tgz package/bin/esbuild
-move /Y esbuild esbuild_linux_arm64
+move /Y esbuild esbuild_linux_x64
 
 del *.tgz
--- vendor/esbuild/update.sh
+++ vendor/esbuild/update.sh
@@ -6,23 +6,23 @@ VERSION=0.14.10
 
 curl -O https://registry.npmjs.org/esbuild-windows-64/-/esbuild-windows-64-$VERSION.tgz
 tar zx --strip-components=1 -f esbuild-windows-64-$VERSION.tgz package/esbuild.exe
-mv esbuild.exe esbuild_windows_amd64.exe
-chmod -x esbuild_windows_amd64.exe
+mv esbuild.exe esbuild_win_x64.exe
+chmod -x esbuild_win_x64.exe
 
 curl -O https://registry.npmjs.org/esbuild-windows-32/-/esbuild-windows-32-$VERSION.tgz
 tar zx --strip-components=1 -f esbuild-windows-32-$VERSION.tgz package/esbuild.exe
-mv esbuild.exe esbuild_windows_x86.exe
-chmod -x esbuild_windows_x86.exe
+mv esbuild.exe esbuild_win_ia32.exe
+chmod -x esbuild_win_ia32.exe
 
 curl -O https://registry.npmjs.org/esbuild-darwin-64/-/esbuild-darwin-64-$VERSION.tgz
 tar zx --strip-components=2 -f esbuild-darwin-64-$VERSION.tgz package/bin/esbuild
-mv esbuild esbuild_darwin_amd64
-chmod +x esbuild_darwin_amd64
+mv esbuild esbuild_mac_x64
+chmod +x esbuild_mac_x64
 
 curl -O https://registry.npmjs.org/esbuild-linux-64/-/esbuild-linux-64-$VERSION.tgz
 tar zx --strip-components=2 -f esbuild-linux-64-$VERSION.tgz package/bin/esbuild
-mv esbuild esbuild_linux_amd64
-chmod +x esbuild_linux_amd64
+mv esbuild esbuild_linux_x64
+chmod +x esbuild_linux_x64
 
 curl -O https://registry.npmjs.org/esbuild-linux-arm64/-/esbuild-linux-arm64-$VERSION.tgz
 tar zx --strip-components=2 -f esbuild-linux-arm64-$VERSION.tgz package/bin/esbuild
