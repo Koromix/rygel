ea80f3a4d48e6bdac3b38995fe2a808139a358ff
Niels Martignène <niels.martignene@protonmail.com>
2022-03-30 13:18:00 +0200

    Vendorize stb_image and stb_image_resize

---
+++ vendor/stb/stb_image.c
@@ -0,0 +1,14 @@
+#ifdef __GNUC__
+    #pragma GCC diagnostic ignored "-Wall"
+    #pragma GCC diagnostic ignored "-Wextra"
+    #pragma GCC diagnostic ignored "-Wconversion"
+    #pragma GCC diagnostic ignored "-Wsign-conversion"
+    #pragma GCC diagnostic ignored "-Wunused-function"
+    #pragma GCC diagnostic ignored "-Wunused-variable"
+    #pragma GCC diagnostic ignored "-Wunused-but-set-variable"
+    #pragma GCC diagnostic ignored "-Wunused-parameter"
+#endif
+
+#define STBI_FAILURE_USERMSG
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
