b8b4db6ec105b5161fc777aaceaff93c5b2a2b97
Niels Martignène <niels.martignene@protonmail.com>
2022-04-24 16:13:47 +0200

    Fix harmless MSVC conversion warning

--- src/ffi.cc
+++ src/ffi.cc
@@ -116,7 +116,7 @@ static Napi::Value CreateStructType(const Napi::CallbackInfo &info)
         if (!member.type)
             return env.Null();
 
-        type->size = AlignLen(type->size, member.type->align) + member.type->size;
+        type->size = (int16_t)(AlignLen(type->size, member.type->align) + member.type->size);
         type->align = std::max(type->align, member.type->align);
 
         type->members.Append(member);
