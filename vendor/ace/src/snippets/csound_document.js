db591a90e0cf3525eac5f1efb2dce036dc351e5b
Niels Martignène <niels.martignene@protonmail.com>
2022-05-12 14:23:47 +0200

    Update to ACE 1.5.0

--- vendor/ace/src/snippets/csound_document.js
+++ vendor/ace/src/snippets/csound_document.js
@@ -1,4 +1,20 @@
-define("ace/snippets/csound_document",["require","exports","module"],function(e,t,n){"use strict";t.snippetText="# <CsoundSynthesizer>\nsnippet synth\n	<CsoundSynthesizer>\n	<CsInstruments>\n	${1}\n	</CsInstruments>\n	<CsScore>\n	e\n	</CsScore>\n	</CsoundSynthesizer>\n",t.scope="csound_document"});                (function() {
+define("ace/snippets/csound_document",["require","exports","module"], function(require, exports, module) {
+"use strict";
+
+exports.snippetText = "# <CsoundSynthesizer>\n\
+snippet synth\n\
+	<CsoundSynthesizer>\n\
+	<CsInstruments>\n\
+	${1}\n\
+	</CsInstruments>\n\
+	<CsScore>\n\
+	e\n\
+	</CsScore>\n\
+	</CsoundSynthesizer>\n\
+";
+exports.scope = "csound_document";
+
+});                (function() {
                     window.require(["ace/snippets/csound_document"], function(m) {
                         if (typeof module == "object" && typeof exports == "object" && module) {
                             module.exports = m;
--- vendor/ace/src/snippets/fsl.js
+++ vendor/ace/src/snippets/fsl.js
@@ -1,4 +1,10 @@
-define("ace/snippets/fsl",["require","exports","module"],function(e,t,n){"use strict";t.snippetText=undefined,t.scope=""});                (function() {
+define("ace/snippets/fsl",["require","exports","module"], function(require, exports, module) {
+"use strict";
+
+exports.snippetText =undefined;
+exports.scope = "";
+
+});                (function() {
                     window.require(["ace/snippets/fsl"], function(m) {
                         if (typeof module == "object" && typeof exports == "object" && module) {
                             module.exports = m;
