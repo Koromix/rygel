94fa269fbf30755953f5fcf5d388ecf18b1cff46
Niels Martignène <niels.martignene@protonmail.com>
2022-06-03 15:53:16 +0200

    Add callback support to x86 ABI code

--- src/abi_x86.cc
+++ src/abi_x86.cc
@@ -22,12 +22,61 @@
 
 namespace RG {
 
-extern "C" uint64_t ForwardCallG(const void *func, uint8_t *sp);
-extern "C" float ForwardCallF(const void *func, uint8_t *sp);
-extern "C" double ForwardCallD(const void *func, uint8_t *sp);
-extern "C" uint64_t ForwardCallRG(const void *func, uint8_t *sp);
-extern "C" float ForwardCallRF(const void *func, uint8_t *sp);
-extern "C" double ForwardCallRD(const void *func, uint8_t *sp);
+struct BackRegisters {
+    uint32_t eax;
+    uint32_t edx;
+    double d;
+    float f;
+    bool is_double;
+};
+
+extern "C" uint64_t ForwardCallG(const void *func, uint8_t *sp, uint8_t **out_old_sp);
+extern "C" float ForwardCallF(const void *func, uint8_t *sp, uint8_t **out_old_sp);
+extern "C" double ForwardCallD(const void *func, uint8_t *sp, uint8_t **out_old_sp);
+extern "C" uint64_t ForwardCallRG(const void *func, uint8_t *sp, uint8_t **out_old_sp);
+extern "C" float ForwardCallRF(const void *func, uint8_t *sp, uint8_t **out_old_sp);
+extern "C" double ForwardCallRD(const void *func, uint8_t *sp, uint8_t **out_old_sp);
+
+extern "C" int Trampoline0; extern "C" int TrampolineX0;
+extern "C" int Trampoline1; extern "C" int TrampolineX1;
+extern "C" int Trampoline2; extern "C" int TrampolineX2;
+extern "C" int Trampoline3; extern "C" int TrampolineX3;
+extern "C" int Trampoline4; extern "C" int TrampolineX4;
+extern "C" int Trampoline5; extern "C" int TrampolineX5;
+extern "C" int Trampoline6; extern "C" int TrampolineX6;
+extern "C" int Trampoline7; extern "C" int TrampolineX7;
+extern "C" int Trampoline8; extern "C" int TrampolineX8;
+extern "C" int Trampoline9; extern "C" int TrampolineX9;
+extern "C" int Trampoline10; extern "C" int TrampolineX10;
+extern "C" int Trampoline11; extern "C" int TrampolineX11;
+extern "C" int Trampoline12; extern "C" int TrampolineX12;
+extern "C" int Trampoline13; extern "C" int TrampolineX13;
+extern "C" int Trampoline14; extern "C" int TrampolineX14;
+extern "C" int Trampoline15; extern "C" int TrampolineX15;
+
+extern "C" napi_value CallSwitchStack(Napi::Function *func, size_t argc, napi_value *argv,
+                                      uint8_t *old_sp, Span<uint8_t> *new_stack,
+                                      napi_value (*call)(Napi::Function *func, size_t argc, napi_value *argv));
+
+static void *const Trampolines[][2] = {
+    { &Trampoline0, &TrampolineX0 },
+    { &Trampoline1, &TrampolineX1 },
+    { &Trampoline2, &TrampolineX2 },
+    { &Trampoline3, &TrampolineX3 },
+    { &Trampoline4, &TrampolineX4 },
+    { &Trampoline5, &TrampolineX5 },
+    { &Trampoline6, &TrampolineX6 },
+    { &Trampoline7, &TrampolineX7 },
+    { &Trampoline8, &TrampolineX8 },
+    { &Trampoline9, &TrampolineX9 },
+    { &Trampoline10, &TrampolineX10 },
+    { &Trampoline11, &TrampolineX11 },
+    { &Trampoline12, &TrampolineX12 },
+    { &Trampoline13, &TrampolineX13 },
+    { &Trampoline14, &TrampolineX14 },
+    { &Trampoline15, &TrampolineX15 }
+};
+RG_STATIC_ASSERT(RG_LEN(Trampolines) == MaxTrampolines);
 
 static RG_THREAD_LOCAL CallData *exec_call;
 
@@ -290,8 +339,8 @@ void CallData::Execute()
 
 #define PERFORM_CALL(Suffix) \
         ([&]() { \
-            auto ret = (func->fast ? ForwardCallR ## Suffix(func->func, new_sp) \
-                                   : ForwardCall ## Suffix(func->func, new_sp)); \
+            auto ret = (func->fast ? ForwardCallR ## Suffix(func->func, new_sp, &old_sp) \
+                                   : ForwardCall ## Suffix(func->func, new_sp, &old_sp)); \
             return ret; \
         })()
 
@@ -368,12 +417,297 @@ Napi::Value CallData::Complete()
 
 void CallData::Relay(Size idx, uint8_t *own_sp, uint8_t *caller_sp, BackRegisters *out_reg)
 {
-    RG_UNREACHABLE();
+    const FunctionInfo *proto = instance->trampolines[idx].proto;
+    Napi::Function func = instance->trampolines[idx].func;
+
+    if (RG_UNLIKELY(proto->convention != CallConvention::Cdecl &&
+                    proto->convention != CallConvention::Stdcall)) {
+        ThrowError<Napi::Error>(env, "Only Cdecl and Stdcall callbacks are supported on x86 platforms");
+        return;
+    }
+
+    // Allow reuse of static trampoline
+    instance->free_trampolines |= 1u << idx;
+    used_trampolines &= ~(1u << idx);
+
+    uint32_t *args_ptr = (uint32_t *)caller_sp;
+    uint8_t *return_ptr = !proto->ret.trivial ? (uint8_t *)args_ptr[0] : nullptr;
+
+    LocalArray<napi_value, MaxParameters> arguments;
+
+    // Convert to JS arguments
+    for (Size i = 0; i < proto->parameters.len; i++) {
+        const ParameterInfo &param = proto->parameters[i];
+        RG_ASSERT(param.directions >= 1 && param.directions <= 3);
+
+        switch (param.type->primitive) {
+            case PrimitiveKind::Void: { RG_UNREACHABLE(); } break;
+
+            case PrimitiveKind::Bool: {
+                bool b = *(bool *)(args_ptr++);
+
+                Napi::Value arg = Napi::Boolean::New(env, b);
+                arguments.Append(arg);
+            } break;
+            case PrimitiveKind::Int8: {
+                double d = (double)*(int8_t *)(args_ptr++);
+
+                Napi::Value arg = Napi::Number::New(env, d);
+                arguments.Append(arg);
+            } break;
+            case PrimitiveKind::UInt8: {
+                double d = (double)*(uint8_t *)(args_ptr++);
+
+                Napi::Value arg = Napi::Number::New(env, d);
+                arguments.Append(arg);
+            } break;
+            case PrimitiveKind::Int16: {
+                double d = (double)*(int16_t *)(args_ptr++);
+
+                Napi::Value arg = Napi::Number::New(env, d);
+                arguments.Append(arg);
+            } break;
+            case PrimitiveKind::UInt16: {
+                double d = (double)*(uint16_t *)(args_ptr++);
+
+                Napi::Value arg = Napi::Number::New(env, d);
+                arguments.Append(arg);
+            } break;
+            case PrimitiveKind::Int32: {
+                double d = (double)*(int32_t *)(args_ptr++);
+
+                Napi::Value arg = Napi::Number::New(env, d);
+                arguments.Append(arg);
+            } break;
+            case PrimitiveKind::UInt32: {
+                double d = (double)*(int32_t *)(args_ptr++);
+
+                Napi::Value arg = Napi::Number::New(env, d);
+                arguments.Append(arg);
+            } break;
+            case PrimitiveKind::Int64: {
+                int64_t v = *(int64_t *)args_ptr;
+                args_ptr += 2;
+
+                Napi::Value arg = Napi::BigInt::New(env, v);
+                arguments.Append(arg);
+            } break;
+            case PrimitiveKind::UInt64: {
+                uint64_t v = *(uint64_t *)args_ptr;
+                args_ptr += 2;
+
+                Napi::Value arg = Napi::BigInt::New(env, v);
+                arguments.Append(arg);
+            } break;
+            case PrimitiveKind::String: {
+                const char *str = *(const char **)(args_ptr++);
+
+                Napi::Value arg = Napi::String::New(env, str);
+                arguments.Append(arg);
+            } break;
+            case PrimitiveKind::String16: {
+                const char16_t *str16 = *(const char16_t **)(args_ptr++);
+
+                Napi::Value arg = Napi::String::New(env, str16);
+                arguments.Append(arg);
+            } break;
+            case PrimitiveKind::Pointer:
+            case PrimitiveKind::Callback: {
+                void *ptr2 = *(void **)(args_ptr++);
+
+                if (ptr2) {
+                    Napi::External<void> external = Napi::External<void>::New(env, ptr2);
+                    SetValueTag(instance, external, param.type);
+
+                    arguments.Append(external);
+                } else {
+                    arguments.Append(env.Null());
+                }
+            } break;
+            case PrimitiveKind::Record: {
+                RG_ASSERT(!param.fast);
+
+                uint8_t *ptr = AlignUp((uint8_t *)args_ptr, param.type->align);
+                args_ptr = (uint32_t *)AlignUp(ptr + param.type->size, 4);
+                Napi::Object obj2 = PopObject(ptr, param.type);
+                arguments.Append(obj2);
+            } break;
+            case PrimitiveKind::Array: { RG_UNREACHABLE(); } break;
+            case PrimitiveKind::Float32: {
+                float f = *(float *)(args_ptr++);
+
+                Napi::Value arg = Napi::Number::New(env, (double)f);
+                arguments.Append(arg);
+            } break;
+            case PrimitiveKind::Float64: {
+                double d = *(double *)args_ptr;
+                args_ptr += 2;
+
+                Napi::Value arg = Napi::Number::New(env, d);
+                arguments.Append(arg);
+            } break;
+        }
+    }
+
+    const TypeInfo *type = proto->ret.type;
+
+    // Make the call
+    napi_value ret = CallSwitchStack(&func, (size_t)arguments.len, arguments.data, old_sp, &mem->stack,
+                                     [](Napi::Function *func, size_t argc, napi_value *argv) { return (napi_value)func->Call(argc, argv); });
+    Napi::Value value(env, ret);
+
+    switch (type->primitive) {
+        case PrimitiveKind::Void: {} break;
+        case PrimitiveKind::Bool: {
+            if (RG_UNLIKELY(!value.IsBoolean())) {
+                ThrowError<Napi::TypeError>(env, "Unexpected %1 value for return value, expected boolean", GetValueType(instance, value));
+                return;
+            }
+
+            bool b = value.As<Napi::Boolean>();
+            out_reg->eax = (uint32_t)b;
+        } break;
+        case PrimitiveKind::Int8:
+        case PrimitiveKind::UInt8:
+        case PrimitiveKind::Int16:
+        case PrimitiveKind::UInt16:
+        case PrimitiveKind::Int32:
+        case PrimitiveKind::UInt32: {
+            if (RG_UNLIKELY(!value.IsNumber() && !value.IsBigInt())) {
+                ThrowError<Napi::TypeError>(env, "Unexpected %1 value for return value, expected number", GetValueType(instance, value));
+                return;
+            }
+
+            int32_t v = CopyNumber<int32_t>(value);
+            out_reg->eax = (uint32_t)v;
+        } break;
+        case PrimitiveKind::Int64:
+        case PrimitiveKind::UInt64: {
+            if (RG_UNLIKELY(!value.IsNumber() && !value.IsBigInt())) {
+                ThrowError<Napi::TypeError>(env, "Unexpected %1 value for return value, expected number", GetValueType(instance, value));
+                return;
+            }
+
+            int64_t v = CopyNumber<int64_t>(value);
+            out_reg->eax = (uint32_t)(v & 0xFFFFFFFFul);
+            out_reg->edx = (uint32_t)(v << 32);
+        } break;
+        case PrimitiveKind::String: {
+            const char *str;
+            if (RG_LIKELY(value.IsString())) {
+                str = PushString(value);
+                if (RG_UNLIKELY(!str))
+                    return;
+            } else if (IsNullOrUndefined(value)) {
+                str = nullptr;
+            } else {
+                ThrowError<Napi::TypeError>(env, "Unexpected %1 value for return value, expected string", GetValueType(instance, value));
+                return;
+            }
+
+            out_reg->eax = (uint32_t)str;
+        } break;
+        case PrimitiveKind::String16: {
+            const char16_t *str16;
+            if (RG_LIKELY(value.IsString())) {
+                str16 = PushString16(value);
+                if (RG_UNLIKELY(!str16))
+                    return;
+            } else if (IsNullOrUndefined(value)) {
+                str16 = nullptr;
+            } else {
+                ThrowError<Napi::TypeError>(env, "Unexpected %1 value for return value, expected string", GetValueType(instance, value));
+                return;
+            }
+
+            out_reg->eax = (uint32_t)str16;
+        } break;
+        case PrimitiveKind::Pointer: {
+            uint8_t *ptr;
+
+            if (CheckValueTag(instance, value, type)) {
+                ptr = value.As<Napi::External<uint8_t>>().Data();
+            } else if (IsObject(value) && type->ref->primitive == PrimitiveKind::Record) {
+                Napi::Object obj = value.As<Napi::Object>();
+
+                if (RG_UNLIKELY(!AllocHeap(type->ref->size, 16, &ptr)))
+                    return;
+
+                if (!PushObject(obj, type->ref, ptr))
+                    return;
+            } else if (IsNullOrUndefined(value)) {
+                ptr = nullptr;
+            } else {
+                ThrowError<Napi::TypeError>(env, "Unexpected %1 value for return value, expected %2", GetValueType(instance, value), type->name);
+                return;
+            }
+
+            out_reg->eax = (uint32_t)ptr;
+        } break;
+        case PrimitiveKind::Record: {
+            if (RG_UNLIKELY(!IsObject(value))) {
+                ThrowError<Napi::TypeError>(env, "Unexpected %1 value for return value, expected object", GetValueType(instance, value));
+                return;
+            }
+
+            Napi::Object obj = value.As<Napi::Object>();
+
+            if (return_ptr) {
+                if (!PushObject(obj, type, return_ptr))
+                    return;
+                out_reg->eax = (uint32_t)return_ptr;
+            } else {
+                if (!PushObject(obj, type, (uint8_t *)&out_reg->eax))
+                    return;
+            }
+        } break;
+        case PrimitiveKind::Array: { RG_UNREACHABLE(); } break;
+        case PrimitiveKind::Float32: {
+            if (RG_UNLIKELY(!value.IsNumber() && !value.IsBigInt())) {
+                ThrowError<Napi::TypeError>(env, "Unexpected %1 value for return value, expected number", GetValueType(instance, value));
+                return;
+            }
+
+            out_reg->f = CopyNumber<float>(value);
+        } break;
+        case PrimitiveKind::Float64: {
+            if (RG_UNLIKELY(!value.IsNumber() && !value.IsBigInt())) {
+                ThrowError<Napi::TypeError>(env, "Unexpected %1 value for return value, expected number", GetValueType(instance, value));
+                return;
+            }
+
+            out_reg->d = CopyNumber<double>(value);
+            out_reg->is_double = true;
+        } break;
+        case PrimitiveKind::Callback: {
+            void *ptr;
+
+            if (value.IsFunction()) {
+                Napi::Function func = value.As<Napi::Function>();
+
+                Size idx = ReserveTrampoline(type->proto, func);
+                if (RG_UNLIKELY(idx < 0))
+                    return;
+
+                ptr = GetTrampoline(idx, type->proto);
+            } else if (CheckValueTag(instance, value, type)) {
+                ptr = value.As<Napi::External<uint8_t>>().Data();
+            } else if (IsNullOrUndefined(value)) {
+                ptr = nullptr;
+            } else {
+                ThrowError<Napi::TypeError>(env, "Unexpected %1 value for return value, expected %2", GetValueType(instance, value), type->name);
+                return;
+            }
+
+            out_reg->eax = (uint32_t)ptr;
+        } break;
+    }
 }
 
-void *GetTrampoline(Size idx, const FunctionInfo *)
+void *GetTrampoline(Size idx, const FunctionInfo *proto)
 {
-    return nullptr;
+    bool x87 = IsFloat(proto->ret.type);
+    return Trampolines[idx][x87];
 }
 
 extern "C" void RelayCallBack(Size idx, uint8_t *own_sp, uint8_t *caller_sp, BackRegisters *out_reg)
--- src/abi_x86_fwd.asm
+++ src/abi_x86_fwd.asm
@@ -11,6 +11,9 @@
 ; You should have received a copy of the GNU Affero General Public License
 ; along with this program. If not, see https://www.gnu.org/licenses/.
 
+; Forward
+; ----------------------------
+
 public ForwardCallG
 public ForwardCallF
 public ForwardCallD
@@ -27,6 +30,8 @@ prologue macro
     endbr32
     push ebx
     mov ebx, esp
+    mov eax, dword ptr [esp+16]
+    mov dword ptr [eax+0], esp
     mov eax, dword ptr [esp+8]
     mov esp, dword ptr [esp+12]
 endm
@@ -79,5 +84,207 @@ ForwardCallRD proc
     fastcall
     epilogue
 ForwardCallRD endp
+
+; Callback trampolines
+; ----------------------------
+
+public Trampoline0
+public Trampoline1
+public Trampoline2
+public Trampoline3
+public Trampoline4
+public Trampoline5
+public Trampoline6
+public Trampoline7
+public Trampoline8
+public Trampoline9
+public Trampoline10
+public Trampoline11
+public Trampoline12
+public Trampoline13
+public Trampoline14
+public Trampoline15
+public TrampolineX0
+public TrampolineX1
+public TrampolineX2
+public TrampolineX3
+public TrampolineX4
+public TrampolineX5
+public TrampolineX6
+public TrampolineX7
+public TrampolineX8
+public TrampolineX9
+public TrampolineX10
+public TrampolineX11
+public TrampolineX12
+public TrampolineX13
+public TrampolineX14
+public TrampolineX15
+extern RelayCallBack : PROC
+public CallSwitchStack
+
+trampoline macro ID
+    endbr32
+    sub esp, 44
+    mov dword ptr [esp+0], ID
+    mov dword ptr [esp+4], esp
+    lea eax, dword ptr [esp+48]
+    mov dword ptr [esp+8], eax
+    lea eax, dword ptr [esp+16]
+    mov dword ptr [esp+12], eax
+    call RelayCallBack
+    mov eax, dword ptr[esp+16]
+    mov edx, dword ptr[esp+20]
+    add esp, 44
+    ret
+endm
+
+trampoline_x87 macro ID
+    local l1, l2, l3
+
+    endbr32
+    sub esp, 44
+    mov dword ptr [esp+0], ID
+    mov dword ptr [esp+4], esp
+    lea eax, dword ptr [esp+48]
+    mov dword ptr [esp+8], eax
+    lea eax, dword ptr [esp+16]
+    mov dword ptr [esp+12], eax
+    call RelayCallBack
+    cmp byte ptr[esp+36], 1
+    je l2
+l1:
+    fld dword ptr [esp+32]
+    jmp l3
+l2:
+    fld qword ptr [esp+24]
+l3:
+    mov eax, dword ptr[esp+16]
+    mov edx, dword ptr[esp+20]
+    add esp, 44
+    ret
+endm
+
+Trampoline0 proc
+    trampoline 0
+Trampoline0 endp
+Trampoline1 proc
+    trampoline 1
+Trampoline1 endp
+Trampoline2 proc
+    trampoline 2
+Trampoline2 endp
+Trampoline3 proc
+    trampoline 3
+Trampoline3 endp
+Trampoline4 proc
+    trampoline 4
+Trampoline4 endp
+Trampoline5 proc
+    trampoline 5
+Trampoline5 endp
+Trampoline6 proc
+    trampoline 6
+Trampoline6 endp
+Trampoline7 proc
+    trampoline 7
+Trampoline7 endp
+Trampoline8 proc
+    trampoline 8
+Trampoline8 endp
+Trampoline9 proc
+    trampoline 9
+Trampoline9 endp
+Trampoline10 proc
+    trampoline 10
+Trampoline10 endp
+Trampoline11 proc
+    trampoline 11
+Trampoline11 endp
+Trampoline12 proc
+    trampoline 12
+Trampoline12 endp
+Trampoline13 proc
+    trampoline 13
+Trampoline13 endp
+Trampoline14 proc
+    trampoline 14
+Trampoline14 endp
+Trampoline15 proc
+    trampoline 15
+Trampoline15 endp
+
+TrampolineX0 proc
+    trampoline_x87 0
+TrampolineX0 endp
+TrampolineX1 proc
+    trampoline_x87 1
+TrampolineX1 endp
+TrampolineX2 proc
+    trampoline_x87 2
+TrampolineX2 endp
+TrampolineX3 proc
+    trampoline_x87 3
+TrampolineX3 endp
+TrampolineX4 proc
+    trampoline_x87 4
+TrampolineX4 endp
+TrampolineX5 proc
+    trampoline_x87 5
+TrampolineX5 endp
+TrampolineX6 proc
+    trampoline_x87 6
+TrampolineX6 endp
+TrampolineX7 proc
+    trampoline_x87 7
+TrampolineX7 endp
+TrampolineX8 proc
+    trampoline_x87 8
+TrampolineX8 endp
+TrampolineX9 proc
+    trampoline_x87 9
+TrampolineX9 endp
+TrampolineX10 proc
+    trampoline_x87 10
+TrampolineX10 endp
+TrampolineX11 proc
+    trampoline_x87 11
+TrampolineX11 endp
+TrampolineX12 proc
+    trampoline_x87 12
+TrampolineX12 endp
+TrampolineX13 proc
+    trampoline_x87 13
+TrampolineX13 endp
+TrampolineX14 proc
+    trampoline_x87 14
+TrampolineX14 endp
+TrampolineX15 proc
+    trampoline_x87 15
+TrampolineX15 endp
+
+CallSwitchStack proc
+    endbr32
+    push ebx
+    mov ebx, esp
+    mov edx, dword ptr [esp+28]
+    mov ecx, dword ptr [esp+24]
+    mov eax, esp
+    sub eax, dword ptr [ecx+0]
+    and eax, -16
+    mov dword ptr [ecx+4], eax
+    mov esp, dword ptr [esp+20]
+    sub esp, 16
+    mov eax, dword ptr [ebx+8]
+    mov dword ptr [esp+0], eax
+    mov eax, dword ptr [ebx+12]
+    mov dword ptr [esp+4], eax
+    mov eax, dword ptr [ebx+16]
+    mov dword ptr [esp+8], eax
+    call edx
+    mov esp, ebx
+    pop ebx
+    ret
+CallSwitchStack endp
 
 end
