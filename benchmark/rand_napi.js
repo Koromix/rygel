dea7f5885524dc297772b4cd3a1217d3d6884827
Niels Martignène <niels.martignene@protonmail.com>
2022-06-14 14:04:20 +0200

    Add benchmark tests based around C rand()

---
+++ benchmark/rand_napi.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see https://www.gnu.org/licenses/.
+
+const rand = require('./build/rand_napi.node');
+
+let sum = 0;
+
+main();
+
+function main() {
+    let iterations = 20000000;
+
+    if (process.argv.length >= 3) {
+        iterations = parseInt(process.argv[2], 10);
+        if (Number.isNaN(iterations))
+            throw new Error('Not a valid number');
+        if (iterations < 1)
+            throw new Error('Value must be positive');
+    }
+    console.log('Iterations:', iterations);
+
+    rand.srand(42);
+
+    let start = performance.now();
+
+    for (let i = 0; i < iterations; i++) {
+        sum += rand.rand();
+    }
+
+    let time = performance.now()- start;
+    console.log('Time:', (time / 1000.0).toFixed(2) + 's');
+}
---
+++ benchmark/rand_node_ffi.js
@@ -0,0 +1,55 @@
+#!/usr/bin/env node
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see https://www.gnu.org/licenses/.
+
+const ref = require('ref-napi');
+const ffi = require('ffi-napi');
+const struct = require('ref-struct-di')(ref);
+
+let sum = 0;
+
+main();
+
+async function main() {
+    let iterations = 20000000;
+
+    if (process.argv.length >= 3) {
+        iterations = parseInt(process.argv[2], 10);
+        if (Number.isNaN(iterations))
+            throw new Error('Not a valid number');
+        if (iterations < 1)
+            throw new Error('Value must be positive');
+    }
+    console.log('Iterations:', iterations);
+
+    const lib = ffi.Library(process.platform == 'win32' ? 'msvcrt.dll' : null, {
+        srand: ['void', ['uint']],
+        rand: ['int', []]
+    });
+
+    lib.srand(42);
+
+    let start = performance.now();
+
+    for (let i = 0; i < iterations; i++) {
+        if (i % 1000000 == 0)
+            await new Promise(resolve => setTimeout(resolve, 0));
+
+        sum += lib.rand();
+    }
+
+    let time = performance.now()- start;
+    console.log('Time:', (time / 1000.0).toFixed(2) + 's');
+}
+
