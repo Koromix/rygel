1ea3abe3b9e4ce009169eb44f1105617f2e5ba3f
Niels Martignène <niels.martignene@protonmail.com>
2022-07-01 13:05:46 +0200

    Document command to install manage_hds.py dependencies

--- deploy/ansible/manage_hds.py
+++ deploy/ansible/manage_hds.py
@@ -13,6 +13,8 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
 
+# pip install pynacl ruamel.yaml ansible-vault
+
 import os
 from nacl import public as nacl
 import base64
