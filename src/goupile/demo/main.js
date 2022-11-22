66079ce6797b0d450cde4d9051a85a38ed33732f
Niels Martignène <niels.martignene@protonmail.com>
2022-11-22 10:30:35 +0100

    Switch to app.form() in demo

--- src/goupile/demo/main.js
+++ src/goupile/demo/main.js
@@ -1,7 +1,7 @@
 // Ce fichier définit un formulaire composé de 3 pages
 
 app.form("suivi", "Suivi", () => {
-    app.page("introduction", "Introduction")
-    app.page("avance", "Avancé")
-    app.page("mise_en_page", "Mise en page")
+    app.form("introduction", "Introduction")
+    app.form("avance", "Avancé")
+    app.form("mise_en_page", "Mise en page")
 })
