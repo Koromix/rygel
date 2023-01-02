2f088ab358a3655a8cd52beaf1acc20e0cbf6dba
Niels Martignène <niels.martignene@protonmail.com>
2023-01-02 16:29:13 +0100

    Improve API and support detection in JS SQLite3 wrapper

--- src/web/libjs/sqlite3.js
+++ src/web/libjs/sqlite3.js
@@ -11,105 +11,156 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see https://www.gnu.org/licenses/.
 
-async function sqlite3(filename) {
-    if (sqlite3.promiser == null) {
-        self.id = 0;
+const sqlite3 = new function() {
+    let self = this;
 
-        sqlite3.promiser = await new Promise((resolve, reject) => {
+    this.worker = 'sqlite3-worker1.js';
+
+    if (document.currentScript && document.currentScript.src) {
+        let parts = document.currentScript.src.split('/');
+        parts.pop();
+        this.worker = parts.join('/') + '/' + this.worker;
+    }
+
+    let promiser = null;
+    let id = 0;
+
+    this.open = async function(filename) {
+        await init();
+
+        if (filename == null)
+            filename = ':memory:';
+
+        if (filename != ':memory:') {
+            if (!self.hasPersistence())
+                throw new Error('This browser does not support persistent SQLite3 databases');
+
+            filename = `file:${filename}?vfs=opfs`;
+        }
+
+        try {
+            await promiser({ type: 'open', dbId: ++id, args: { filename: filename }} );
+        } catch (msg) {
+            throw msg.result;
+        }
+
+        let db = new DatabaseWrapper(promiser, id);
+        return db;
+    };
+
+    this.hasPersistence = function() {
+        if (!window.crossOriginIsolated)
+            return false;
+        if (!window.SharedArrayBuffer)
+            return false;
+        if (!window.Atomics)
+            return false;
+        if (!window.FileSystemHandle)
+            return false;
+        if (!window.FileSystemDirectoryHandle)
+            return false;
+        if (!window.FileSystemFileHandle)
+            return false;
+        // if (!window.FileSystemFileHandle.prototype.createSyncAccessHandle)
+        //    return false;
+        if (!navigator.storage.getDirectory)
+            return false;
+
+        return true;
+    };
+
+    async function init() {
+        if (promiser != null)
+            return;
+
+        promiser = await new Promise((resolve, reject) => {
             let sqlite3 = null;
+            let worker = null;
+
+            if (typeof self.worker === 'string') {
+                worker = new Worker(self.worker);
+            } else if (typeof self.worker === 'function') {
+                worker = self.worker();
+            }
+            worker.onerror = e => reject(new Error('Failed to load SQLite3 worker script'));
 
             let config = {
-                onready: () => resolve(sqlite3),
-                onerror: err => reject(err)
+                worker: worker,
+                onready: () => resolve(sqlite3)
             };
 
             sqlite3 = sqlite3Worker1Promiser(config);
         });
     }
 
-    let promiser = sqlite3.promiser;
-    let id = ++sqlite3.id;
+    function DatabaseWrapper(promiser, db) {
+        let self = this;
 
-    if (filename != null && filename != ':memory:')
-        filename = `file:${filename}?vfs=opfs`;
+        this.exec = async function(sql, ...args) {
+            try {
+                await promiser({ type: 'exec', dbId: db, args: {
+                    sql: sql,
+                    bind: args
+                }});
+            } catch (msg) {
+                throw msg.result;
+            }
+        };
 
-    try {
-        await sqlite3.promiser({ type: 'open', dbId: id, args: { filename: filename }} );
-    } catch (msg) {
-        throw msg.result;
-    }
+        this.fetch1 = async function(sql, ...args) {
+            let result = await new Promise((resolve, reject) => {
+                let p = promiser({ type: 'exec', dbId: db, args: {
+                    sql: sql,
+                    bind: args,
+                    rowMode: 'object',
+                    callback: msg => {
+                        if (msg.rowNumber != null) {
+                            resolve(msg.row);
+                        } else {
+                            resolve(null);
+                        }
 
-    let db = new SQLiteDatabase(sqlite3.promiser, id);
-    return db;
-}
-
-function SQLiteDatabase(promiser, db) {
-    let self = this;
-
-    this.exec = async function(sql, ...args) {
-        try {
-            await promiser({ type: 'exec', dbId: db, args: {
-                sql: sql,
-                bind: args
-            }});
-        } catch (msg) {
-            throw msg.result;
-        }
-    };
-
-    this.fetch1 = async function(sql, ...args) {
-        let result = await new Promise((resolve, reject) => {
-            let p = promiser({ type: 'exec', dbId: db, args: {
-                sql: sql,
-                bind: args,
-                rowMode: 'object',
-                callback: msg => {
-                    if (msg.rowNumber != null) {
-                        resolve(msg.row);
-                    } else {
-                        resolve(null);
-                    }
-
-                    return false;
-                }
-            }});
-
-            p.catch(msg => reject(msg.result));
-        });
-
-        return result;
-    };
-
-    this.fetchAll = async function(sql, ...args) {
-        let results = await new Promise((resolve, reject) => {
-            let results = [];
-
-            let p = promiser({ type: 'exec', dbId: db, args: {
-                sql: sql,
-                bind: args,
-                rowMode: 'object',
-                callback: msg => {
-                    if (msg.rowNumber == null) {
-                        resolve(results);
                         return false;
                     }
+                }});
 
-                    results.push(msg.row);
-                    return true;
-                }
-            }});
+                p.catch(msg => reject(msg.result));
+            });
 
-            p.catch(msg => reject(msg.result));
-        });
+            return result;
+        };
 
-        return results;
-    };
+        this.fetchAll = async function(sql, ...args) {
+            let results = await new Promise((resolve, reject) => {
+                let results = [];
 
-    this.close = async function() {
-        try {
-            await promiser('close', { dbId: db });
-        } catch (msg) {
-            throw msg.result;
-        }
-    };
-}
+                let p = promiser({ type: 'exec', dbId: db, args: {
+                    sql: sql,
+                    bind: args,
+                    rowMode: 'object',
+                    callback: msg => {
+                        if (msg.rowNumber == null) {
+                            resolve(results);
+                            return false;
+                        }
+
+                        results.push(msg.row);
+                        return true;
+                    }
+                }});
+
+                p.catch(msg => reject(msg.result));
+            });
+
+            return results;
+        };
+
+        this.close = async function() {
+            try {
+                await promiser('close', { dbId: db });
+            } catch (msg) {
+                throw msg.result;
+            }
+        };
+    }
+};
