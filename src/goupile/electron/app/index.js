388c8f5ac8ada8cc6ce0ee8c6389af995c1638f2
Niels Martignène <niels.martignene@protonmail.com>
2023-01-12 10:11:24 +0100

    Drop USB hacks from Electron client

--- src/goupile/electron/app/index.js
+++ src/goupile/electron/app/index.js
@@ -2,45 +2,11 @@
 
 const settings = require('./package.json');
 const path = require('path');
-const fs = require('fs');
 const { app, BrowserWindow, dialog, ipcMain, Menu, MenuItem } = require('electron');
 const { autoUpdater } = require('electron-updater');
 
 if (app.requestSingleInstanceLock()) {
     let win = null;
-
-    ipcMain.on('get_usb_key', (e, id) => {
-        let drives = 'DEFGHIJKLMNOPQRSTUVWXYZ';
-
-        for (let drive of drives) {
-            let filename = `${drive}:/Goupile/${id}`;
-
-            if (fs.existsSync(filename)) {
-                let key = fs.readFileSync(filename, { encoding: 'ascii' });
-                key = key.trim();
-
-                e.returnValue = key;
-                return;
-            }
-        }
-
-        e.returnValue = null;
-    });
-
-    ipcMain.on('has_usb_key', (e, id) => {
-        let drives = 'DEFGHIJKLMNOPQRSTUVWXYZ';
-
-        for (let drive of drives) {
-            let filename = `${drive}:/Goupile/${id}`;
-
-            if (fs.existsSync(filename)) {
-                e.returnValue = true;
-                return;
-            }
-        }
-
-        e.returnValue = false;
-    })
 
     app.on('ready', async () => {
         if (isPackaged())
