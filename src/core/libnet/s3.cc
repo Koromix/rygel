7d22acc0e1cf68a7d18a76c3c92007554aaa0256
Niels Martignène <niels.martignene@protonmail.com>
2023-03-27 09:36:04 +0200

    Share TLS stuff between S3 curl requests

--- src/core/libnet/s3.cc
+++ src/core/libnet/s3.cc
@@ -238,6 +238,7 @@ s3_Session::s3_Session()
 
     curl_share_setopt(share, CURLSHOPT_SHARE, CURL_LOCK_DATA_DNS);
     curl_share_setopt(share, CURLSHOPT_SHARE, CURL_LOCK_DATA_CONNECT);
+    curl_share_setopt(share, CURLSHOPT_SHARE, CURL_LOCK_DATA_SSL_SESSION);
 }
 
 s3_Session::~s3_Session()
