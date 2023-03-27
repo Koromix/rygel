393169d417c124c5965d0884e35753210b6abc6e
Niels Martignène <niels.martignene@protonmail.com>
2023-03-27 08:25:04 +0200

    Use 64 threads for S3 requests

--- src/rekord/disk_s3.cc
+++ src/rekord/disk_s3.cc
@@ -56,7 +56,7 @@ S3Disk::S3Disk(const s3_Config &config, int threads)
         this->threads = threads;
     } else {
         // S3 is slow unless you use parallelism
-        this->threads = GetCoreCount() * 10;
+        this->threads = 64;
     }
 
     // We're good!
