93141fff9f5ac58962c7b203c7edaa6c58c2858b
Niels Martignène <niels.martignene@protonmail.com>
2023-05-26 11:01:05 +0200

    Optimize felix bootstrap scripts

--- src/felix/bootstrap.bat
+++ src/felix/bootstrap.bat
@@ -4,41 +4,44 @@ setlocal enableDelayedExpansion
 cd %~dp0
 
 set SRC=*.cc ..\core\libcc\libcc.cc ..\core\libwrap\json.cc ..\..\vendor\pugixml\src\pugixml.cpp
-set BIN=..\..\felix.exe
-set BUILD=..\..\bin\BootstrapFelix
+set BINARY=..\..\felix.exe
+set TEMP=..\..\bin\BootstrapFelix
+set BUILD=..\..\bin\Fast
+
+if EXIST %BINARY% (
+    %BINARY% -pFast felix && copy %BUILD%\felix.exe %BINARY% && exit /B
+)
 
 where /q cl
 if NOT ERRORLEVEL 1 (
     where /q clang-cl
     if NOT ERRORLEVEL 1 (
         echo Bootstrapping felix with Clang...
-        mkdir %BUILD%
-        clang-cl /nologo /std:c++latest /I../.. /W0 /EHsc /MP /DNDEBUG /DNOMINMAX /DWINVER=0x0601 /D_WIN32_WINNT=0x0601 /DUNICODE /D_UNICODE /c %SRC% /Fo%BUILD%\
-        lld-link /nologo %BUILD%\*.obj ws2_32.lib advapi32.lib shell32.lib ole32.lib /out:%BUILD%\felix.exe
-        %BUILD%\felix.exe --no_presets --features=OptimizeSpeed,StaticLink -O %BUILD%\Fast felix
-        move %BUILD%\Fast\felix.exe %BIN%
+        mkdir %TEMP%
+        clang-cl /nologo /std:c++latest /I../.. /W0 /EHsc /MP /DNDEBUG /DNOMINMAX /DWINVER=0x0601 /D_WIN32_WINNT=0x0601 /DUNICODE /D_UNICODE /c %SRC% /Fo%TEMP%\
+        lld-link /nologo %TEMP%\*.obj ws2_32.lib advapi32.lib shell32.lib ole32.lib /out:%TEMP%\felix.exe
+        %TEMP%\felix.exe -pFast felix
+        copy %BUILD%\felix.exe %BINARY% >NUL
 
         echo Cleaning up...
-        ping -n 4 127.0.0.1 >NUL
-        rmdir /S /Q %BUILD%\Fast
-        del /Q %BUILD%\*
-        rmdir /Q %BUILD%
+        ping -n 2 127.0.0.1 >NUL
+        del /Q %TEMP%\*
+        rmdir /Q %TEMP%
 
         exit /B
     )
 
     echo Bootstrapping felix with MSVC...
-    mkdir %BUILD%
-    cl /nologo /std:c++latest /I../.. /W0 /EHsc /MP /DNDEBUG /DNOMINMAX /DWINVER=0x0601 /D_WIN32_WINNT=0x0601 /DUNICODE /D_UNICODE /c %SRC% /Fo%BUILD%\
-    link /nologo %BUILD%\*.obj ws2_32.lib advapi32.lib shell32.lib ole32.lib /out:%BUILD%\felix.exe
-    %BUILD%\felix.exe --no_presets --features=OptimizeSpeed,StaticLink -O %BUILD%\Fast felix
-    move %BUILD%\Fast\felix.exe %BIN%
+    mkdir %TEMP%
+    cl /nologo /std:c++latest /I../.. /W0 /EHsc /MP /DNDEBUG /DNOMINMAX /DWINVER=0x0601 /D_WIN32_WINNT=0x0601 /DUNICODE /D_UNICODE /c %SRC% /Fo%TEMP%\
+    link /nologo %TEMP%\*.obj ws2_32.lib advapi32.lib shell32.lib ole32.lib /out:%TEMP%\felix.exe
+    %TEMP%\felix.exe -pFast felix
+    copy %BUILD%\felix.exe %BINARY% >NUL
 
     echo Cleaning up...
-    ping -n 4 127.0.0.1 >NUL
-    rmdir /S /Q %BUILD%\Fast
-    del /Q %BUILD%\*
-    rmdir /Q %BUILD%
+    ping -n 2 127.0.0.1 >NUL
+    del /Q %TEMP%\*
+    rmdir /Q %TEMP%
 
     exit /B
 )
@@ -46,16 +49,15 @@ if NOT ERRORLEVEL 1 (
 where /q g++
 if NOT ERRORLEVEL 1 (
     echo Bootstrapping felix with GCC...
-    mkdir %BUILD%
-    g++ -std=gnu++2a -O0 -I../.. -DNDEBUG -DNOMINMAX  -DWINVER=0x0601 -D_WIN32_WINNT=0x0601 -DUNICODE -D_UNICODE %SRC% -lws2_32 -ladvapi32 -lshell32 -lole32 -luuid -w -o%BUILD%\felix.exe
-    %BUILD%\felix.exe --no_presets --features=OptimizeSpeed,StaticLink -O %BUILD%\Fast felix
-    move %BUILD%\Fast\felix.exe %BIN%
+    mkdir %TEMP%
+    g++ -std=gnu++2a -O0 -I../.. -DNDEBUG -DNOMINMAX  -DWINVER=0x0601 -D_WIN32_WINNT=0x0601 -DUNICODE -D_UNICODE %SRC% -lws2_32 -ladvapi32 -lshell32 -lole32 -luuid -w -o%TEMP%\felix.exe
+    %TEMP%\felix.exe -pFast felix
+    copy %BUILD%\felix.exe %BINARY% >NUL
 
     echo Cleaning up...
-    ping -n 4 127.0.0.1 >NUL
-    rmdir /S /Q %BUILD%\Fast
-    del /Q %BUILD%\*
-    rmdir /Q %BUILD%
+    ping -n 2 127.0.0.1 >NUL
+    del /Q %TEMP%\*
+    rmdir /Q %TEMP%
 
     exit /B
 )
--- src/felix/bootstrap.sh
+++ src/felix/bootstrap.sh
@@ -3,35 +3,39 @@
 cd $(dirname $0)
 
 SRC=$(ls -1 ../felix/*.cc ../core/libcc/libcc.cc ../core/libwrap/json.cc ../../vendor/pugixml/src/pugixml.cpp)
-BIN=../../felix
-BUILD=../../bin/BootstrapFelix
+
+TEMP=../../bin/BootstrapFelix
+BUILD=../../bin/Fast
+BINARY=../../felix
+
+if command -v $BINARY >/dev/null 2>&1; then
+    $BINARY -pFast felix && cp $BUILD/felix $BINARY && exit
+fi
 
 if command -v clang++ >/dev/null 2>&1; then
     echo "Bootstrapping felix with Clang..."
-    mkdir -p $BUILD
-    clang++ -std=gnu++2a -O0 -I../.. -DNDEBUG $SRC -Wno-everything -pthread -o $BUILD/felix
-    $BUILD/felix --no_presets --features=OptimizeSpeed -O $BUILD/Fast felix
-    mv $BUILD/Fast/felix $BIN
+    mkdir -p $TEMP
+    clang++ -std=gnu++2a -O0 -I../.. -DNDEBUG $SRC -Wno-everything -pthread -o $TEMP/felix
+    $TEMP/felix -pFast felix
+    cp $BUILD/felix $BINARY
 
     echo "Cleaning up..."
-    rm -rf $BUILD/Fast
-    rm -f $BUILD/*
-    rmdir $BUILD
+    rm -f $TEMP/*
+    rmdir $TEMP
 
     exit
 fi
 
 if command -v g++ >/dev/null 2>&1; then
     echo "Bootstrapping felix with GCC..."
-    mkdir -p $BUILD
-    g++ -std=gnu++2a -O0 -I../.. -DNDEBUG $SRC -w -pthread -o $BUILD/felix
-    $BUILD/felix --no_presets --features=OptimizeSpeed -O $BUILD/Fast felix
-    mv $BUILD/Fast/felix $BIN
+    mkdir -p $TEMP
+    g++ -std=gnu++2a -O0 -I../.. -DNDEBUG $SRC -w -pthread -o $TEMP/felix
+    $TEMP/felix -pFast felix
+    cp $BUILD/felix $BINARY
 
     echo "Cleaning up..."
-    rm -rf $BUILD/Fast
-    rm -f $BUILD/*
-    rmdir $BUILD
+    rm -f $TEMP/*
+    rmdir $TEMP
 
     exit
 fi
