8d22ef5d201d94479532a65de71c0e10e5ba4929
Niels Martignène <niels.martignene@protonmail.com>
2023-06-07 11:57:47 +0200

    Add daemon mode to Meestic (Linux)

--- src/meestic/meestic.cc
+++ src/meestic/meestic.cc
@@ -14,19 +14,426 @@
 #include "src/core/libcc/libcc.hh"
 #include "config.hh"
 #include "lights.hh"
+#include "src/core/libsandbox/libsandbox.hh"
 #include "vendor/libhs/libhs.h"
+
+#ifdef __linux__
+    #include <sys/types.h>
+    #include <sys/stat.h>
+    #include <sys/poll.h>
+    #include <sys/socket.h>
+    #include <fcntl.h>
+    #include <linux/input.h>
+#endif
 
 namespace RG {
 
-int Main(int argc, char **argv)
-{
-    RG_CRITICAL(argc >= 1, "First argument is missing");
+#ifdef __linux__
 
+static Config config;
+static Size profile_idx = 0;
+
+static hs_port *port = nullptr;
+
+static bool ApplySandbox()
+{
+    if (!sb_IsSandboxSupported()) {
+        LogError("Sandbox mode is not supported on this platform");
+        return false;
+    }
+
+    sb_SandboxBuilder sb;
+
+    sb.FilterSyscalls(sb_FilterAction::Kill, {
+        {"exit", sb_FilterAction::Allow},
+        {"exit_group", sb_FilterAction::Allow},
+        {"brk", sb_FilterAction::Allow},
+        {"mmap/anon", sb_FilterAction::Allow},
+        {"munmap", sb_FilterAction::Allow},
+        {"close", sb_FilterAction::Allow},
+        {"fcntl", sb_FilterAction::Allow},
+        {"read", sb_FilterAction::Allow},
+        {"readv", sb_FilterAction::Allow},
+        {"write", sb_FilterAction::Allow},
+        {"writev", sb_FilterAction::Allow},
+        {"pread64", sb_FilterAction::Allow},
+        {"fsync", sb_FilterAction::Allow},
+        {"poll", sb_FilterAction::Allow},
+        {"ppoll", sb_FilterAction::Allow},
+        {"clock_nanosleep", sb_FilterAction::Allow},
+        {"clock_gettime", sb_FilterAction::Allow},
+        {"clock_gettime64", sb_FilterAction::Allow},
+        {"clock_nanosleep", sb_FilterAction::Allow},
+        {"clock_nanosleep_time64", sb_FilterAction::Allow},
+        {"nanosleep", sb_FilterAction::Allow},
+        {"ioctl", sb_FilterAction::Allow},
+        {"getpid", sb_FilterAction::Allow},
+        {"recv", sb_FilterAction::Allow},
+        {"recvfrom", sb_FilterAction::Allow},
+        {"recvmmsg", sb_FilterAction::Allow},
+        {"recvmmsg_time64", sb_FilterAction::Allow},
+        {"recvmsg", sb_FilterAction::Allow},
+        {"sendmsg", sb_FilterAction::Allow},
+        {"sendmmsg", sb_FilterAction::Allow},
+        {"sendto", sb_FilterAction::Allow},
+        {"rt_sigaction", sb_FilterAction::Allow},
+        {"rt_sigpending", sb_FilterAction::Allow},
+        {"rt_sigprocmask", sb_FilterAction::Allow},
+        {"rt_sigqueueinfo", sb_FilterAction::Allow},
+        {"rt_sigreturn", sb_FilterAction::Allow},
+        {"rt_sigsuspend", sb_FilterAction::Allow},
+        {"rt_sigtimedwait", sb_FilterAction::Allow},
+        {"rt_sigtimedwait_time64", sb_FilterAction::Allow},
+        {"kill", sb_FilterAction::Allow},
+        {"tgkill", sb_FilterAction::Allow}
+    });
+
+    return sb.Apply();
+}
+
+static int OpenInputDevice(const char *needle, int flags)
+{
+    BlockAllocator temp_alloc;
+
+    int ret_fd = -1;
+
+    EnumerateDirectory("/dev/input", "event*", 1024, [&](const char *basename, FileType) {
+        const char *filename = Fmt(&temp_alloc, "/dev/input/%1", basename).ptr;
+
+        // Open device
+        int fd = open(filename, O_RDONLY | O_CLOEXEC | flags);
+        if (fd < 0) {
+            LogError("Failed to open '%1': %2", filename, strerror(errno));
+            return true;
+        }
+        RG_DEFER_N(fd_guard) { close(fd); };
+
+        char name[256];
+        if (ioctl(fd, EVIOCGNAME(RG_LEN(name)), name) < 0) {
+            LogError("Failed to get device name of '%1': %2", filename, strerror(errno));
+            return true;
+        }
+
+        if (TestStr(name, needle)) {
+            ret_fd = fd;
+            fd_guard.Disable();
+
+            return false;
+        }
+
+        return true;
+    });
+
+    if (ret_fd < 0) {
+        LogError("Cannot find input device '%1'", needle);
+        return -1;
+    }
+
+    return ret_fd;
+}
+
+static bool ApplyProfile(Size idx)
+{
+    LogInfo("Applying profile %1", idx);
+
+    if (!ApplyLight(port, config.profiles[idx].settings))
+        return false;
+
+    profile_idx = idx;
+    return true;
+}
+
+static bool ToggleProfile(int delta)
+{
+    if (!delta)
+        return true;
+
+    Size next_idx = profile_idx;
+
+    do {
+        next_idx += delta;
+
+        if (next_idx < 0) {
+            next_idx = config.profiles.len - 1;
+        } else if (next_idx >= config.profiles.len) {
+            next_idx = 0;
+        }
+    } while (config.profiles[next_idx].manual);
+
+    return ApplyProfile(next_idx);
+}
+
+static bool HandleInputEvent(int fd)
+{
+    struct input_event ev = {};
+    Size len = read(fd, &ev, RG_SIZE(ev));
+
+    if (len < 0) {
+        if (errno == EAGAIN)
+            return true;
+
+        LogError("Failed to read evdev event: %1", strerror(errno));
+        return false;
+    }
+    RG_ASSERT(len == RG_SIZE(ev));
+
+    if (ev.type == EV_KEY && ev.code == BTN_TRIGGER_HAPPY40 && ev.value == 1) {
+        ToggleProfile(1);
+    }
+
+    return true;
+}
+
+static bool HandleClientConnect(int fd)
+{
+    const auto write = [&](Span<const uint8_t> buf) {
+        while (buf.len) {
+            Size sent = send(fd, buf.ptr, (size_t)buf.len, 0);
+            if (sent < 0) {
+                LogError("Failed to send data to client: %1", strerror(errno));
+                return false;
+            }
+
+            buf.ptr += sent;
+            buf.len -= sent;
+        }
+
+        return true;
+    };
+
+    StreamWriter writer(write, "<client>");
+
+    for (const ConfigProfile &profile: config.profiles) {
+        PrintLn(&writer, "[%1]", profile.name);
+
+        PrintLn(&writer, "Mode = %1", LightModeOptions[(int)profile.settings.mode].name);
+        PrintLn(&writer, "Speed = %1", profile.settings.speed);
+        PrintLn(&writer, "Intensity = %1", profile.settings.intensity);
+
+        Print(&writer, "Colors =");
+        for (RgbColor color: profile.settings.colors) {
+            Print(&writer, " #%1%2%3", FmtHex(color.red).Pad0(-2), FmtHex(color.blue).Pad0(-2), FmtHex(color.green).Pad0(-2));
+        }
+        PrintLn(&writer);
+
+        PrintLn(&writer);
+    }
+
+    // Signal EOF (full config)
+    shutdown(fd, SHUT_WR);
+
+    return writer.IsValid();
+}
+
+static bool HandleClientMessage(int fd)
+{
+    uint8_t payload;
+    Size received = recv(fd, &payload, RG_SIZE(payload), 0);
+
+    if (received < 0) {
+        if (errno == EAGAIN || errno == EWOULDBLOCK)
+            return true;
+
+        LogError("Failed to read client data");
+        return false;
+    } else if (!received) {
+        return false;
+    }
+
+    if (payload < 0x80) {
+        Size idx = payload;
+
+        if (idx >= config.profiles.len) {
+            LogError("Client asked for invalid profile");
+            return false;
+        }
+        if (!ApplyProfile(idx))
+            return false;
+    } else if (payload == 0x80) {
+        if (!ToggleProfile(-1))
+            return false;
+    } else if (payload == 0x81) {
+        if (!ToggleProfile(1))
+            return false;
+    } else {
+        LogError("Invalid command 0x%1 from client", FmtHex(payload));
+        return false;
+    }
+
+    return true;
+}
+
+static int RunDaemon(Span<const char *> arguments)
+{
+    BlockAllocator temp_alloc;
+
+    // Default config filename
+    LocalArray<const char *, 4> config_filenames;
+    const char *config_filename = FindConfigFile("MeesticGui.ini", &temp_alloc, &config_filenames);
+    const char *socket_filename = "/run/meestic.sock";
+    bool sandbox = false;
+
+    const auto print_usage = [=](FILE *fp) {
+        PrintLn(fp,
+R"(Usage: %!..+%1 daemon [options]%!0
+
+Options:
+    %!..+-C, --config_file <file>%!0     Set configuration file
+                                 %!D..(default: %2)%!0
+    %!..+-S, --socket_file <socket>%!0   Change control socket
+                                 %!D..(default: %3)%!0
+
+        %!..+--sandbox%!0                Run in strict OS sandbox (if supported))",
+                FelixTarget, FmtSpan(config_filenames.As()), socket_filename);
+    };
+
+    // Parse options
+    {
+        OptionParser opt(arguments);
+
+        while (opt.Next()) {
+            if (opt.Test("--help")) {
+                print_usage(stdout);
+                return 0;
+            } else if (opt.Test("-C", "--config_file", OptionType::Value)) {
+                if (IsDirectory(opt.current_value)) {
+                    config_filename = Fmt(&temp_alloc, "%1%/meestic.ini", TrimStrRight(opt.current_value, RG_PATH_SEPARATORS)).ptr;
+                } else {
+                    config_filename = opt.current_value;
+                }
+            } else if (opt.Test("-S", "--socket_file", OptionType::Value)) {
+                socket_filename = opt.current_value;
+            } else if (opt.Test("--sandbox")) {
+                sandbox = true;
+            } else if (opt.TestHasFailed()) {
+                return 1;
+            }
+        }
+    }
+
+    // Parse config file
+    if (config_filename && TestFile(config_filename, FileType::File)) {
+        if (!LoadConfig(config_filename, &config))
+            return 1;
+
+        if (config.profiles.len >= 128) {
+            LogError("Too many profiles (maximum = 128)");
+            return 1;
+        }
+
+        profile_idx = config.default_idx;
+    } else {
+        config.profiles.Append({ .name = "Enable", .settings = { .mode = LightMode::Static } });
+        config.profiles.Append({ .name = "Disable", .settings = { .mode = LightMode::Disabled } });
+    }
+
+    // Open the keyboard for Fn keys
+    int input_fd = OpenInputDevice("AT Translated Set 2 keyboard", O_NONBLOCK);
+    if (input_fd < 0)
+        return 1;
+
+    // Open the light MSI HID device ahead of time
+    port = OpenLightDevice();
+    if (!port)
+        return 1;
+    RG_DEFER { CloseLightDevice(port); };
+
+    // Open control socket
+    int listen_fd = OpenUnixSocket(socket_filename);
+    if (listen_fd < 0)
+        return 1;
+    RG_DEFER { close(listen_fd); };
+
+    // Listen for clients
+    if (listen(listen_fd, 4) < 0) {
+        LogError("listen() failed: %1", strerror(errno));
+        return 1;
+    }
+
+    if (sandbox && !ApplySandbox())
+        return 1;
+
+    // Check that it works once, at least
+    if (!ApplyProfile(config.default_idx))
+        return 1;
+
+    // From here on, don't quit abruptly
+    WaitForInterrupt(0);
+
+    // Wait for events and clients
+    {
+        HeapArray<struct pollfd> pfds;
+
+        pfds.Append({ input_fd, POLLIN, 0 });
+        pfds.Append({ listen_fd, POLLIN, 0 });
+
+        for (;;) {
+            if (poll(pfds.ptr, pfds.len, -1) < 0) {
+                if (errno == EINTR) {
+                    if (WaitForResult(0) == WaitForResult::Interrupt) {
+                        break;
+                    } else {
+                        continue;
+                    }
+                }
+
+                LogError("Failed to poll I/O descriptors: %1", strerror(errno));
+                return 1;
+            }
+
+            // Handle input events
+            if (pfds[0].revents && !HandleInputEvent(input_fd))
+                return 1;
+
+            // Accept new clients
+            if (pfds[1].revents) {
+                int fd = accept4(listen_fd, nullptr, nullptr, SOCK_NONBLOCK | SOCK_CLOEXEC);
+
+                if (fd >= 0) {
+                    if (HandleClientConnect(fd)) {
+                        pfds.Append({ fd, POLLIN, 0 });
+                    } else {
+                        close(fd);
+                    }
+                } else {
+                    LogError("Failed to accept new client: %1", strerror(errno));
+                }
+            }
+
+            // Handle existing clients
+            {
+                Size j = 2;
+                for (Size i = 2; i < pfds.len; i++) {
+                    pfds[j] = pfds[i];
+
+                    bool keep = true;
+                    if (pfds[i].revents & POLLIN) {
+                        keep &= HandleClientMessage(pfds[i].fd);
+                    }
+                    if (!keep) {
+                        close(pfds[i].fd);
+                        continue;
+                    }
+
+                    j++;
+                }
+                pfds.len = j;
+            }
+        }
+    }
+
+    return 0;
+}
+
+#endif
+
+static int RunChange(Span<const char *> arguments)
+{
     // Options
     LightSettings settings;
 
     const auto print_usage = [=](FILE *fp) {
-        PrintLn(fp, R"(Usage: %!..+%1 [options...] [colors...]%!0
+        PrintLn(fp, R"(Usage: %!..+%1 change [options...] [colors...]%!0
 
 Options:
     %!..+-m, --mode <mode>%!0            Set light mode (see below)
@@ -68,13 +475,6 @@ Examples:
 Be careful, color names and most options are %!..+case-sensitive%!0.)", FelixTarget);
     };
 
-    // Handle version
-    if (argc >= 2 && TestStr(argv[1], "--version")) {
-        PrintLn("%!R..%1%!0 %!..+%2%!0", FelixTarget, FelixVersion);
-        PrintLn("Compiler: %1", FelixCompiler);
-        return 0;
-    }
-
     // Harmonize log output
     hs_log_set_handler([](hs_log_level level, int, const char *msg, void *) {
         switch (level) {
@@ -86,7 +486,7 @@ Be careful, color names and most options are %!..+case-sensitive%!0.)", FelixTar
 
     // Parse options
     {
-        OptionParser opt(argc, argv);
+        OptionParser opt(arguments);
 
         while (opt.Next()) {
             if (opt.Test("--help")) {
@@ -131,6 +531,56 @@ Be careful, color names and most options are %!..+case-sensitive%!0.)", FelixTar
 
     LogInfo("Done!");
     return 0;
+}
+
+int Main(int argc, char **argv)
+{
+    RG_CRITICAL(argc >= 1, "First argument is missing");
+
+    // Handle help and version arguments
+    if (argc >= 2) {
+        if (TestStr(argv[1], "--help") || TestStr(argv[1], "help")) {
+            if (argc >= 3 && argv[2][0] != '-') {
+                argv[1] = argv[2];
+                argv[2] = const_cast<char *>("--help");
+            } else {
+                const char *args[] = {"--help"};
+                return RunChange(args);
+            }
+        } else if (TestStr(argv[1], "--version")) {
+            PrintLn("%!R..%1%!0 %!..+%2%!0", FelixTarget, FelixVersion);
+            PrintLn("Compiler: %1", FelixCompiler);
+
+            return 0;
+        }
+    }
+
+    const char *cmd = nullptr;
+    Span<const char *> arguments = {};
+
+    if (argc >= 2) {
+        cmd = argv[1];
+
+        if (cmd[0] == '-') {
+            cmd = "change";
+            arguments = MakeSpan((const char **)argv + 1, argc - 1);
+        } else {
+            arguments = MakeSpan((const char **)argv + 2, argc - 2);
+        }
+    } else {
+        cmd = "change";
+    }
+
+    if (TestStr(cmd, "change")) {
+        return RunChange(arguments);
+#ifdef __linux__
+    } else if (TestStr(cmd, "daemon")) {
+        return RunDaemon(arguments);
+#endif
+    } else {
+        LogError("Unknown command '%1'", cmd);
+        return 1;
+    }
 }
 
 }
