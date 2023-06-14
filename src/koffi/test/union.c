e0af4454b5741117d0f551d3e7c4c556f9a4ba5f
Niels Martignène <niels.martignene@protonmail.com>
2023-06-14 13:12:54 +0200

    Add some basic tests for Koffi unions

---
+++ src/koffi/test/union.c
@@ -0,0 +1,100 @@
+// Copyright 2023 Niels Martignène <niels.martignene@protonmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the “Software”), to deal in 
+// the Software without restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+// Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <inttypes.h>
+#include <string.h>
+#include <stdarg.h>
+#if __has_include(<uchar.h>)
+    #include <uchar.h>
+#else
+    typedef uint16_t char16_t;
+    typedef uint32_t char32_t;
+#endif
+#ifdef _WIN32
+    #define NOMINMAX
+    #define WIN32_LEAN_AND_MEAN
+    #include <windows.h>
+#else
+    #include <unistd.h>
+    #include <errno.h>
+    #include <pthread.h>
+#endif
+
+#ifdef _WIN32
+    #define EXPORT __declspec(dllexport)
+#else
+    #define EXPORT __attribute__((visibility("default")))
+#endif
+#if defined(_M_IX86) || defined(__i386__)
+    #ifdef _MSC_VER
+        #define FASTCALL __fastcall
+        #define STDCALL __stdcall
+    #else
+        #define FASTCALL __attribute__((fastcall))
+        #define STDCALL __attribute__((stdcall))
+    #endif
+#else
+    #define FASTCALL
+    #define STDCALL
+#endif
+
+typedef union DualU {
+    double d;
+    uint64_t u;
+} DualU;
+
+typedef union MultiU {
+    float f2[2];
+    uint64_t u;
+    uint8_t raw[8];
+} MultiU;
+
+EXPORT DualU MakeDualU(double d)
+{
+    DualU u;
+
+    u.d = d;
+
+    return u;
+}
+
+EXPORT void MakeDualUIndirect(double d, DualU *out)
+{
+    out->d = d;
+}
+
+EXPORT MultiU MakeMultiU(float a, float b)
+{
+    MultiU u;
+
+    u.f2[0] = a;
+    u.f2[1] = b;
+
+    return u;
+}
+
+EXPORT void MakeMultiUIndirect(float a, float b, MultiU *out)
+{
+    out->f2[0] = a;
+    out->f2[1] = b;
+}
---
+++ src/koffi/test/union.js
@@ -0,0 +1,97 @@
+#!/usr/bin/env node
+
+// Copyright 2023 Niels Martignène <niels.martignene@protonmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the “Software”), to deal in 
+// the Software without restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+// Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+
+const koffi = require('./build/koffi.node');
+const assert = require('assert');
+const util = require('util');
+
+const DualU = koffi.union('DualU', {
+    d: 'double',
+    u: 'uint64_t'
+});
+
+const MultiU = koffi.union('MultiU', {
+    f2: koffi.array('float', 2),
+    u: 'uint64_t',
+    raw: koffi.array('uint8_t', 8)
+});
+
+main();
+
+async function main() {
+    try {
+        await test();
+        console.log('Success!');
+    } catch (err) {
+        console.error(err);
+        process.exit(1);
+    }
+}
+
+async function test() {
+    let lib_filename = __dirname + '/build/union' + koffi.extension;
+    let lib = koffi.load(lib_filename);
+
+    const MakeDualU = lib.func('DualU MakeDualU(double d)');
+    const MakeDualUIndirect = lib.func('void MakeDualUIndirect(double d, _Out_ DualU *out)');
+    const MakeMultiU = lib.func('MultiU MakeMultiU(float a, float b)');
+    const MakeMultiUIndirect = lib.func('void MakeMultiUIndirect(float a, float b, _Out_ MultiU *out)');
+
+    // Make direct dual union
+    {
+        let u = MakeDualU(-2);
+
+        assert.equal(u.u, 13835058055282163712n);
+        assert.equal(u.d, -2);
+    }
+
+    // Make indirect dual union
+    {
+        let ptr = [null];
+        MakeDualUIndirect(42, ptr);
+        let u = ptr[0];
+
+        assert.equal(u.u, 4631107791820423168n);
+        assert.equal(u.d, 42);
+    }
+
+    // Make direct multi union
+    {
+        let u = MakeMultiU(12, 5);
+
+        assert.equal(u.u, 4656722015795806208n);
+        assert.deepEqual(u.f2, Float32Array.from([12, 5]));
+        assert.deepEqual(u.raw, Uint8Array.from([0, 0, 64, 65, 0, 0, 160, 64]));
+    }
+
+    // Make indirect multi union
+    {
+        let ptr = [null];
+        MakeMultiUIndirect(1, 3, ptr);
+        let u = ptr[0];
+
+        assert.equal(u.u, 4629700418002223104n);
+        assert.deepEqual(u.f2, Float32Array.from([1, 3]));
+        assert.deepEqual(u.raw, Uint8Array.from([0, 0, 128, 63, 0, 0, 64, 64]));
+    }
+}
