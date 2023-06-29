f4cd69e982f701405826710b7d838c4d5d094aea
Niels Martignène <niels.martignene@protonmail.com>
2023-06-29 08:39:20 +0200

    Fix date format on some distributions

--- src/meestic/dist/debian/package.sh
+++ src/meestic/dist/debian/package.sh
@@ -5,7 +5,7 @@ cd "$(dirname $0)/../../../.."
 ./bootstrap.sh
 
 VERSION=$(./felix --version | awk '/^felix/ {print $2}' | sed -e 's/_.*//')
-DATE=$(echo $VERSION | sed 's/\./ /' | LANG=C xargs -0 -n1 date "+%a, %d %b %Y %H:%M:%S %z" -d)
+DATE=$(echo $VERSION | sed 's/\./ /' | LANG=en_US xargs -0 -n1 date "+%a, %d %b %Y %H:%M:%S %z" -d)
 PACKAGE_DIR=bin/Packages/meestic/debian
 
 rm -rf $PACKAGE_DIR/pkg
