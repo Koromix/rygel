ccf57f63db1804771eccc254d07002e3c15fb9d2
Niels Martignène <niels.martignene@protonmail.com>
2023-07-19 15:33:27 +0200

    Update vendorized esbuild binaries

--- vendor/esbuild/update.sh
+++ vendor/esbuild/update.sh
@@ -2,30 +2,30 @@
 
 cd $(dirname $0)
 
-VERSION=0.14.10
+VERSION=0.18.14
 
-curl -O https://registry.npmjs.org/esbuild-windows-64/-/esbuild-windows-64-$VERSION.tgz
-tar zx --strip-components=1 -f esbuild-windows-64-$VERSION.tgz package/esbuild.exe
-mv esbuild.exe esbuild_win_x64.exe
-chmod -x esbuild_win_x64.exe
+curl -O https://registry.npmjs.org/@esbuild/win32-x64/-/win32-x64-$VERSION.tgz
+tar zx --strip-components=1 -f win32-x64-$VERSION.tgz package/esbuild.exe
+mv esbuild.exe esbuild_windows_x64.exe
+chmod -x esbuild_windows_x64.exe
 
-curl -O https://registry.npmjs.org/esbuild-windows-32/-/esbuild-windows-32-$VERSION.tgz
-tar zx --strip-components=1 -f esbuild-windows-32-$VERSION.tgz package/esbuild.exe
-mv esbuild.exe esbuild_win_ia32.exe
-chmod -x esbuild_win_ia32.exe
+curl -O https://registry.npmjs.org/@esbuild/win32-ia32/-/win32-ia32-$VERSION.tgz
+tar zx --strip-components=1 -f win32-ia32-$VERSION.tgz package/esbuild.exe
+mv esbuild.exe esbuild_windows_x86.exe
+chmod -x esbuild_windows_x86.exe
 
-curl -O https://registry.npmjs.org/esbuild-darwin-64/-/esbuild-darwin-64-$VERSION.tgz
-tar zx --strip-components=2 -f esbuild-darwin-64-$VERSION.tgz package/bin/esbuild
-mv esbuild esbuild_mac_x64
-chmod +x esbuild_mac_x64
+curl -O https://registry.npmjs.org/@esbuild/darwin-x64/-/darwin-x64-$VERSION.tgz
+tar zx --strip-components=2 -f darwin-x64-$VERSION.tgz package/bin/esbuild
+mv esbuild esbuild_macos_x64
+chmod +x esbuild_macos_x64
 
-curl -O https://registry.npmjs.org/esbuild-linux-64/-/esbuild-linux-64-$VERSION.tgz
-tar zx --strip-components=2 -f esbuild-linux-64-$VERSION.tgz package/bin/esbuild
+curl -O https://registry.npmjs.org/@esbuild/linux-x64/-/linux-x64-$VERSION.tgz
+tar zx --strip-components=2 -f linux-x64-$VERSION.tgz package/bin/esbuild
 mv esbuild esbuild_linux_x64
 chmod +x esbuild_linux_x64
 
-curl -O https://registry.npmjs.org/esbuild-linux-arm64/-/esbuild-linux-arm64-$VERSION.tgz
-tar zx --strip-components=2 -f esbuild-linux-arm64-$VERSION.tgz package/bin/esbuild
+curl -O https://registry.npmjs.org/@esbuild/linux-arm64/-/linux-arm64-$VERSION.tgz
+tar zx --strip-components=2 -f linux-arm64-$VERSION.tgz package/bin/esbuild
 mv esbuild esbuild_linux_arm64
 chmod +x esbuild_linux_arm64
 
