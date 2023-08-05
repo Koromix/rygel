66f2bdadc74dfb848b436b1007e8ccb742eb16fe
Niels Martignène <niels.martignene@protonmail.com>
2023-08-05 20:53:12 +0200

    Add script to package TyTools for macOS

---
+++ src/tytools/dist/macos/package.sh
@@ -0,0 +1,23 @@
+#!/bin/sh -e
+
+cd "$(dirname $0)/../../../.."
+
+./bootstrap.sh
+./felix -pFast TyCommander TyUploader tycmd
+
+VERSION=$(bin/Fast/TyCommander --version | awk -F'[ _]' '/^TyCommander/ {print $2}')
+PACKAGE_DIR=bin/Packages/tytools/macos
+DMG_FILENAME=$PACKAGE_DIR/TyTools_${VERSION}_osx.dmg
+
+rm -rf $PACKAGE_DIR/pkg
+mkdir -p $PACKAGE_DIR $PACKAGE_DIR/pkg
+
+install -m0755 bin/Fast/tycmd $PACKAGE_DIR/pkg/tycmd
+cp -rp bin/Fast/TyCommander.app $PACKAGE_DIR/pkg/TyCommander.app
+cp -rp bin/Fast/TyUploader.app $PACKAGE_DIR/pkg/TyUploader.app
+ln -s /Applications $PACKAGE_DIR/pkg/Applications
+install -m0644 src/tytools/README.md $PACKAGE_DIR/pkg/README.md
+install -m0644 src/tytools/LICENSE.txt $PACKAGE_DIR/pkg/LICENSE.txt
+
+rm -f $DMG_FILENAME
+hdiutil create -srcfolder "$PACKAGE_DIR/pkg" -volname "TyTools" $DMG_FILENAME
