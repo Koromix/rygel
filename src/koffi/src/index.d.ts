f08e36abd90bb06822c4bebb003c31f9010552bb
Niels Martignène <niels.martignene@protonmail.com>
2023-09-12 15:04:12 +0200

    Add missing Union primitive in TS definition

--- src/koffi/src/index.d.ts
+++ src/koffi/src/index.d.ts
@@ -28,7 +28,7 @@ declare module 'koffi' {
 
     type PrimitiveKind = 'Void' | 'Bool' | 'Int8' | 'UInt8' | 'Int16' | 'Int16S' | 'UInt16' | 'UInt16S' |
                          'Int32' | 'Int32S' | 'UInt32' | 'UInt32S' | 'Int64' | 'Int64S' | 'UInt64' | 'UInt64S' |
-                         'String' | 'String16' | 'Pointer' | 'Record' /* | 'Union' */ | 'Array' | 'Float32' | 'Float64' |
+                         'String' | 'String16' | 'Pointer' | 'Record' | 'Union' | 'Array' | 'Float32' | 'Float64' |
                          'Prototype' | 'Callback';
     type ArrayHint = 'Array' | 'Typed' | 'String';
 
