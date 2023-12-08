2c67958c3f146838a682151fc336ee1b07d9fc46
Niels Martignène <niels.martignene@protonmail.com>
2023-12-08 12:28:44 +0100

    Add Catatonia map to Napka

---
+++ src/napka/src/client/catatonia/catatonia.js
@@ -0,0 +1,169 @@
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see https://www.gnu.org/licenses/.
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see https://www.gnu.org/licenses/.
+
+import { render, html } from '../../../node_modules/lit/html.js';
+import { unsafeHTML } from '../../../node_modules/lit/directives/unsafe-html.js';
+import { Util, Log, Net } from '../../../../web/libjs/common.js';
+import { start, zoom, makeField, makeEdit, updateEntry, deleteEntry, renderMarkdown, isConnected } from '../map.js';
+
+const ICONS = {
+    dual: 'static/icons/dual.png',
+    center: 'static/icons/center.png',
+    ect: 'static/icons/ect.png'
+};
+
+function CatatoniaProvider() {
+    let entries;
+
+    let icons = {};
+
+    this.loadMap = async function() {
+        let [data, images] = await Promise.all([
+            Net.get('api/entries'),
+            Promise.all(Object.values(ICONS).map(url => Net.loadImage(url)))
+        ]);
+
+        entries = [
+            ...data.centres.rows.map(centre => ({ type: 'Centre', ...centre }))
+        ];
+
+        // We've preloaded images
+        {
+            let keys = Object.keys(ICONS);
+
+            for (let i = 0; i < keys.length; i++)
+                icons[keys[i]] = images[i];
+        }
+    };
+
+    this.renderFilters = function() {
+        return html`
+            <div id="filters">
+                <div id="count"></div>
+
+                <div class="group">
+                    <label>
+                        <input type="checkbox" data-filter="this.type === 'Centre' && !this.ect" checked /> Sans ECT
+                        <img src="static/icons/center.png" width="32" height="32" alt="" />
+                    </label>
+                    <label>
+                        <input type="checkbox" data-filter="this.type === 'Centre' && this.ect" checked /> Avec ECT
+                        <img src="static/icons/dual.png" width="32" height="32" alt="" />
+                    </label>
+                </div>
+            </div>
+        `;
+    };
+
+    this.fillMap = function(filters) {
+        let markers = [];
+        let total = 0;
+
+        for (let entry of entries) {
+            if (entry.address.latitude != null) {
+                total++;
+
+                if (filters.every(filtre => filtre(entry))) {
+                    let marker = {
+                        latitude: entry.address.latitude,
+                        longitude: entry.address.longitude,
+                        cluster: null,
+                        priority: 1 + entry.ect,
+                        icon: getEntryIcon(entry),
+                        size: 48,
+                        clickable: true
+                    };
+
+                    marker.entry = entry;
+
+                    markers.push(marker);
+                }
+            }
+        }
+
+        if (markers.length < total) {
+            let text = `${markers.length} / ${total}`;
+            render(text, document.querySelector('#count'));
+        } else if (markers.length) {
+            let text = `${markers.length} entrée${markers.length > 1 ? 's' : ''}`;
+            render(text, document.querySelector('#count'));
+        } else {
+            let text = 'Aucun résultat';
+            render(text, document.querySelector('#count'));
+        }
+
+        return markers;
+    };
+
+    function getEntryIcon(entry) {
+        return entry.ect ? icons.dual : icons.center;
+    }
+
+    this.renderEntry = function(entry, edit_key) {
+        let content = html`
+            <div>
+                ${entry.type == 'Centre' && entry.ect ? html`Centre <span class="tag" style="background: #18a059;">ECT</span><br/><br/>` : ''}
+
+                Adresse : <b>${field(entry, 'address')}</b><br/><br/>
+
+                <u>Pour prendre un rendez-vous :</u><br/>
+                ${entry.type == 'Centre' && entry.mail ?
+                    html`Adressez un courriel électronique à l'adresse <a href=${'mailto:' + entry.mail}>${entry.mail}</a><br/>` : ''}
+                ${entry.type == 'Centre' && !entry.mail ? html`Prenez rendez-vous par téléphone<br/>` : ''}
+                <br/>
+                ${entry.mail ? html`Courriel : <a href=${'mailto:' + entry.mail}>${entry.mail}</a><br/>` : ''}
+                ${entry.telephone ? html`Téléphone : <a href=${'tel:+33' + entry.telephone.substr(1)}>${entry.telephone}</a><br/>` : ''}
+            </div>
+        `;
+
+        return content;
+    };
+
+    function field(entry, key, view = null) {
+        if (key == 'address' && entry[key] != null) {
+            return makeField(entry, key, 'address');
+        } else {
+            return makeField(entry, key, fields[key], view);
+        }
+    }
+}
+
+export async function main() {
+    let provider = new CatatoniaProvider;
+
+    let latitude = (window.innerWidth > 1100) ? 47 :
+                   (window.innerWidth > 580) ? 47.65 : 48.4;
+    let longitude = 1.9;
+    let zoom = (window.innerWidth <= 580) ? 4 : 5;
+
+    await start(provider, {
+        latitude: latitude,
+        longitude: longitude,
+        zoom: zoom
+    });
+}
+
+export { zoom }
---
+++ src/napka/src/importers/catatonia.js
@@ -0,0 +1,164 @@
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see https://www.gnu.org/licenses/.
+
+import crypto from 'crypto';
+import fs from 'fs';
+import fetch from 'node-fetch';
+import xlsx from 'node-xlsx';
+import sqlite3 from 'better-sqlite3';
+import * as database from '../lib/database.js';
+import { Util } from '../../../web/libjs/common.js';
+import * as parse from '../lib/parse.js';
+import * as imp from '../lib/import.js';
+
+const CATATONIA_XLSX_URL = process.env.CATATONIA_XLSX_URL || '';
+const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY || '';
+
+const LIST_KEYS = ["Identites", "Mails"];
+
+const CENTER_MAPPINGS = {};
+const PSY_MAPPINGS = {
+    orientation: 'markdown'
+};
+
+main();
+
+async function main() {
+    try {
+        await run();
+        console.log('Success!');
+    } catch (err) {
+        console.error(err);
+        process.exit(1);
+    }
+}
+
+async function run() {
+    // Check prerequisites
+    {
+        let errors = [];
+
+        if (!CATATONIA_XLSX_URL)
+            errors.push('Missing CATATONIA_XLSX_URL (public link to download XLSX file)');
+        if (!GOOGLE_API_KEY)
+            errors.push('Missing GOOGLE_API_KEY');
+
+        if (errors.length)
+            throw new Error(errors.join('\n'));
+    }
+
+    let db = database.open();
+
+    // Create map and layers if needed
+    db.transaction(() => {
+        let map_id = db.prepare(`INSERT INTO maps (name, title, mail)
+                                     VALUES ('catatonia', 'Catatonia', 'ali.amad@chu-lille.fr')
+                                     ON CONFLICT DO UPDATE SET title = excluded.title,
+                                                               mail = excluded.mail
+                                     RETURNING id`).pluck().get();
+
+        let stmt = db.prepare(`INSERT INTO layers (map_id, name, title, fields) VALUES (?, ?, ?, ?)
+                                   ON CONFLICT DO UPDATE SET title = excluded.title,
+                                                             fields = excluded.fields`);
+
+        stmt.run(map_id, 'centres', 'Centres', JSON.stringify(CENTER_MAPPINGS));
+        stmt.run(map_id, 'psychologues', 'Psychologues', JSON.stringify(PSY_MAPPINGS));
+    })();
+
+    // Load online spreadsheet file
+    let wb;
+    {
+        let response = await fetch(CATATONIA_XLSX_URL);
+
+        if (!response.ok) {
+            let text = (await response.text()).trim();
+            throw new Error(text);
+        }
+
+        let blob = await response.blob();
+        let buffer = Buffer.from(await blob.arrayBuffer());
+
+        wb = xlsx.parse(buffer);
+    }
+
+    // Transform rows to objects
+    let tables = {};
+    for (let ws of wb) {
+        let keys = ws.data[0].map(key => key.trim());
+
+        let table = [];
+
+        for (let i = 1; i < ws.data.length; i++) {
+            let row = {};
+
+            for (let j = 0; j < keys.length; j++) {
+                let value = ws.data[i][j];
+
+                if (value == null) {
+                    row[keys[j]] = null;
+                } else if (typeof value === 'string') {
+                    value = value.trim();
+
+                    if (LIST_KEYS.includes(keys[j])) {
+                        value = value.split('\n');
+                        value = value.map(it => it.trim()).filter(it => it);
+                    } else if (!value) {
+                        value = null;
+                    }
+
+                    row[keys[j]] = value;
+                } else {
+                    row[keys[j]] = value;
+                }
+            }
+
+            if (row.ID == null || row.Structure == null)
+                break;
+
+            if (row.Adresse != null)
+                table.push(row);
+        }
+
+        tables[ws.name] = table;
+    }
+
+    let centres = tables['Centres'].map(transformCenter);
+
+    db.transaction(() => {
+        imp.updateEntries(db, 'catatonia', 'centres', centres);
+    })();
+    await imp.geomapMissing(db, 'catatonia', GOOGLE_API_KEY);
+
+    db.close();
+}
+
+function transformCenter(row) {
+    let entry = {
+        import: '' + row.ID,
+        version: null,
+        hide: 0,
+
+        name: row.Structure,
+        address: row.Adresse,
+
+        ect: String(row.ECT).trim().toUpperCase() == "OUI",
+
+        mail: parse.cleanMail(row.Mail),
+        telephone: parse.cleanPhoneNumber(row.Telephone),
+        referents: row.Referents.split(/[,;/\n]/).map(it => it.trim()).filter(it => it)
+    };
+
+    entry.version = crypto.createHash('sha256').update(JSON.stringify(entry)).digest('hex');
+
+    return entry;
+}
