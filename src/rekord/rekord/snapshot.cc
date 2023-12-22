c008a639209c141ab1a0c71efe53aff325519754
Niels Martignène <niels.martignene@protonmail.com>
2023-12-22 12:50:48 +0100

    Move snapshot commands to dedicated file

---
+++ src/rekord/rekord/snapshot.cc
@@ -0,0 +1,260 @@
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+#include "src/core/libcc/libcc.hh"
+#include "rekord.hh"
+
+namespace RG {
+
+int RunPut(Span<const char *> arguments)
+{
+    BlockAllocator temp_alloc;
+
+    // Options
+    rk_Config config;
+    rk_PutSettings settings;
+    bool allow_anonymous = false;
+    HeapArray<const char *> filenames;
+
+    const auto print_usage = [=](FILE *fp) {
+        PrintLn(fp,
+R"(Usage: %!..+%1 put [-R <repo>] <filename> ...%!0
+
+Options:
+    %!..+-C, --config_file <file>%!0     Set configuration file
+
+    %!..+-R, --repository <dir>%!0       Set repository directory
+    %!..+-u, --user <user>%!0            Set repository username
+        %!..+--password <pwd>%!0         Set repository password
+
+    %!..+-n, --name <name>%!0            Set user friendly name
+        %!..+--anonymous%!0              Allow snapshot without name
+        %!..+--raw%!0                    Skip snapshot object and report data ID
+
+        %!..+--follow_symlinks%!0        Follow symbolic links (instead of storing them as-is
+
+    %!..+-j, --threads <threads>%!0      Change number of threads
+                                 %!D..(default: automatic)%!0)", FelixTarget);
+    };
+
+    if (!FindAndLoadConfig(arguments, &config))
+        return 1;
+
+    // Parse arguments
+    {
+        OptionParser opt(arguments);
+
+        while (opt.Next()) {
+            if (opt.Test("--help")) {
+                print_usage(stdout);
+                return 0;
+            } else if (opt.Test("-C", "--config_file", OptionType::Value)) {
+                // Already handled
+            } else if (opt.Test("-R", "--repository", OptionType::Value)) {
+                if (!rk_DecodeURL(opt.current_value, &config))
+                    return 1;
+            } else if (opt.Test("-u", "--username", OptionType::Value)) {
+                config.username = opt.current_value;
+            } else if (opt.Test("--password", OptionType::Value)) {
+                config.password = opt.current_value;
+            } else if (opt.Test("-n", "--name", OptionType::Value)) {
+                settings.name = opt.current_value;
+            } else if (opt.Test("--follow_symlinks")) {
+                settings.follow_symlinks = true;
+            } else if (opt.Test("--anonymous")) {
+                allow_anonymous = true;
+            } else if (opt.Test("--raw")) {
+                settings.raw = true;
+            } else if (opt.Test("-j", "--threads", OptionType::Value)) {
+                if (!ParseInt(opt.current_value, &config.threads))
+                    return 1;
+                if (config.threads < 1) {
+                    LogError("Threads count cannot be < 1");
+                    return 1;
+                }
+            } else {
+                opt.LogUnknownError();
+                return 1;
+            }
+        }
+
+        opt.ConsumeNonOptions(&filenames);
+    }
+
+    if (!filenames.len) {
+        LogError("No filename provided");
+        return 1;
+    }
+
+    if (!settings.name && !allow_anonymous && !settings.raw) {
+        LogError("Use --anonymous to create unnamed snapshot object");
+        return 1;
+    }
+
+    if (!config.Complete(true))
+        return 1;
+
+    std::unique_ptr<rk_Disk> disk = rk_Open(config, true);
+    if (!disk)
+        return 1;
+
+    LogInfo("Repository: %!..+%1%!0 (%2)", disk->GetURL(), rk_DiskModeNames[(int)disk->GetMode()]);
+    if (disk->GetMode() != rk_DiskMode::WriteOnly) {
+        LogWarning("You should use the write-only key with this command");
+    }
+    LogInfo();
+
+    LogInfo("Backing up...");
+
+    int64_t now = GetMonotonicTime();
+
+    rk_ID id = {};
+    int64_t total_len = 0;
+    int64_t total_written = 0;
+    if (!rk_Put(disk.get(), settings, filenames, &id, &total_len, &total_written))
+        return 1;
+
+    double time = (double)(GetMonotonicTime() - now) / 1000.0;
+
+    LogInfo();
+    LogInfo("%1 ID: %!..+%2%!0", settings.raw ? "Data" : "Snapshot", id);
+    LogInfo("Stored size: %!..+%1%!0", FmtDiskSize(total_len));
+    LogInfo("Total written: %!..+%1%!0", FmtDiskSize(total_written));
+    LogInfo("Execution time: %!..+%1s%!0", FmtDouble(time, 1));
+
+    return 0;
+}
+
+int RunGet(Span<const char *> arguments)
+{
+    BlockAllocator temp_alloc;
+
+    // Options
+    rk_Config config;
+    rk_GetSettings settings;
+    const char *dest_filename = nullptr;
+    const char *name = nullptr;
+
+    const auto print_usage = [=](FILE *fp) {
+        PrintLn(fp,
+R"(Usage: %!..+%1 get [-R <repo>] <ID> -O <path>%!0
+
+Options:
+    %!..+-C, --config_file <file>%!0     Set configuration file
+
+    %!..+-R, --repository <dir>%!0       Set repository directory
+    %!..+-u, --user <user>%!0            Set repository username
+        %!..+--password <pwd>%!0         Set repository password
+
+    %!..+-O, --output <path>%!0          Restore file or directory to path
+
+    %!..+-f, --force%!0                  Overwrite destination if not empty
+
+        %!..+--flat%!0                   Use flat names for snapshot files
+        %!..+--chown%!0                  Restore original file UID and GID
+
+    %!..+-j, --threads <threads>%!0      Change number of threads
+                                 %!D..(default: automatic)%!0)", FelixTarget);
+    };
+
+    if (!FindAndLoadConfig(arguments, &config))
+        return 1;
+
+    // Parse arguments
+    {
+        OptionParser opt(arguments);
+
+        while (opt.Next()) {
+            if (opt.Test("--help")) {
+                print_usage(stdout);
+                return 0;
+            } else if (opt.Test("-C", "--config_file", OptionType::Value)) {
+                // Already handled
+            } else if (opt.Test("-R", "--repository", OptionType::Value)) {
+                if (!rk_DecodeURL(opt.current_value, &config))
+                    return 1;
+            } else if (opt.Test("-u", "--username", OptionType::Value)) {
+                config.username = opt.current_value;
+            } else if (opt.Test("--password", OptionType::Value)) {
+                config.password = opt.current_value;
+            } else if (opt.Test("-O", "--output", OptionType::Value)) {
+                dest_filename = opt.current_value;
+            } else if (opt.Test("-f", "--force")) {
+                settings.force = true;
+            } else if (opt.Test("--flat")) {
+                settings.flat = true;
+            } else if (opt.Test("--chown")) {
+                settings.chown = true;
+            } else if (opt.Test("-j", "--threads", OptionType::Value)) {
+                if (!ParseInt(opt.current_value, &config.threads))
+                    return 1;
+                if (config.threads < 1) {
+                    LogError("Threads count cannot be < 1");
+                    return 1;
+                }
+            } else {
+                opt.LogUnknownError();
+                return 1;
+            }
+        }
+
+        name = opt.ConsumeNonOption();
+    }
+
+    if (!name) {
+        LogError("No ID provided");
+        return 1;
+    }
+    if (!dest_filename) {
+        LogError("Missing destination filename");
+        return 1;
+    }
+
+    if (!config.Complete(true))
+        return 1;
+
+    std::unique_ptr<rk_Disk> disk = rk_Open(config, true);
+    if (!disk)
+        return 1;
+
+    LogInfo("Repository: %!..+%1%!0 (%2)", disk->GetURL(), rk_DiskModeNames[(int)disk->GetMode()]);
+    if (disk->GetMode() != rk_DiskMode::ReadWrite) {
+        LogError("Cannot decrypt with write-only key");
+        return 1;
+    }
+    LogInfo();
+
+    LogInfo("Extracting...");
+
+    int64_t now = GetMonotonicTime();
+
+    int64_t file_len = 0;
+    {
+        rk_ID id = {};
+        if (!rk_ParseID(name, &id))
+            return 1;
+        if (!rk_Get(disk.get(), id, settings, dest_filename, &file_len))
+            return 1;
+    }
+
+    double time = (double)(GetMonotonicTime() - now) / 1000.0;
+
+    LogInfo();
+    LogInfo("Restored: %!..+%1%!0 (%2)", dest_filename, FmtDiskSize(file_len));
+    LogInfo("Execution time: %!..+%1s%!0", FmtDouble(time, 1));
+
+    return 0;
+}
+
+}
+
