2a22965f46fb1cb197f922dba808c4a8c84f044b
Niels Martignène <niels.martignene@protonmail.com>
2024-01-08 20:02:54 +0100

    Fix blake3 build on ARM64

--- deploy/docker/debian11/customize.sh
+++ deploy/docker/debian11/customize.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -e
 
 apt update
-apt install -y build-essential git cmake ninja-build pkg-config gdb debhelper dh-make devscripts
+apt install -y build-essential git cmake ninja-build pkg-config gdb debhelper dh-make devscripts \
+               gcc-i686-linux-gnu g++-i686-linux-gnu binutils-i686-linux-gnu \
+               gcc-aarch64-linux-gnu g++-aarch64-linux-gnu binutils-aarch64-linux-gnu \
+               gcc-riscv64-linux-gnu g++-riscv64-linux-gnu binutils-riscv64-linux-gnu
 
 curl https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add -
 echo "deb http://apt.llvm.org/bullseye/ llvm-toolchain-bullseye-15 main" >> /etc/apt/sources.list
--- deploy/docker/debian12/customize.sh
+++ deploy/docker/debian12/customize.sh
@@ -1,4 +1,7 @@
 #!/bin/bash -e
 
 apt update
-apt install -y build-essential git cmake ninja-build pkg-config gdb debhelper dh-make devscripts clang lld
+apt install -y build-essential git cmake ninja-build pkg-config gdb debhelper dh-make devscripts clang lld \
+               gcc-i686-linux-gnu g++-i686-linux-gnu binutils-i686-linux-gnu \
+               gcc-aarch64-linux-gnu g++-aarch64-linux-gnu binutils-aarch64-linux-gnu \
+               gcc-riscv64-linux-gnu g++-riscv64-linux-gnu binutils-riscv64-linux-gnu
