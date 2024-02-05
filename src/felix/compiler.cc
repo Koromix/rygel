6f0e8fed782e6d8aeb76169ce11be703603c05b6
Niels Martignène <niels.martignene@protonmail.com>
2024-02-05 11:08:44 +0100

    Use /Zc:preprocessor with MSVC

--- src/felix/compiler.cc
+++ src/felix/compiler.cc
@@ -1403,7 +1403,7 @@ public:
         } else {
             Fmt(&buf, " /Yc \"/Fp%1.pch\" \"/Fo%1.obj\"", src_filename);
         }
-        Fmt(&buf, " /showIncludes");
+        Fmt(&buf, " /Zc:preprocessor /showIncludes");
         out_cmd->rsp_offset = buf.len;
 
         // Build options
