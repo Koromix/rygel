a2f935e0d72ce0228b224cd5908afced11c5fcaa
Niels Martignène <niels.martignene@protonmail.com>
2024-04-15 14:05:17 +0200

    Provide proper separate Emscripten config for libssh

--- vendor/libssh/config.h
+++ vendor/libssh/config.h
@@ -79,6 +79,49 @@
     #define WITH_ZLIB 1
     #define WITH_SFTP 1
     #define WITH_GEX 1
+#elif defined(__EMSCRIPTEN__)
+    #define GLOBAL_BIND_CONFIG "/etc/ssh/libssh_server_config"
+    #define GLOBAL_CLIENT_CONFIG "/etc/ssh/ssh_config"
+
+    #define HAVE_ARPA_INET_H 1
+    #define HAVE_GLOB_H 1
+    #define HAVE_PTY_H 1
+    #define HAVE_UTMP_H 1
+    #define HAVE_SYS_TIME_H 1
+    #define HAVE_TERMIOS_H 1
+    #define HAVE_UNISTD_H 1
+    #define HAVE_STDINT_H 1
+    #define HAVE_PTHREAD_H 1
+
+    #define HAVE_SNPRINTF 1
+    #define HAVE_VSNPRINTF 1
+    #define HAVE_ISBLANK 1
+    #define HAVE_STRNCPY 1
+    #define HAVE_STRNDUP 1
+    #define HAVE_CFMAKERAW 1
+    #define HAVE_GETADDRINFO 1
+    #define HAVE_POLL 1
+    #define HAVE_SELECT 1
+    #define HAVE_CLOCK_GETTIME 1
+    #define HAVE_STRTOULL 1
+    #define HAVE_GLOB 1
+    #define HAVE_EXPLICIT_BZERO 1
+
+    // #define HAVE_PTHREAD 1
+
+    #define HAVE_GCC_THREAD_LOCAL_STORAGE 1
+    #define HAVE_FALLTHROUGH_ATTRIBUTE 1
+    #define HAVE_UNUSED_ATTRIBUTE 1
+    #define HAVE_WEAK_ATTRIBUTE 1
+    #define HAVE_CONSTRUCTOR_ATTRIBUTE 1
+    #define HAVE_DESTRUCTOR_ATTRIBUTE 1
+    #define HAVE_GCC_VOLATILE_MEMORY_PROTECTION 1
+    #define HAVE_COMPILER__FUNC__ 1
+    #define HAVE_COMPILER__FUNCTION__ 1
+
+    #define WITH_ZLIB 1
+    #define WITH_SFTP 1
+    #define WITH_GEX 1
 #else
     #define GLOBAL_BIND_CONFIG "/etc/ssh/libssh_server_config"
     #define GLOBAL_CLIENT_CONFIG "/etc/ssh/ssh_config"
@@ -91,9 +134,7 @@
     #define HAVE_STDINT_H 1
     #define HAVE_PTHREAD_H 1
     #define HAVE_ECC 1
-    #ifndef __EMSCRIPTEN__
-        #define HAVE_GLOB_GL_FLAGS_MEMBER 1
-    #endif
+    #define HAVE_GLOB_GL_FLAGS_MEMBER 1
 
     #define HAVE_SNPRINTF 1
     #define HAVE_VSNPRINTF 1
