14ab3976ed46afb457932c4c046ca2ac72f15670
Niels Martignène <niels.martignene@protonmail.com>
2024-06-24 10:25:28 +0200

    Skip need for vcvars64 to bootstrap felix with clang-cl

--- src/felix/bootstrap.bat
+++ src/felix/bootstrap.bat
@@ -15,25 +15,25 @@ if EXIST %BINARY% (
     if EXIST %BINARY% del %BINARY%
 )
 
+where /q clang-cl && where /q lld-link
+if NOT ERRORLEVEL 1 (
+    echo Bootstrapping felix with Clang...
+    mkdir %TEMP%
+    clang-cl /nologo /std:c++20 /I../.. /W0 /EHsc /MP /DNDEBUG /DNOMINMAX /DWINVER=0x0601 /D_WIN32_WINNT=0x0601 /DUNICODE /D_UNICODE /c %SRC% /Fo%TEMP%\
+    lld-link /nologo %TEMP%\*.obj ws2_32.lib advapi32.lib shell32.lib ole32.lib /out:%TEMP%\felix.exe
+    %TEMP%\felix.exe -pFast felix %*
+    copy %BUILD%\felix.exe %BINARY% >NUL
+
+    echo Cleaning up...
+    timeout /T 2 /NOBREAK >NUL
+    del /Q %TEMP%\*
+    rmdir /Q %TEMP%
+
+    exit /B
+)
+
 where /q cl
 if NOT ERRORLEVEL 1 (
-    where /q clang-cl
-    if NOT ERRORLEVEL 1 (
-        echo Bootstrapping felix with Clang...
-        mkdir %TEMP%
-        clang-cl /nologo /std:c++20 /I../.. /W0 /EHsc /MP /DNDEBUG /DNOMINMAX /DWINVER=0x0601 /D_WIN32_WINNT=0x0601 /DUNICODE /D_UNICODE /c %SRC% /Fo%TEMP%\
-        lld-link /nologo %TEMP%\*.obj ws2_32.lib advapi32.lib shell32.lib ole32.lib /out:%TEMP%\felix.exe
-        %TEMP%\felix.exe -pFast felix %*
-        copy %BUILD%\felix.exe %BINARY% >NUL
-
-        echo Cleaning up...
-        timeout /T 2 /NOBREAK >NUL
-        del /Q %TEMP%\*
-        rmdir /Q %TEMP%
-
-        exit /B
-    )
-
     echo Bootstrapping felix with MSVC...
     mkdir %TEMP%
     cl /nologo /std:c++20 /Zc:twoPhase- /I../.. /W0 /EHsc /MP /DNDEBUG /DNOMINMAX /DWINVER=0x0601 /D_WIN32_WINNT=0x0601 /DUNICODE /D_UNICODE /c %SRC% /Fo%TEMP%\
@@ -65,6 +65,6 @@ if NOT ERRORLEVEL 1 (
     exit /B
 )
 
-echo Could not find any compiler (cl/link, clang-cl/link, g++) in PATH
-echo To build with Clang or MSVC, you need to use the Visual Studio command prompt
+echo Could not find any compiler (clang-cl/lld-link, cl/link, g++) in PATH
+echo To build with MSVC, you need to use the Visual Studio command prompt
 exit /B 1
