c737542f602ae7bea99cf2136d0e625bd24233c3
Niels Martignène <niels.martignene@protonmail.com>
2024-08-27 11:25:20 +0200

    Build goupile debian packages with Clang/LLD 18

--- src/goupile/dist/linux/debian.sh
+++ src/goupile/dist/linux/debian.sh
@@ -15,7 +15,7 @@ DOCKER_IMAGE=debian11
 
 build() {
     ./bootstrap.sh
-    ./felix -pParanoid --host=$1:clang-17:lld-17 goupile
+    ./felix -pParanoid --host=$1:clang-18:lld-18 goupile
 
     install -D -m0755 bin/Paranoid/goupile ${ROOT_DIR}/bin/goupile
 
