e7611d313a9fe3a11bd812343d275162db90c41c
Niels Martignène <niels.martignene@protonmail.com>
2024-09-06 15:06:34 +0200

    Add missing TS definition for `koffi.alloc()`
    Fixes Koromix/koffi#189

--- src/koffi/index.d.ts
+++ src/koffi/index.d.ts
@@ -152,6 +152,7 @@ declare module 'koffi' {
     export function config(cfg: Record<string, unknown>): Record<string, unknown>;
     export function stats(): Record<string, unknown>;
 
+    export function alloc(type: TypeSpec, length: number): any;
     export function free(value: any): void;
 
     export function errno(): number;
