9853d2fae089e75d0b91b90d7938fd777e722b05
Niels Martignène <niels.martignene@protonmail.com>
2024-09-10 15:35:46 +0200

    Fix broken demheter.fr build

--- web/demheter.fr/admin/static/admin.js
+++ web/demheter.fr/admin/static/admin.js
@@ -1,6 +1,6 @@
 import { render, html } from '../../../../vendor/lit-html/lit-html.bundle.js';
 import { Util, Net, Log } from '../../../../src/web/libjs/common.js';
-import { Base64 } from '../../../../src/web/libjs/crypto.js';
+import { Base64 } from '../../../../src/web/libjs/mixer.js';
 import { UI } from './ui.js';
 
 import './admin.css';
