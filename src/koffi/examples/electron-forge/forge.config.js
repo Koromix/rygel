ad2a43d25b539f4c1a53e345e29fa9294fb86c93
Niels Martignène <niels.martignene@protonmail.com>
2025-02-13 10:59:13 +0100

    Update electron-forge example

--- src/koffi/examples/electron-forge/forge.config.js
+++ src/koffi/examples/electron-forge/forge.config.js
@@ -1,3 +1,6 @@
+const { FusesPlugin } = require('@electron-forge/plugin-fuses');
+const { FuseV1Options, FuseVersion } = require('@electron/fuses');
+
 module.exports = {
   packagerConfig: {
     asar: true,
@@ -10,8 +13,16 @@ module.exports = {
     },
     {
       name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+    },
+    {
+      name: '@electron-forge/maker-deb',
       config: {},
-    }
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {},
+    },
   ],
   plugins: [
     {
@@ -35,9 +46,18 @@ module.exports = {
             },
           ],
         },
-        "port": 3001,
-        "loggerPort": 9001
       },
     },
+    // Fuses are used to enable/disable various Electron functionality
+    // at package time, before code signing the application
+    new FusesPlugin({
+      version: FuseVersion.V1,
+      [FuseV1Options.RunAsNode]: false,
+      [FuseV1Options.EnableCookieEncryption]: true,
+      [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
+      [FuseV1Options.EnableNodeCliInspectArguments]: false,
+      [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
+      [FuseV1Options.OnlyLoadAppFromAsar]: true,
+    }),
   ],
 };
