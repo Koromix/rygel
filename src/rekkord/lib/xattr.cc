4a72e9792962fa06f12e354e263fa87f891de148
Niels Martignène <niels.martignene@protonmail.com>
2025-05-05 15:14:43 +0200

    Process ACLs separately from other xattrs

--- src/rekkord/lib/xattr.cc
+++ src/rekkord/lib/xattr.cc
@@ -17,8 +17,10 @@
 #include "xattr.hh"
 
 #if defined(__linux__)
+    #include <acl/libacl.h>
     #include <sys/xattr.h>
 #elif defined(__FreeBSD__)
+    #include <sys/acl.h>
     #include <sys/types.h>
     #include <sys/extattr.h>
 #endif
@@ -27,7 +29,50 @@ namespace RG {
 
 #if defined(__linux__)
 
-static Size ReadValue(int fd, const char *filename, const char *key, HeapArray<uint8_t> *out_value)
+static Span<const char> ReadACLs(int fd, const char *filename, Allocator *alloc)
+{
+    acl_t acl = acl_get_fd(fd);
+    if (!acl) {
+        LogError("Failed to open ACL entries for '%1': %2", filename, strerror(errno));
+        return {};
+    }
+    RG_DEFER { acl_free(acl); };
+
+    if (!acl_equiv_mode(acl, nullptr))
+        return {};
+
+    char *str = acl_to_any_text(acl, nullptr, '\n', TEXT_NUMERIC_IDS);
+    if (!str) {
+        LogError("Failed to read ACL entries for '%1': %2", filename, strerror(errno));
+        return {};
+    }
+    RG_DEFER { acl_free(str); };
+
+    Span<const char> copy = DuplicateString(str, alloc).ptr;
+    return copy;
+}
+
+static bool WriteACLs(int fd, const char *filename, Span<const char> str)
+{
+    char buf[32768];
+    CopyString(str, buf);
+
+    acl_t acl = acl_from_text(buf);
+    if (!acl) {
+        LogError("Failed to decode ACL string for '%1': %2", filename, strerror(errno));
+        return false;
+    }
+    RG_DEFER { acl_free(acl); };
+
+    if (acl_set_fd(fd, acl) < 0) {
+        LogError("Failed to set ACL for '%1': %2", filename, strerror(errno));
+        return false;
+    }
+
+    return true;
+}
+
+static Size ReadAttribute(int fd, const char *filename, const char *key, HeapArray<uint8_t> *out_value)
 {
     Size size = fgetxattr(fd, key, nullptr, 0);
 
@@ -63,6 +108,13 @@ bool ReadXAttributes(int fd, const char *filename, Allocator *alloc, HeapArray<X
 {
     RG_DEFER_NC(err_guard, len = out_xattrs->len) { out_xattrs->RemoveFrom(len); };
 
+    Span<const char> acls = ReadACLs(fd, filename, alloc);
+
+    if (acls.len) {
+        XAttrInfo xattr = { "rekkord.acl1", acls.As<uint8_t>() };
+        out_xattrs->Append(xattr);
+    }
+
     HeapArray<char> list(alloc);
     {
         Size size = Kibibytes(4);
@@ -94,8 +146,12 @@ retry:
         // Prepare next iteration
         offset += key.len + 1;
 
+        // Skip ACL xattrs
+        if (StartsWith(key, "system."))
+            continue;
+
         HeapArray<uint8_t> value(alloc);
-        Size len = ReadValue(fd, filename, key.ptr, &value);
+        Size len = ReadAttribute(fd, filename, key.ptr, &value);
 
         if (len < 0)
             continue;
@@ -117,6 +173,11 @@ bool WriteXAttributes(int fd, const char *filename, Span<const XAttrInfo> xattrs
     bool success = true;
 
     for (const XAttrInfo &xattr: xattrs) {
+        if (TestStr(xattr.key, "rekkord.acl1")) {
+            success &= WriteACLs(fd, filename, xattr.value.As<char>());
+            continue;
+        }
+
         int ret = fsetxattr(fd, xattr.key, xattr.value.ptr, xattr.value.len, 0);
 
         if (ret < 0) {
@@ -130,7 +191,56 @@ bool WriteXAttributes(int fd, const char *filename, Span<const XAttrInfo> xattrs
 
 #elif defined(__FreeBSD__)
 
-static Size ReadValue(int fd, const char *filename, const char *key, HeapArray<uint8_t> *out_value)
+static Span<const char> ReadACLs(int fd, const char *filename, Allocator *alloc)
+{
+    acl_t acl = acl_get_fd(fd);
+    if (!acl) {
+        LogError("Failed to open ACL entries for '%1': %2", filename, strerror(errno));
+        return {};
+    }
+    RG_DEFER { acl_free(acl); };
+
+    // Ignore trivial ACLs
+    {
+        int trivial = 0;
+        acl_is_trivial_np(acl, &trivial);
+
+        if (trivial)
+            return {};
+    }
+
+    char *str = acl_to_text_np(acl, nullptr, ACL_TEXT_NUMERIC_IDS);
+    if (!str) {
+        LogError("Failed to read ACL entries for '%1': %2", filename, strerror(errno));
+        return {};
+    }
+    RG_DEFER { acl_free(str); };
+
+    Span<const char> copy = DuplicateString(str, alloc).ptr;
+    return copy;
+}
+
+static bool WriteACLs(int fd, const char *filename, Span<const char> str)
+{
+    char buf[32768];
+    CopyString(str, buf);
+
+    acl_t acl = acl_from_text(buf);
+    if (!acl) {
+        LogError("Failed to decode ACL string for '%1': %2", filename, strerror(errno));
+        return false;
+    }
+    RG_DEFER { acl_free(acl); };
+
+    if (acl_set_fd(fd, acl) < 0) {
+        LogError("Failed to set ACL for '%1': %2", filename, strerror(errno));
+        return false;
+    }
+
+    return true;
+}
+
+static Size ReadAttribute(int fd, const char *filename, const char *key, HeapArray<uint8_t> *out_value)
 {
     Size size = extattr_get_fd(fd, EXTATTR_NAMESPACE_USER, key, nullptr, 0);
 
@@ -166,6 +276,13 @@ bool ReadXAttributes(int fd, const char *filename, Allocator *alloc, HeapArray<X
 {
     RG_DEFER_NC(err_guard, len = out_xattrs->len) { out_xattrs->RemoveFrom(len); };
 
+    Span<const char> acls = ReadACLs(fd, filename, alloc);
+
+    if (acls.len) {
+        XAttrInfo xattr = { "rekkord.acl1", acls.As<uint8_t>() };
+        out_xattrs->Append(xattr);
+    }
+
     HeapArray<char> list;
     {
         Size size = Kibibytes(4);
@@ -199,7 +316,7 @@ retry:
         key = Fmt(alloc, "user.%1", key);
 
         HeapArray<uint8_t> value(alloc);
-        Size len = ReadValue(fd, filename, key.ptr + 5, &value);
+        Size len = ReadAttribute(fd, filename, key.ptr + 5, &value);
 
         if (len < 0)
             continue;
@@ -219,6 +336,11 @@ bool WriteXAttributes(int fd, const char *filename, Span<const XAttrInfo> xattrs
     bool success = true;
 
     for (const XAttrInfo &xattr: xattrs) {
+        if (TestStr(xattr.key, "rekkord.acl1")) {
+            success &= WriteACLs(fd, filename, xattr.value.As<char>());
+            continue;
+        }
+
         if (!StartsWith(xattr.key, "user.")) {
             LogError("Cannot restore extended attribute '%1' for '%2': unsupported prefix", xattr.key, filename);
             success = false;
