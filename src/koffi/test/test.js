ba5ec918dd0818fc2ff389842beacd2b03134574
Niels Martignène <niels.martignene@protonmail.com>
2025-08-09 10:36:13 +0200

    Use tsc on test scripts to check Koffi TypeScript declaration file

--- src/koffi/test/test.js
+++ src/koffi/test/test.js
@@ -27,6 +27,8 @@ const path = require('path');
 const tty = require('tty');
 const { style_ansi } = require('../../../tools/qemu/qemu.js');
 
+const TSC_OPTIONS = '--target es2020 --module node16 --allowJs --checkJs --noEmit --resolveJsonModule'.split(' ');
+
 main();
 
 function main() {
@@ -40,41 +42,56 @@ function main() {
 }
 
 function test() {
+    let scripts = {};
     let success = true;
 
-    success &= run('Sync', 'sync.js');
-    success &= run('Async', 'async.js');
-    success &= run('Callbacks', 'callbacks.js');
-    success &= run('Union', 'union.js');
+    scripts.Sync = 'sync.js';
+    scripts.Async = 'async.js';
+    scripts.Callbacks = 'callbacks.js';
+    scripts.Union = 'union.js';
     if (process.platform != 'win32' && process.platform != 'darwin')
-        success &= run('POSIX', 'posix.js');
+        scripts.POSIX = 'posix.js';
     if (process.platform == 'win32')
-        success &= run('Win32', 'win32.js');
+        scripts.Win32 = 'win32.js';
     if (process.platform != 'darwin')
-        success &= run('Raylib', 'raylib.js');
-    success &= run('SQLite', 'sqlite.js');
+        scripts.Raylib = 'raylib.js';
+    scripts.SQLite = 'sqlite.js';
+
+    for (let key in scripts) {
+        let filename = path.join(__dirname, scripts[key]);
+        success &= run('Test', key, [filename]);
+    }
+
+    for (let key in scripts) {
+        let filename = path.join(__dirname, scripts[key]);
+        let args = ['../../../vendor/typescript/bin/tsc', ...TSC_OPTIONS, filename];
+
+        success &= run('TypeScript', key, args);
+    }
 
     return success;
 }
 
-function run(title, name) {
+function run(action, title, args) {
     let start = process.hrtime.bigint();
-
-    let filename = path.join(__dirname, name);
-    let proc = spawnSync(process.execPath, [filename]);
+    let proc = spawnSync(process.execPath, args);
 
     try {
         if (proc.status == null)
             throw new Error(proc.error);
-        if (proc.status !== 0)
-            throw new Error(proc.stderr || proc.stdout);
+        if (proc.status !== 0) {
+            let stdout = proc.stdout.toString().trim();
+            let stderr = proc.stderr.toString().trim();
+
+            throw new Error(stderr || stdout);
+        }
 
         let time = Number((process.hrtime.bigint() - start) / 1000000n);
-        console.log(`>> Test ${title} ${style_ansi('[' + (time / 1000).toFixed(2) + 's]', 'green bold')}`);
+        console.log(`>> ${action} ${title} ${style_ansi('[' + (time / 1000).toFixed(2) + 's]', 'green bold')}`);
 
         return true;
     } catch (err) {
-        console.log(`>> Test ${title} ${style_ansi('[error]', 'red bold')}`);
+        console.log(`>> ${action} ${title} ${style_ansi('[error]', 'red bold')}`);
 
         let str = '\n' + style_ansi(err.message.replace(/^/gm, ' '.repeat(3)), '33');
         console.log(str);
