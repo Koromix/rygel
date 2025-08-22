ce45bff1d191b81b239e7763820f434b41647a83
Niels Martignène <niels.martignene@protonmail.com>
2025-08-22 17:06:05 +0200

    Add scripts to list and sync translatable log strings

---
+++ tools/scripts/i18n_list.js
@@ -0,0 +1,123 @@
+#!/usr/bin/env node
+
+// Copyright (C) 2025  Niels Martignène <niels.martignene@protonmail.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+const fs = require('fs');
+const path = require('path');
+
+const DIRECTORIES = [
+    'src/core/base',
+    'src/goupile',
+    'src/rekkord'
+];
+const LANGUAGES = ['fr'];
+
+main();
+
+async function main() {
+    // All the code assumes we are working from root directory
+    process.chdir(__dirname + '/../..');
+
+    try {
+        await run();
+    } catch (err) {
+        console.error(err);
+        process.exit(1);
+    }
+}
+
+async function run() {
+    let args = process.argv.slice(2);
+
+    if (args.includes('--help')) {
+        console.log('Usage: i18n_list.js');
+        return 0;
+    } else {
+        let opt = args.find(arg => arg.startsWith('-'));
+
+        if (opt != null)
+            throw new Error(`Invalid option '${opt}'`);
+    }
+
+    for (let root of DIRECTORIES) {
+        let i18n = path.join(root, 'i18n');
+
+        if (!fs.existsSync(i18n))
+            fs.mkdirSync(i18n);
+
+        let strings = [
+            ...listFilesRec(root, '.cc').flatMap(detectCxxMessages),
+        ];
+        strings = Array.from(new Set(strings)).sort();
+
+        for (let lang of LANGUAGES) {
+            let filename = path.join(i18n, lang + '.json');
+            let translations = fs.existsSync(filename) ? JSON.parse(fs.readFileSync(filename)) : {};
+
+            translations = strings.reduce((obj, str) => { obj[str] = translations[str] ?? null; return obj; }, {});
+
+            fs.writeFileSync(filename, JSON.stringify(translations, null, 4));
+        }
+    }
+}
+
+function listFilesRec(path, ext = null) {
+    let filenames = [];
+
+    let entries = fs.readdirSync(path, { withFileTypes: true });
+
+    for (let entry of entries) {
+        let filename = path + '/' + entry.name;
+
+        if (entry.isDirectory()) {
+            let ret = listFilesRec(filename, ext);
+            filenames.push(...ret);
+        } else if (entry.isFile()) {
+            if (ext == null || entry.name.endsWith(ext))
+                filenames.push(filename);
+        }
+    }
+
+    return filenames;
+}
+
+function completeObject(dest, src) {
+    for (let key in src) {
+        let value = src[key];
+
+        if (value != null && typeof value == 'object') {
+            if (dest[key] == null || typeof dest[key] != 'object')
+                dest[key] = {};
+
+            dest[key] = completeObject(dest[key], value);
+        } else if (!dest.hasOwnProperty(key)) {
+            dest[key] = null;
+        }
+    }
+
+    dest = Object.keys(src).sort().reduce((obj, key) => { obj[key] = dest[key]; return obj; }, {});
+    return dest;
+}
+
+function detectCxxMessages(filename) {
+    let code = fs.readFileSync(filename).toString();
+
+    let matches = [
+        ...code.matchAll(/(?:T|Print|PrintLn|LogError|LogWarning|LogInfo)\(\"(.+)\"(?:, .*)?\)/g)
+    ];
+
+    return matches.map(m => m[1]);
+}
---
+++ tools/scripts/i18n_tolgee.js
@@ -0,0 +1,201 @@
+#!/usr/bin/env node
+
+// Copyright (C) 2025  Niels Martignène <niels.martignene@protonmail.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+const fs = require('fs');
+const path = require('path');
+
+const PROJECTS = {
+    'core/base': 'src/core/base',
+    'goupile': 'src/goupile',
+    'rekkord': 'src/rekkord'
+};
+const LANGUAGES = ['fr'];
+
+const TOLGEE_API_KEY = process.env.TOLGEE_API_KEY || '';
+
+main();
+
+async function main() {
+    // All the code assumes we are working from root directory
+    process.chdir(__dirname + '/../..');
+
+    try {
+        await run();
+        console.log('Done');
+    } catch (err) {
+        console.error(err);
+        process.exit(1);
+    }
+}
+
+async function run() {
+    // Check prerequisites
+    {
+        let errors = [];
+
+        if (!TOLGEE_API_KEY)
+            errors.push('Missing TOLGEE_API_KEY');
+
+        if (errors.length)
+            throw new Error(errors.join('\n'));
+    }
+
+    console.log('Loading languages...');
+    let projects = loadProjects();
+
+    console.log('Fetching translations...');
+    let translations = await fetchTranslations();
+
+    console.log('Pushing new message strings...');
+    for (let project of projects) {
+        for (let key of project.keys.values()) {
+            if (translations.find(t => t.keyName == key))
+                continue;
+
+            let obj = { en: key };
+
+            for (let lang of LANGUAGES)
+                obj[lang] = project.translations[lang][key];
+
+            let json = await fetchOrFail('https://app.tolgee.io/v2/projects/translations', {
+                method: 'POST',
+                headers: {
+                    'X-API-Key': TOLGEE_API_KEY,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    key: key,
+                    namespace: project.name,
+                    translations: obj
+                })
+            }).then(response => response.json());
+        }
+    }
+
+    console.log('Delete unneeded strings...');
+    {
+        let ids = translations.filter(t => !projects.some(project => project.keys.has(t.keyName)));
+
+        if (ids.length) {
+            await fetchOrFail('https://app.tolgee.io/v2/projects/keys', {
+                method: 'DELETE',
+                headers: {
+                    'X-API-Key': TOLGEE_API_KEY,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    ids: ids
+                })
+            });
+        }
+    }
+
+    console.log('Fetching translations (again)...');
+    translations = await fetchTranslations();
+
+    console.log('Apply translations locally...');
+    for (let project of projects) {
+        for (let lang in project.translations) {
+            let target = project.translations[lang];
+
+            for (let item of translations) {
+                if (!target.hasOwnProperty(item.keyName))
+                    continue;
+
+                target[item.keyName] = item.translations[lang].text;
+            }
+
+            let filename = path.join(project.root, 'i18n', lang + '.json');
+            fs.writeFileSync(filename, JSON.stringify(target, null, 4));
+        }
+    }
+}
+
+function loadProjects() {
+    let projects = [];
+
+    for (let name in PROJECTS) {
+        let root = PROJECTS[name];
+
+        let project = {
+            name: name,
+            root: root,
+            keys: [],
+            translations: {}
+        };
+
+        for (let lang of LANGUAGES) {
+            let filename = path.join(root, 'i18n', lang + '.json');
+
+            if (fs.existsSync(filename)) {
+                let translations = JSON.parse(fs.readFileSync(filename));
+
+                project.translations[lang] = translations;
+                project.keys.push(...Object.keys(translations));
+            } else {
+                project.translations[lang] = {};
+            }
+        }
+
+        project.keys = new Set(project.keys);
+        projects.push(project);
+    }
+
+    return projects;
+}
+
+async function fetchTranslations() {
+    let translations = [];
+    let cursor = null;
+
+    for (;;) {
+        let params = {
+            size: 100
+        };
+        if (cursor != null)
+            params.cursor = cursor;
+
+        let url = 'https://app.tolgee.io/v2/projects/translations?' + new URLSearchParams(params);
+
+        let response = await fetchOrFail(url, {
+            headers: {
+                'X-API-Key': TOLGEE_API_KEY
+            }
+        });
+
+        let json = await response.json();
+
+        if (json._embedded == null)
+            break;
+        cursor = json.nextCursor;
+
+        translations.push(...json._embedded.keys);
+    }
+
+    return translations;
+}
+
+async function fetchOrFail(url, options = {}) {
+    let response = await fetch(url, options);
+
+    if (!response.ok) {
+        let text = (await response.text()).trim();
+        throw new Error(text);
+    }
+
+    return response;
+}
