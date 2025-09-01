b07e2005cb65e7191ec0fd28eede4a8026f6e3d1
Niels Martignène <niels.martignene@protonmail.com>
2025-09-01 13:46:12 +0200

    Tolerate one 400 error in S3 for buggy endpoints

--- src/core/request/s3.cc
+++ src/core/request/s3.cc
@@ -934,8 +934,11 @@ void s3_Client::ReleaseConnection(CURL *curl)
     connections.Append(curl);
 }
 
-static inline bool ShouldRetry(int status)
+static inline bool ShouldRetry(int count, int status)
 {
+    if (status == 400 && !count) // Retry once for buggy endpoints
+        return true;
+
     if (status == 409) // Transient conflict
         return true;
 
@@ -983,7 +986,7 @@ int s3_Client::RunSafe(const char *action, int tries, int expect, FunctionRef<in
 
         if (status == 200 || status == expect)
             return status;
-        if (status > 0 && !ShouldRetry(status))
+        if (status > 0 && !ShouldRetry(i, status))
             break;
 
         // Connection may be busted for some reason, start from scratch
