f65aed0829be132bd03e4c27da8be217c8eb6a2e
Niels Martignène <niels.martignene@protonmail.com>
2025-09-15 15:32:59 +0200

    Remove unnecessary code block for FPS counter

--- src/heimdall/client/heimdall.js
+++ src/heimdall/client/heimdall.js
@@ -1260,13 +1260,10 @@ function draw() {
         ctx.font = `${18 * window.devicePixelRatio}px Open Sans`;
         ctx.fillStyle = 'black';
 
-        // FPS counter
-        {
-            let text = `FPS : ${(1000 / runner.frameTime).toFixed(0)} (${runner.frameTime.toFixed(1)} ms)` +
-                       ` | Update : ${runner.updateTime.toFixed(1)} ms | Draw : ${runner.drawTime.toFixed(1)} ms` +
-                       ` | Entities : ${world.entities.length}`;
-            runner.text(layout.main.left + 12, layout.main.top + 8, text, { align: 7, background: '#ffffffcc' });
-        }
+        let text = `FPS : ${(1000 / runner.frameTime).toFixed(0)} (${runner.frameTime.toFixed(1)} ms)` +
+                   ` | Update : ${runner.updateTime.toFixed(1)} ms | Draw : ${runner.drawTime.toFixed(1)} ms` +
+                   ` | Entities : ${world.entities.length}`;
+        runner.text(layout.main.left + 12, layout.main.top + 8, text, { align: 7, background: '#ffffffcc' });
     }
 }
 
