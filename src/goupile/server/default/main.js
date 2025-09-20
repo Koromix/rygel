ec3c470915db9a6cec5e53db5f8e85f16b1daa3b
Niels Martignène <niels.martignene@protonmail.com>
2025-09-20 11:42:01 +0200

    Enable annotations for new projects

--- src/goupile/server/default/main.js
+++ src/goupile/server/default/main.js
@@ -1,6 +1,9 @@
-// Ce fichier définit un formulaire composé de 3 pages
+// Permettre d'annoter chaque variable définit dans les formulaires du projet
+app.annotate = true;
 
-app.form("suivi", "Suivi", () => {
+app.form("projet", "Titre du projet", () => {
+    // Ce fichier définit un formulaire composé de 3 pages
+
     app.form("introduction", "Introduction")
     app.form("avance", "Avancé")
     app.form("mise_en_page", "Mise en page")
