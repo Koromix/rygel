98ac13c47f2e867d7ad27698f7b28827c4a3b096
Niels Martignène <niels.martignene@protonmail.com>
2025-09-29 16:56:40 +0200

    Fix buffer overflow in JSON number parser

--- src/core/wrap/json.cc
+++ src/core/wrap/json.cc
@@ -100,7 +100,7 @@ bool json_Parser::Handler::RawNumber(const char *str, Size len, bool)
     token = json_TokenType::Number;
 
     u.num.len = std::min(len, K_SIZE(u.num.data) - 1);
-    MemCpy(u.num.data, str, len);
+    MemCpy(u.num.data, str, u.num.len);
     u.num.data[u.num.len] = 0;
 
     return true;
