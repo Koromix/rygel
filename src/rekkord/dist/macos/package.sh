c7d907afcc62508e634779a995e1ccdb7e3d82eb
Niels Martignène <niels.martignene@protonmail.com>
2025-10-19 05:48:36 +0200

    Create x86_64 and ARM64 macOS packages

--- src/rekkord/dist/macos/package.sh
+++ src/rekkord/dist/macos/package.sh
@@ -4,18 +4,23 @@ set -e
 cd "$(dirname $0)/../../../.."
 
 ./bootstrap.sh
-./felix -pFast rekkord
+./felix -pDebug rekkord
 
-VERSION=$(bin/Fast/rekkord --version | awk -F'[ _]' '/^rekkord/ {print $2}')
+VERSION=$(bin/Debug/rekkord --version | awk -F'[ _]' '/^rekkord/ {print $2}')
 PACKAGE_DIR=bin/Packages/rekkord/macos
-DMG_FILENAME=bin/Packages/rekkord_${VERSION}_macos.dmg
 
 rm -rf $PACKAGE_DIR/pkg
 mkdir -p $PACKAGE_DIR $PACKAGE_DIR/pkg
 
-install -m0755 bin/Fast/rekkord $PACKAGE_DIR/pkg/rekkord
 install -m0644 src/rekkord/README.md $PACKAGE_DIR/pkg/README.md
 install -m0644 LICENSE.txt $PACKAGE_DIR/pkg/LICENSE.txt
 
-rm -f $DMG_FILENAME
-hdiutil create -srcfolder "$PACKAGE_DIR/pkg" -volname "rekkord" $DMG_FILENAME
+for arch in x86_64 ARM64; do
+    ./felix -pFast --host=$arch -O bin/Fast_${arch} rekkord
+
+    dmg_filename=bin/Packages/rekkord_${VERSION}_macos_${arch}.dmg
+
+    rm -f "$dmg_filename"
+    install -m0755 bin/Fast_${arch}/rekkord "$PACKAGE_DIR/pkg/rekkord"
+    hdiutil create -srcfolder "$PACKAGE_DIR/pkg" -volname "rekkord" "$dmg_filename"
+done
