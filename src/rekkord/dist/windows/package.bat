e7dcf2451a2ee5bb8c0d2239269acf33a3eb0396
Niels Martignène <niels.martignene@protonmail.com>
2025-10-19 05:21:31 +0200

    Add LICENSE file to Windows package

--- src/rekkord/dist/windows/package.bat
+++ src/rekkord/dist/windows/package.bat
@@ -5,7 +5,7 @@ cd %~dp0
 cd ..\..\..\..
 
 start bootstrap.bat
-felix.exe -pFast rekkord RekkordTray
+felix.exe -pFast rekkord
 
 for /f "tokens=2 delims= " %%i in ('bin\Fast\rekkord.exe --version') do (
     set RAW_VERSION=%%i
@@ -27,10 +27,10 @@ set PACKAGE_DIR=bin\Packages\rekkord\windows
 mkdir %PACKAGE_DIR%
 
 copy bin\Fast\rekkord.exe %PACKAGE_DIR%\rekkord.exe
-copy bin\Fast\RekkordTray.exe %PACKAGE_DIR%\RekkordTray.exe
 copy src\rekkord\README.md %PACKAGE_DIR%\README.md
+copy LICENSE.txt %PACKAGE_DIR%\LICENSE.txt
 
 cd %PACKAGE_DIR%
 
 REM Create ZIP file
-tar.exe -a -c -f ..\..\rekkord_%VERSION%_win64.zip README.md rekkord.exe
+tar.exe -a -c -f ..\..\rekkord_%VERSION%_win64.zip LICENSE.txt README.md rekkord.exe
