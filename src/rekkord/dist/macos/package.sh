c9e20dd6fb6526581069877702eca59f73abae25
Niels Martignène <niels.martignene@protonmail.com>
2025-10-19 08:47:38 +0200

    Release rekkord in single universal DMG file

--- src/rekkord/dist/macos/package.sh
+++ src/rekkord/dist/macos/package.sh
@@ -8,6 +8,7 @@ cd "$(dirname $0)/../../../.."
 
 VERSION=$(bin/Debug/rekkord --version | awk -F'[ _]' '/^rekkord/ {print $2}')
 PACKAGE_DIR=bin/Packages/rekkord/macos
+DMG_FILENAME=bin/Packages/rekkord_${VERSION}_macos.dmg
 
 rm -rf $PACKAGE_DIR/pkg
 mkdir -p $PACKAGE_DIR $PACKAGE_DIR/pkg
@@ -15,12 +16,9 @@ mkdir -p $PACKAGE_DIR $PACKAGE_DIR/pkg
 install -m0644 src/rekkord/README.md $PACKAGE_DIR/pkg/README.md
 install -m0644 LICENSE.txt $PACKAGE_DIR/pkg/LICENSE.txt
 
-for arch in x86_64 ARM64; do
-    ./felix -pFast --host=$arch -O bin/Fast_${arch} rekkord
+./felix -pFast --host=x86_64 -O bin/x86_64 rekkord
+./felix -pFast --host=ARM64 -O bin/ARM64 rekkord
+lipo -create -output $PACKAGE_DIR/pkg/rekkord bin/x86_64/rekkord bin/ARM64/rekkord
 
-    dmg_filename=bin/Packages/rekkord_${VERSION}_macos_${arch}.dmg
-
-    rm -f "$dmg_filename"
-    install -m0755 bin/Fast_${arch}/rekkord "$PACKAGE_DIR/pkg/rekkord"
-    hdiutil create -srcfolder "$PACKAGE_DIR/pkg" -volname "rekkord" "$dmg_filename"
-done
+rm -f "$DMG_FILENAME"
+hdiutil create -srcfolder "$PACKAGE_DIR/pkg" -volname "rekkord" "$DMG_FILENAME"
