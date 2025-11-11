57d3042af7d7f5a01e1f3b204e1cbdf7aa43cd56
Niels Martignène <niels.martignene@protonmail.com>
2025-11-11 12:14:49 +0100

    Create en.html symlink in goupile.org build script

--- web/goupile.org/build.sh
+++ web/goupile.org/build.sh
@@ -8,3 +8,5 @@ cd "$(dirname $0)"
 mkdir -p ../../bin/Web/goupile.org/en
 ../../bin/Fast/hodler fr -O ../../bin/Web/goupile.org --gzip
 ../../bin/Fast/hodler en -b /en/ -O ../../bin/Web/goupile.org/en --gzip
+
+ln -s en/index.html ../../bin/Web/goupile.org/en.html
