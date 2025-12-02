85113a763e3fe17c463974b1f1e28611265ae374
Niels Martignène <niels.martignene@protonmail.com>
2025-12-02 13:50:10 +0100

    Fix ldv-recherche.fr develop script

--- web/ldv-recherche.fr/develop.sh
+++ web/ldv-recherche.fr/develop.sh
@@ -2,14 +2,14 @@
 
 cd "$(dirname $0)"
 
-../../../bootstrap.sh
-../../../felix -pFast nestor hodler
+../../bootstrap.sh
+../../felix -pFast nestor hodler
 
 trap 'kill $(jobs -p) 2>/dev/null' EXIT
 trap 'kill $(jobs -p) 2>/dev/null' SIGINT
 
 mkdir -p dist
-../../../bin/Fast/nestor dist/ &
-../../../bin/Fast/hodler . -O dist --loop --sourcemap &
+../../bin/Fast/nestor dist/ &
+../../bin/Fast/hodler . -O dist --loop --sourcemap &
 
 wait $(jobs -p)
