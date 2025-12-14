847beda98e3e6462347978701ab7d1422f46d79e
Niels Martignène <niels.martignene@protonmail.com>
2025-12-14 17:25:50 +0100

    Add abstract background to STAKS

--- src/staks/assets/assets.js
+++ src/staks/assets/assets.js
@@ -1,116 +1,115 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
-// SPDX-FileCopyrightText: 2025 Niels Martignène <niels.martignene@protonmail.com>
-
 import asset0 from './staks.png';
-import asset1 from './backgrounds/aurora.webp';
-import asset2 from './backgrounds/somewhere.webp';
-import asset3 from './backgrounds/winter.webp';
-import asset4 from './ui/background.png';
-import asset5 from './ui/clockwise.png';
-import asset6 from './ui/counterclock.png';
-import asset7 from './ui/drop.png';
-import asset8 from './ui/fullscreen.png';
-import asset9 from './ui/ghost.png';
-import asset10 from './ui/help.png';
-import asset11 from './ui/left.png';
-import asset12 from './ui/music.png';
-import asset13 from './ui/music0.png';
-import asset14 from './ui/music1.png';
-import asset15 from './ui/pause0.png';
-import asset16 from './ui/pause1.png';
-import asset17 from './ui/right.png';
-import asset18 from './ui/sound0.png';
-import asset19 from './ui/sound1.png';
-import asset20 from './ui/start.png';
-import asset21 from './ui/turbo.png';
-import asset22 from './musics/mp3/abstract_piano_ambient.mp3';
-import asset23 from './musics/mp3/ambient_future_tech.mp3';
-import asset24 from './musics/mp3/architecture.mp3';
-import asset25 from './musics/mp3/medical.mp3';
-import asset26 from './musics/mp3/motivational_day.mp3';
-import asset27 from './musics/mp3/motivational_stylish_background.mp3';
-import asset28 from './musics/mp3/serious_corporate.mp3';
-import asset29 from './musics/mp3/summer_beauty.mp3';
-import asset30 from './musics/mp3/summer_every_day.mp3';
-import asset31 from './musics/mp3/tech_corporation.mp3';
-import asset32 from './musics/opus/abstract_piano_ambient.webm';
-import asset33 from './musics/opus/ambient_future_tech.webm';
-import asset34 from './musics/opus/architecture.webm';
-import asset35 from './musics/opus/medical.webm';
-import asset36 from './musics/opus/motivational_day.webm';
-import asset37 from './musics/opus/motivational_stylish_background.webm';
-import asset38 from './musics/opus/serious_corporate.webm';
-import asset39 from './musics/opus/summer_beauty.webm';
-import asset40 from './musics/opus/summer_every_day.webm';
-import asset41 from './musics/opus/tech_corporation.webm';
-import asset42 from './sounds/mp3/clear.mp3';
-import asset43 from './sounds/mp3/gameover.mp3';
-import asset44 from './sounds/mp3/hold.mp3';
-import asset45 from './sounds/mp3/levelup.mp3';
-import asset46 from './sounds/mp3/lock.mp3';
-import asset47 from './sounds/mp3/move.mp3';
-import asset48 from './sounds/opus/clear.webm';
-import asset49 from './sounds/opus/gameover.webm';
-import asset50 from './sounds/opus/hold.webm';
-import asset51 from './sounds/opus/levelup.webm';
-import asset52 from './sounds/opus/lock.webm';
-import asset53 from './sounds/opus/move.webm';
+import asset1 from './backgrounds/abstract.webp';
+import asset2 from './backgrounds/aurora.webp';
+import asset3 from './backgrounds/somewhere.webp';
+import asset4 from './backgrounds/winter.webp';
+import asset5 from './ui/background.png';
+import asset6 from './ui/clockwise.png';
+import asset7 from './ui/counterclock.png';
+import asset8 from './ui/drop.png';
+import asset9 from './ui/fullscreen.png';
+import asset10 from './ui/ghost.png';
+import asset11 from './ui/help.png';
+import asset12 from './ui/left.png';
+import asset13 from './ui/music.png';
+import asset14 from './ui/music0.png';
+import asset15 from './ui/music1.png';
+import asset16 from './ui/pause0.png';
+import asset17 from './ui/pause1.png';
+import asset18 from './ui/right.png';
+import asset19 from './ui/sound0.png';
+import asset20 from './ui/sound1.png';
+import asset21 from './ui/start.png';
+import asset22 from './ui/turbo.png';
+import asset23 from './musics/mp3/abstract_piano_ambient.mp3';
+import asset24 from './musics/mp3/ambient_future_tech.mp3';
+import asset25 from './musics/mp3/architecture.mp3';
+import asset26 from './musics/mp3/medical.mp3';
+import asset27 from './musics/mp3/motivational_day.mp3';
+import asset28 from './musics/mp3/motivational_stylish_background.mp3';
+import asset29 from './musics/mp3/serious_corporate.mp3';
+import asset30 from './musics/mp3/summer_beauty.mp3';
+import asset31 from './musics/mp3/summer_every_day.mp3';
+import asset32 from './musics/mp3/tech_corporation.mp3';
+import asset33 from './musics/opus/abstract_piano_ambient.webm';
+import asset34 from './musics/opus/ambient_future_tech.webm';
+import asset35 from './musics/opus/architecture.webm';
+import asset36 from './musics/opus/medical.webm';
+import asset37 from './musics/opus/motivational_day.webm';
+import asset38 from './musics/opus/motivational_stylish_background.webm';
+import asset39 from './musics/opus/serious_corporate.webm';
+import asset40 from './musics/opus/summer_beauty.webm';
+import asset41 from './musics/opus/summer_every_day.webm';
+import asset42 from './musics/opus/tech_corporation.webm';
+import asset43 from './sounds/mp3/clear.mp3';
+import asset44 from './sounds/mp3/gameover.mp3';
+import asset45 from './sounds/mp3/hold.mp3';
+import asset46 from './sounds/mp3/levelup.mp3';
+import asset47 from './sounds/mp3/lock.mp3';
+import asset48 from './sounds/mp3/move.mp3';
+import asset49 from './sounds/opus/clear.webm';
+import asset50 from './sounds/opus/gameover.webm';
+import asset51 from './sounds/opus/hold.webm';
+import asset52 from './sounds/opus/levelup.webm';
+import asset53 from './sounds/opus/lock.webm';
+import asset54 from './sounds/opus/move.webm';
 
 const ASSETS = {
     'staks': asset0,
-    'backgrounds/aurora': asset1,
-    'backgrounds/somewhere': asset2,
-    'backgrounds/winter': asset3,
-    'ui/background': asset4,
-    'ui/clockwise': asset5,
-    'ui/counterclock': asset6,
-    'ui/drop': asset7,
-    'ui/fullscreen': asset8,
-    'ui/ghost': asset9,
-    'ui/help': asset10,
-    'ui/left': asset11,
-    'ui/music': asset12,
-    'ui/music0': asset13,
-    'ui/music1': asset14,
-    'ui/pause0': asset15,
-    'ui/pause1': asset16,
-    'ui/right': asset17,
-    'ui/sound0': asset18,
-    'ui/sound1': asset19,
-    'ui/start': asset20,
-    'ui/turbo': asset21,
-    'musics/mp3/abstract_piano_ambient': asset22,
-    'musics/mp3/ambient_future_tech': asset23,
-    'musics/mp3/architecture': asset24,
-    'musics/mp3/medical': asset25,
-    'musics/mp3/motivational_day': asset26,
-    'musics/mp3/motivational_stylish_background': asset27,
-    'musics/mp3/serious_corporate': asset28,
-    'musics/mp3/summer_beauty': asset29,
-    'musics/mp3/summer_every_day': asset30,
-    'musics/mp3/tech_corporation': asset31,
-    'musics/opus/abstract_piano_ambient': asset32,
-    'musics/opus/ambient_future_tech': asset33,
-    'musics/opus/architecture': asset34,
-    'musics/opus/medical': asset35,
-    'musics/opus/motivational_day': asset36,
-    'musics/opus/motivational_stylish_background': asset37,
-    'musics/opus/serious_corporate': asset38,
-    'musics/opus/summer_beauty': asset39,
-    'musics/opus/summer_every_day': asset40,
-    'musics/opus/tech_corporation': asset41,
-    'sounds/mp3/clear': asset42,
-    'sounds/mp3/gameover': asset43,
-    'sounds/mp3/hold': asset44,
-    'sounds/mp3/levelup': asset45,
-    'sounds/mp3/lock': asset46,
-    'sounds/mp3/move': asset47,
-    'sounds/opus/clear': asset48,
-    'sounds/opus/gameover': asset49,
-    'sounds/opus/hold': asset50,
-    'sounds/opus/levelup': asset51,
-    'sounds/opus/lock': asset52,
-    'sounds/opus/move': asset53,
+    'backgrounds/abstract': asset1,
+    'backgrounds/aurora': asset2,
+    'backgrounds/somewhere': asset3,
+    'backgrounds/winter': asset4,
+    'ui/background': asset5,
+    'ui/clockwise': asset6,
+    'ui/counterclock': asset7,
+    'ui/drop': asset8,
+    'ui/fullscreen': asset9,
+    'ui/ghost': asset10,
+    'ui/help': asset11,
+    'ui/left': asset12,
+    'ui/music': asset13,
+    'ui/music0': asset14,
+    'ui/music1': asset15,
+    'ui/pause0': asset16,
+    'ui/pause1': asset17,
+    'ui/right': asset18,
+    'ui/sound0': asset19,
+    'ui/sound1': asset20,
+    'ui/start': asset21,
+    'ui/turbo': asset22,
+    'musics/mp3/abstract_piano_ambient': asset23,
+    'musics/mp3/ambient_future_tech': asset24,
+    'musics/mp3/architecture': asset25,
+    'musics/mp3/medical': asset26,
+    'musics/mp3/motivational_day': asset27,
+    'musics/mp3/motivational_stylish_background': asset28,
+    'musics/mp3/serious_corporate': asset29,
+    'musics/mp3/summer_beauty': asset30,
+    'musics/mp3/summer_every_day': asset31,
+    'musics/mp3/tech_corporation': asset32,
+    'musics/opus/abstract_piano_ambient': asset33,
+    'musics/opus/ambient_future_tech': asset34,
+    'musics/opus/architecture': asset35,
+    'musics/opus/medical': asset36,
+    'musics/opus/motivational_day': asset37,
+    'musics/opus/motivational_stylish_background': asset38,
+    'musics/opus/serious_corporate': asset39,
+    'musics/opus/summer_beauty': asset40,
+    'musics/opus/summer_every_day': asset41,
+    'musics/opus/tech_corporation': asset42,
+    'sounds/mp3/clear': asset43,
+    'sounds/mp3/gameover': asset44,
+    'sounds/mp3/hold': asset45,
+    'sounds/mp3/levelup': asset46,
+    'sounds/mp3/lock': asset47,
+    'sounds/mp3/move': asset48,
+    'sounds/opus/clear': asset49,
+    'sounds/opus/gameover': asset50,
+    'sounds/opus/hold': asset51,
+    'sounds/opus/levelup': asset52,
+    'sounds/opus/lock': asset53,
+    'sounds/opus/move': asset54,
 };
 
 export { ASSETS }
