7b3deeeb57b9cb168335cfb1f7ec02e413054bde
Niels Martignène <niels.martignene@protonmail.com>
2025-12-18 10:23:54 +0100

    Add source distribution script for meestic

---
+++ src/meestic/dist/source/source.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -e
+
+PKG_NAME=meestic
+BUILD_TARGETS="meestic MeesticTray"
+VERSION_TARGET=meestic
+
+adjust() {
+    cp src/meestic/*.md "$1"
+
+    rm -rf "$1/vendor/curl/docs" "$1/vendor/curl/tests"
+    rm -rf "$1/vendor/mbedtls/docs" "$1/vendor/mbedtls/tests"
+}
+
+cd "$(dirname $0)/../../../.."
+. tools/package/build/source/package.sh
