5816591607a41a0faa8b8d69197782cb8b99cec8
Niels Martignène <niels.martignene@protonmail.com>
2025-12-18 07:49:40 +0100

    Use full version string for source packages

--- tools/package/build/source/package.sh
+++ tools/package/build/source/package.sh
@@ -10,7 +10,7 @@ rm -f "${DEST_DIR}/bin/Log/commands.txt"
 
 directories=$(grep -E -o '[|"](lib/native|lib/web|src|vendor)/[a-zA-Z0-9_\-]+/' < "${DEST_DIR}/bin/Log/commands.txt" | cut -c 2- | sort | uniq)
 sources="bootstrap.sh bootstrap.bat FelixBuild.ini.presets ${directories}"
-version=$(./felix -pDebug -O "${DEST_DIR}/bin" --run "${VERSION_TARGET}" --version | awk -F'[ _]' "/^${VERSION_TARGET}/ {print \$2}")
+version=$(./felix -pDebug -O "${DEST_DIR}/bin" --run "${VERSION_TARGET}" --version | awk -F'[ ]' "/^${VERSION_TARGET}/ {print \$2}")
 imports=$(ls "${DEST_DIR}/bin/Log/"*.json | xargs jq -r '.imports | .[]')
 
 rm -rf "${DEST_DIR}/tmp"
