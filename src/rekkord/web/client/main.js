5e6088fd48d54186a588beed65d6d6447b6af4b2
Niels Martignène <niels.martignene@protonmail.com>
2025-12-28 18:31:30 +0100

    Reorder tabs in security dialog when no password is set

--- src/rekkord/web/client/main.js
+++ src/rekkord/web/client/main.js
@@ -833,12 +833,15 @@ async function configureSecurity() {
 
                 <div class="main">
                     <div class="tabbar">
-                        <a class=${classMap({ active: tab == 'password' })}
-                           @click=${e => { tab = 'password'; render(); }}>${T.password}</a>
-                        <a class=${classMap({ active: tab == 'totp', disabled: !security.password })}
-                           @click=${e => { tab = 'totp'; render(); }}>${T.two_factor_authentication}</a>
-                        <a class=${classMap({ active: tab == 'identities' })}
-                           @click=${e => { tab = 'identities'; render(); }}>${T.external_identities}</a>
+                        ${security.password ? html`
+                            <a class=${tab == 'password' ? 'active' : '' } @click=${e => { tab = 'password'; render(); }}>${T.password}</a>
+                            <a class=${tab == 'totp' ? 'active' : '' } @click=${e => { tab = 'totp'; render(); }}>${T.two_factor_authentication}</a>
+                            <a class=${tab == 'identities' ? 'active' : '' } @click=${e => { tab = 'identities'; render(); }}>${T.external_identities}</a>
+                        ` : ''}
+                        ${!security.password ? html`
+                            <a class=${tab == 'identities' ? 'active' : '' } @click=${e => { tab = 'identities'; render(); }}>${T.external_identities}</a>
+                            <a class=${tab == 'password' ? 'active' : '' } @click=${e => { tab = 'password'; render(); }}>${T.password}</a>
+                        ` : ''}
                     </div>
 
                     <div class="tab">
