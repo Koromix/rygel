aaabb6bc55a05033ebe3bfc6c04053de8729ef3d
Niels Martignène <niels.martignene@protonmail.com>
2025-12-28 20:07:06 +0100

    Reverse use of c-ares for DNS queries for now
    For some reason, things go wrong when c-ares is used inside
    a Landlock sandbox, sendto() fails somehow, and without any network
    restriction option.

--- vendor/c-ares/test/fuzzcheck.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Copyright (C) The c-ares project and its contributors
-# SPDX-License-Identifier: MIT
-set -e
-# Check that all of the base fuzzing corpus parse without errors
-./aresfuzz fuzzinput/*
-./aresfuzzname fuzznames/*
