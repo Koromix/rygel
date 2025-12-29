05cf12b351fb455bf264d18e1271eac5a79211b4
Niels Martignène <niels.martignene@protonmail.com>
2025-12-29 13:33:11 +0100

    Add missing CSP value for style-src in Ludivine and Rekkord Web

--- src/rekkord/web/server/rokkerd.cc
+++ src/rekkord/web/server/rokkerd.cc
@@ -419,7 +419,7 @@ static void HandleRequest(http_IO *io)
             Span<const char> csp = Fmt(io->Allocator(), "base-uri 'none'; "
                                                         "default-src 'self'; "
                                                         "script-src 'self' 'nonce-%1'; "
-                                                        "style-src 'unsafe-inline'; "
+                                                        "style-src 'self' 'unsafe-inline'; "
                                                         "style-src-elem 'self' 'nonce-%1'; "
                                                         "style-src-attr 'self' 'unsafe-inline'; "
                                                         "img-src 'self' data:; "
