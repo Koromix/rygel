510fecb2f683134cc6b1b1068b4d8b5895567aa8
Niels Martignène <niels.martignene@protonmail.com>
2026-04-19 03:26:50 +0200

    Add Koffi LTO+PGO batch script for Windows

---
+++ src/koffi/tools/build_pgo.bat
@@ -0,0 +1,21 @@
+@echo off
+setlocal
+
+cd "%~dp0\.."
+
+set PGO_DIR="%cd%\..\..\bin\Koffi\pgo"
+
+rmdir /S /Q "%PGO_DIR%" 2>NUL
+mkdir "%PGO_DIR%"
+
+node "../cnoke/cnoke.js" clean --release
+node "../cnoke/cnoke.js" -d "PGO_GENERATE=%PGO_DIR%" --clang --release -v
+
+node "../cnoke/cnoke.js" -D test --release --clang -v
+node test/test.js
+node test/test.js
+
+llvm-profdata merge -output="%PGO_DIR%\koffi.profdata" "%PGO_DIR%\*.profraw"
+
+node "../cnoke/cnoke.js" clean --release
+node "../cnoke/cnoke.js" -d "PGO_PROFILE=%PGO_DIR%\koffi.profdata" --clang --release -v
