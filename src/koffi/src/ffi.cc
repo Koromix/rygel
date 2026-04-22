cae5e72963e62073e9a1857b0c5c56e969f5a063
Niels Martignène <niels.martignene@protonmail.com>
2026-04-22 06:24:27 +0200

    Fix error message when using invalid String array hint

--- src/koffi/src/ffi.cc
+++ src/koffi/src/ffi.cc
@@ -1025,7 +1025,7 @@ static Napi::Value CreateArrayType(const Napi::CallbackInfo &info)
             hint = ArrayHint::Array;
         } else if (str == "String" || str == "string") {
             if (ref->primitive != PrimitiveKind::Int8 && ref->primitive != PrimitiveKind::Int16) {
-                ThrowError<Napi::Error>(env, "Array hint 'String' can only be used with 8 and 16-bit signed integer types");
+                ThrowError<Napi::Error>(env, "Array hint 'String' can only be used with 8, 16 and 32-bit signed integer types");
                 return env.Null();
             }
 
