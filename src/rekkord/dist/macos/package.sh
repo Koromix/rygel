886fe2326c8743af84c0838ee71511f288956f6d
Niels Martignène <niels.martignene@protonmail.com>
2026-06-29 15:26:47 +0200

    Distribute Rekkord for macOS in ZIP files

--- src/rekkord/dist/macos/package.sh
+++ src/rekkord/dist/macos/package.sh
@@ -7,18 +7,21 @@ cd "$(dirname $0)/../../../.."
 ./felix -pDebug rekkord
 
 VERSION=$(bin/Debug/rekkord --version | awk -F'[ _]' '/^rekkord/ {print $2}')
-PACKAGE_DIR=bin/Packages/rekkord/macos
+PACKAGE_DIR=bin/Packages/rekkord/macos/rekkord_${VERSION}
 DMG_FILENAME=bin/Packages/rekkord_${VERSION}_macos.dmg
+TAR_FILENAME=bin/Packages/rekkord_${VERSION}_macos.tar
 
-rm -rf $PACKAGE_DIR/pkg
-mkdir -p $PACKAGE_DIR $PACKAGE_DIR/pkg
+rm -rf "$PACKAGE_DIR"
+mkdir -p "$PACKAGE_DIR"
 
-install -m0644 src/rekkord/README.md $PACKAGE_DIR/pkg/README.md
-install -m0644 LICENSE.txt $PACKAGE_DIR/pkg/LICENSE.txt
+install -m0644 src/rekkord/README.md "$PACKAGE_DIR/README.md"
+install -m0644 LICENSE.txt "$PACKAGE_DIR/LICENSE.txt"
 
 ./felix -pFast --host=x86_64 -O bin/x86_64 rekkord
 ./felix -pFast --host=ARM64 -O bin/ARM64 rekkord
-lipo -create -output $PACKAGE_DIR/pkg/rekkord bin/x86_64/rekkord bin/ARM64/rekkord
+lipo -create -output "$PACKAGE_DIR/rekkord" bin/x86_64/rekkord bin/ARM64/rekkord
 
 rm -f "$DMG_FILENAME"
-hdiutil create -srcfolder "$PACKAGE_DIR/pkg" -volname "rekkord" "$DMG_FILENAME"
+hdiutil create -srcfolder "$PACKAGE_DIR" -volname "rekkord" "$DMG_FILENAME"
+tar -C "$(dirname $PACKAGE_DIR)" -c "$(basename $PACKAGE_DIR)" | gzip > "$TAR_FILENAME.gz"
+tar -C "$(dirname $PACKAGE_DIR)" -c "$(basename $PACKAGE_DIR)" | xz > "$TAR_FILENAME.xz"
