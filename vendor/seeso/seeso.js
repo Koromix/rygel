25e1d24623e54a41bab860c41ea36327fafc869c
Niels Martignène <niels.martignene@protonmail.com>
2026-08-07 11:19:57 +0200

    Clean up leftover SeeSo files

--- vendor/seeso/seeso.js
+++
@@ -1,4 +0,0 @@
-import Seeso, { CalibrationAccuracyCriteria, InitializationErrorType } from 'seeso/dist/seeso.js';
-
-export default Seeso;
-export { CalibrationAccuracyCriteria, InitializationErrorType }
