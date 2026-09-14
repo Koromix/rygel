907419eadd06a385887b8223a951ab4867908e0a
Niels Martignène <niels.martignene@protonmail.com>
2026-09-14 18:58:50 +0200

    Adjust error message in ppc64le load script

--- src/koffi/tools/loaders/linux-ppc64.js
+++ src/koffi/tools/loaders/linux-ppc64.js
@@ -4,7 +4,7 @@
 import { determineAbi } from '../../../cnoke/src/abi.js';
 
 if (determineAbi() != 'ppc64le')
-    throw new Error('The PowerPC64 prebuild only supports the Little-Endian systems');
+    throw new Error('The PowerPC64 prebuild only supports ELFv2 Little-Endian systems');
 
 let filename = './linux_ppc64le/koffi.node';
 module.exports = require(filename);
