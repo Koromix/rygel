5801fcee214642d90488a4eeba61be9aae8820ea
Niels Martignène <niels.martignene@protonmail.com>
2026-09-15 14:51:22 +0200

    Switch to VersityGW for Rekkord S3 tests
    The POSIX backend is faster, simpler and more complete (conditional
    writes) than Garage.
    
    Also, it's much easier to configure.

--- src/rekkord/test/ftzz_s3_nock.sh
+++ src/rekkord/test/ftzz_s3_nock.sh
@@ -5,48 +5,22 @@ cd "$(dirname $0)"
 ../../../bootstrap.sh
 ../../../felix -pFast rekkord
 
-mkdir -p /tmp/rekkord/ftzz_s3
-install ../../../bin/Fast/rekkord /tmp/rekkord/ftzz_s3/rekkord
-cd /tmp/rekkord/ftzz_s3
+mkdir -p /tmp/rekkord/ftzz_s3_nock
+install ../../../bin/Fast/rekkord /tmp/rekkord/ftzz_s3_nock/rekkord
+cd /tmp/rekkord/ftzz_s3_nock
 
-rm -f linux.tar.xz garage
 curl -L -o linux.tar.xz https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.17.8.tar.xz
-curl -L -o garage https://garagehq.deuxfleurs.fr/_releases/v2.1.0/x86_64-unknown-linux-musl/garage
-chmod +x garage
+curl -L versitygw.tgz https://github.com/versity/versitygw/releases/download/v1.8.0/versitygw_v1.8.0_Linux_x86_64.tar.gz | tar xzO versitygw_v1.8.0_Linux_x86_64/versitygw > versitygw
+curl -L -o ftzz https://github.com/SUPERCILEX/ftzz/releases/download/4.0.0/x86_64-unknown-linux-musl-ftzz
+chmod +x versitygw
+chmod +x ftzz
 
 export LANG=C
 export REKKORD_CONFIG_FILE=rekkord.ini
 export XDG_CACHE_HOME=$PWD/cache
 
-cat > garage.toml <<EOF
-metadata_dir = "repo/meta"
-data_dir = "repo/data"
-db_engine = "sqlite"
-
-replication_factor = 1
-
-rpc_bind_addr = "[::]:3901"
-rpc_public_addr = "127.0.0.1:3901"
-rpc_secret = "$(openssl rand -hex 32)"
-
-[s3_api]
-s3_region = "garage"
-api_bind_addr = "[::]:3900"
-root_domain = ".s3.garage.localhost"
-
-[s3_web]
-bind_addr = "[::]:3902"
-root_domain = ".web.garage.localhost"
-index = "index.html"
-
-[k2v_api]
-api_bind_addr = "[::]:3904"
-
-[admin]
-api_bind_addr = "[::]:3903"
-admin_token = "$(openssl rand -base64 32)"
-metrics_token = "$(openssl rand -base64 32)"
-EOF
+export ROOT_ACCESS_KEY=root
+export ROOT_SECRET_KEY=root
 
 trap 'kill $(jobs -p) 2>/dev/null' EXIT
 trap 'kill $(jobs -p) 2>/dev/null' SIGINT
@@ -54,30 +28,23 @@ trap 'kill $(jobs -p) 2>/dev/null' SIGINT
 while true; do
     rm -rf cache repo src dest rekkord.ini rekkord.key
 
-    mkdir repo
+    mkdir repo repo/iam repo/data repo/version
 
-    RUST_LOG=garage=warn ./garage -c garage.toml server &
-    sleep 5
-
-    node=$(./garage -c garage.toml status | awk '/NO ROLE/ {print $1}')
-    ./garage -c garage.toml layout assign -c 16G -z ftzz $node
-    ./garage -c garage.toml layout apply --version=1
-    ./garage -c garage.toml bucket create ftzz
-    secret=$(./garage -c garage.toml key create ftzz | awk -F ': +' '/Secret key/ {print $2}')
-    key=$(./garage -c garage.toml key info ftzz | awk -F ': +' '/Key ID/ {print $2}')
-    ./garage -c garage.toml bucket allow ftzz --key $key --read --write
+    ./versitygw --quiet --iam-dir $PWD/repo/iam posix --versioning-dir $PWD/repo/version $PWD/repo/data &
+    sleep 2
+    ./versitygw admin --endpoint-url http://localhost:7070 create-bucket --owner $ROOT_ACCESS_KEY --bucket ftzz
 
     echo "[Repository]" > rekkord.ini
-    echo "URL = http://localhost:3900/ftzz" >> rekkord.ini
+    echo "URL = http://localhost:7070/ftzz" >> rekkord.ini
     echo "KeyFile = rekkord.key" >> rekkord.ini
     echo "[S3]" >> rekkord.ini
-    echo "KeyID = $key" >> rekkord.ini
-    echo "SecretKey = $secret" >> rekkord.ini
+    echo "KeyID = $ROOT_ACCESS_KEY" >> rekkord.ini
+    echo "SecretKey = $ROOT_SECRET_KEY" >> rekkord.ini
     echo "ChecksumType = None" >> rekkord.ini
 
     seed=$RANDOM
-    ftzz -n10000 -b1000000000 --seed $seed src/small
-    ftzz -n8 -b2000000000 --seed $seed src/big
+    ./ftzz -n10000 -b1000000000 --seed $seed src/small
+    ./ftzz -n8 -b2000000000 --seed $seed src/big
     mkdir src/linux
     tar -Jxf linux.tar.xz -C src/linux
 
--- src/rekkord/test/repo_s3_nock.sh
+++ src/rekkord/test/repo_s3_nock.sh
@@ -7,47 +7,20 @@ ROOT=$(realpath "$PWD/../../..")
 $ROOT/bootstrap.sh
 $ROOT/felix -pFast rekkord
 
-mkdir -p /tmp/rekkord/repo_s3
-install $ROOT/bin/Fast/rekkord /tmp/rekkord/repo_s3/rekkord
-cd /tmp/rekkord/repo_s3
+mkdir -p /tmp/rekkord/repo_s3nock
+install $ROOT/bin/Fast/rekkord /tmp/rekkord/repo_s3nock/rekkord
+cd /tmp/rekkord/repo_s3nock
 
-curl -L -o garage https://garagehq.deuxfleurs.fr/_releases/v2.1.0/x86_64-unknown-linux-musl/garage
-chmod +x garage
+curl -L versitygw.tgz https://github.com/versity/versitygw/releases/download/v1.8.0/versitygw_v1.8.0_Linux_x86_64.tar.gz | tar xzO versitygw_v1.8.0_Linux_x86_64/versitygw > versitygw
+chmod +x versitygw
 
 export LANG=C
 export REKKORD_CONFIG_FILE=rekkord.ini
 export XDG_CACHE_HOME=$PWD/cache
 export GIT_DISCOVERY_ACROSS_FILESYSTEM=1
 
-cat > garage.toml <<EOF
-metadata_dir = "repo/meta"
-data_dir = "repo/data"
-db_engine = "sqlite"
-
-replication_factor = 1
-
-rpc_bind_addr = "[::]:3901"
-rpc_public_addr = "127.0.0.1:3901"
-rpc_secret = "$(openssl rand -hex 32)"
-
-[s3_api]
-s3_region = "garage"
-api_bind_addr = "[::]:3900"
-root_domain = ".s3.garage.localhost"
-
-[s3_web]
-bind_addr = "[::]:3902"
-root_domain = ".web.garage.localhost"
-index = "index.html"
-
-[k2v_api]
-api_bind_addr = "[::]:3904"
-
-[admin]
-api_bind_addr = "[::]:3903"
-admin_token = "$(openssl rand -base64 32)"
-metrics_token = "$(openssl rand -base64 32)"
-EOF
+export ROOT_ACCESS_KEY=root
+export ROOT_SECRET_KEY=root
 
 trap 'kill $(jobs -p) 2>/dev/null' EXIT
 trap 'kill $(jobs -p) 2>/dev/null' SIGINT
@@ -55,25 +28,18 @@ trap 'kill $(jobs -p) 2>/dev/null' SIGINT
 while true; do
     rm -rf cache repo git src dest rekkord.key
 
-    mkdir repo
+    mkdir repo repo/iam repo/data repo/version
 
-    RUST_LOG=garage=warn ./garage -c garage.toml server &
-    sleep 5
-
-    node=$(./garage -c garage.toml status | awk '/NO ROLE/ {print $1}')
-    ./garage -c garage.toml layout assign -c 16G -z rygel $node
-    ./garage -c garage.toml layout apply --version=1
-    ./garage -c garage.toml bucket create rygel
-    secret=$(./garage -c garage.toml key create rygel | awk -F ': +' '/Secret key/ {print $2}')
-    key=$(./garage -c garage.toml key info rygel | awk -F ': +' '/Key ID/ {print $2}')
-    ./garage -c garage.toml bucket allow rygel --key $key --read --write
+    ./versitygw --quiet --iam-dir $PWD/repo/iam posix --versioning-dir $PWD/repo/version $PWD/repo/data &
+    sleep 2
+    ./versitygw admin --endpoint-url http://localhost:7070 create-bucket --owner $ROOT_ACCESS_KEY --bucket ftzz
 
     echo "[Repository]" > rekkord.ini
-    echo "URL = http://localhost:3900/rygel" >> rekkord.ini
+    echo "URL = http://localhost:7070/ftzz" >> rekkord.ini
     echo "KeyFile = rekkord.key" >> rekkord.ini
     echo "[S3]" >> rekkord.ini
-    echo "KeyID = $key" >> rekkord.ini
-    echo "SecretKey = $secret" >> rekkord.ini
+    echo "KeyID = $ROOT_ACCESS_KEY" >> rekkord.ini
+    echo "SecretKey = $ROOT_SECRET_KEY" >> rekkord.ini
     echo "ChecksumType = None" >> rekkord.ini
 
     git clone --bare "$ROOT" git
